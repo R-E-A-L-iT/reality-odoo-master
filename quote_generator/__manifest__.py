702ddbb51a508c2b57b6254418f5af0ef6a2f51c
Ty Cyr <tyjcyr@gmail.com>
2021-06-24 15:05:04 +0000

    Quote Generator

--- quote_generator/__manifest__.py
+++ quote_generator/__manifest__.py
@@ -3,8 +3,7 @@
     'name': "quoteGenerator",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        A module to Create a Quality Quotation""",
 
     'description': """
         Long description of module's purpose
