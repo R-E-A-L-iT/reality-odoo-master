4846a6463a0c1ac01ab023087e32b9529e5b9a97
Ty Cyr <tyjcyr@gmail.com>
2021-06-24 13:13:32 +0000

    Quote Generator

--- quote_generator/__manifest__.py
+++ quote_generator/__manifest__.py
@@ -10,21 +10,21 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Ty Cyr",
+    'website': "R-E-A-L.iT",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '1.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
+        #'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
