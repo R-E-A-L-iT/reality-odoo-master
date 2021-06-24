5ededbb654268c22af533d62b4cff58658f9f921
Ty Cyr <tyjcyr@gmail.com>
2021-06-24 14:54:48 +0000

    Quote Generator

--- quote_generator/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ quote_generator/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "guoteGenerator",
+    'name': "quoteGenerator",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -34,5 +34,5 @@
     ],
     
     'installable': True,
-    'auto_install': False
+    'auto_install': True
 }
