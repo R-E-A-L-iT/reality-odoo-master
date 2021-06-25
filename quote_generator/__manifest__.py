37ae5f45405c43bb7bdfa770e59587b7e4af612f
Ty Cyr <tyjcyr@gmail.com>
2021-06-25 13:40:16 +0000

    quoteGenerator

--- quote_generator/__manifest__.py
+++ quote_generator/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '1.2',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'sale'],
 
     # always loaded
     'data': [
