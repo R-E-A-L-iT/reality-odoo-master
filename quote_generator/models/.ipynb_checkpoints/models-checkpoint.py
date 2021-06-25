89722ae81a3ba4e6cb66bd5225eb54507fe4076c
Ty Cyr <tyjcyr@gmail.com>
2021-06-25 18:12:42 +0000

    quote_generator

--- quote_generator/models/.ipynb_checkpoints/models-checkpoint.py
+++ quote_generator/models/.ipynb_checkpoints/models-checkpoint.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from datetime import datetime, timedelta
+from functools import partial
+from itertools import groupby
+
+from odoo import api, fields, models, SUPERUSER_ID, _
 from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.tools.misc import formatLang, get_lang
+from odoo.osv import expression
+from odoo.tools import float_is_zero, float_compare
 
 class quote_generator(models.Model):
 
