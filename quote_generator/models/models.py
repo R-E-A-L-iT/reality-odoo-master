143888ac9b3a08cb7e0f19e95682e1444b3ee178
Ty Cyr <tyjcyr@gmail.com>
2021-06-25 14:48:14 +0000

    quote_generator

--- quote_generator/models/models.py
+++ quote_generator/models/models.py
@@ -4,8 +4,9 @@ from odoo import models, fields, api
 
 class quote_generator(models.Model):
     _inherit = "sale.order"
+    _name = "quote_generator"
     
-    quote_Generator_transaction_ids = fields.Many2many('sale_order_id', 'transaction_id',
+    transaction_ids = fields.Many2many('sale_order_id', 'transaction_id',
                                        string='Transactions', copy=False, readonly=True)
     #def action_quotation_sent(self):
      #   if self.filtered(lambda so: so.state != 'draft'):
