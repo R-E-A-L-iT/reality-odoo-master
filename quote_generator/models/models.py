a01cef434537b8818487f5854b1f209fea814cd7
Ty Cyr <tyjcyr@gmail.com>
2021-06-25 18:04:03 +0000

    quote_generator

--- quote_generator/models/models.py
+++ quote_generator/models/models.py
@@ -21,9 +21,9 @@ class quote_generator(models.Model):
         for record in self:
             record.value2 = float(record.value) / 100
 
-    #def action_quotation_sent(self):
-     #   if self.filtered(lambda so: so.state != 'draft'):
-      #      raise UserError(_('Custom Error Message to Prove Sucsess'))
-       # for order in self:
-        #    order.message_subscribe(partner_ids=order.partner_id.ids)
-        #self.write({'state': 'sent'})
+    def action_quotation_sent(self):
+        if self.filtered(lambda so: so.state != 'draft'):
+            raise UserError(_('Custom Error Message to Prove Sucsess'))
+        for order in self:
+            order.message_subscribe(partner_ids=order.partner_id.ids)
+        self.write({'state': 'sent'})
