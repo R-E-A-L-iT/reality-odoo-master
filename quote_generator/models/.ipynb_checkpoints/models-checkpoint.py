f25c6176f9ba40c9b66611bc37f120e195665e6e
Ty Cyr <tyjcyr@gmail.com>
2021-06-25 17:51:33 +0000

    quote_generator

--- quote_generator/models/.ipynb_checkpoints/models-checkpoint.py
+++ quote_generator/models/.ipynb_checkpoints/models-checkpoint.py
@@ -20,12 +20,12 @@ class quote_generator(models.Model):
     def _value_pc(self):
         for record in self:
             record.value2 = float(record.value) / 100
-    #_inherit = "sale.order"
-    #def action_quotation_sent(self):
-     #   if self.filtered(lambda so: so.state != 'draft'):
-      #      raise UserError(_('Custom Error Message to Prove Sucsess'))
-       # for order in self:
-        #    order.message_subscribe(partner_ids=order.partner_id.ids)
-        #self.write({'state': 'sent'})
+
+    def action_quotation_sent(self):
+        if self.filtered(lambda so: so.state != 'draft'):
+            raise UserError(_('Custom Error Message to Prove Sucsess'))
+        for order in self:
+            order.message_subscribe(partner_ids=order.partner_id.ids)
+        self.write({'state': 'sent'})
             
             
