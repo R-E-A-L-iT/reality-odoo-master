e015d54b7a34c7910665db32fab367b74a9f6e16
Ty Cyr <tyjcyr@gmail.com>
2021-06-25 14:00:56 +0000

    quote_generator

--- quote_generator/models/models.py
+++ quote_generator/models/models.py
@@ -4,10 +4,10 @@ from odoo import models, fields, api
 
 class quote_generator(models.Model):
     _inherit = "sale.order"
-    def action_quotation_sent(self):
-        if self.filtered(lambda so: so.state != 'draft'):
-            raise UserError(_('Custom Error Message to Prove Sucsess'))
-        for order in self:
-            order.message_subscribe(partner_ids=order.partner_id.ids)
-        self.write({'state': 'sent'})
+    #def action_quotation_sent(self):
+     #   if self.filtered(lambda so: so.state != 'draft'):
+      #      raise UserError(_('Custom Error Message to Prove Sucsess'))
+       # for order in self:
+        #    order.message_subscribe(partner_ids=order.partner_id.ids)
+        #self.write({'state': 'sent'})
             
