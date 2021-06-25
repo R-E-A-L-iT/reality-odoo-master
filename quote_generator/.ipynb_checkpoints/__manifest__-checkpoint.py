c00ab48e31392dfa0de78f687fc3fcb2560bf50d
Ty Cyr <tyjcyr@gmail.com>
2021-06-25 13:54:04 +0000

    quote_generator

--- quote_generator/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ quote_generator/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -10,7 +10,6 @@
     """,
 
     'author': "Ty Cyr",
-    'website': "R-E-A-L.iT",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
