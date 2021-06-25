1ef1a7c2cdc65bd3c6746c91e348877ccec82c8c
Ty Cyr <tyjcyr@gmail.com>
2021-06-25 13:29:03 +0000

    quoteGenerator

--- quote_generator/models/models.py
+++ quote_generator/models/models.py
@@ -4,17 +4,11 @@ from odoo import models, fields, api
 
 
 class quote_generator(models.Model):
-    _name = 'quote_generator.quote_generator'
-    _description = 'quote_generator.quote_generator'
-
-    name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
-    
-
-    @api.depends('value')
-    def _value_pc(self):
-        for record in self:
-            record.value2 = float(record.value) / 100
+    _inherit = "sale.order"
+    def action_quotation_sent(self):
+        if self.filtered(lambda so: so.state != 'draft'):
+            raise UserError(_('Custom Error Message to Prove Sucsess'))
+        for order in self:
+            order.message_subscribe(partner_ids=order.partner_id.ids)
+        self.write({'state': 'sent'})
             
