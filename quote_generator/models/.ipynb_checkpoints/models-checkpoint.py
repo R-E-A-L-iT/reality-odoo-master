cbe6f4154c9f384eeb736851c147bc16af78fd3d
Ty Cyr <tyjcyr@gmail.com>
2021-06-25 16:40:16 +0000

    quote_generator

--- quote_generator/models/.ipynb_checkpoints/models-checkpoint.py
+++ quote_generator/models/.ipynb_checkpoints/models-checkpoint.py
@@ -3,8 +3,19 @@
 from odoo import models, fields, api
 
 class quote_generator(models.Model):
+    _name = 'quote_generator.quote_generator'
+    _inherit = "sales.order"
+    _description = 'quote_generator.quote_generator'
+
+    name = fields.Char()
+    description = fields.Text()
+
+
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
     #_inherit = "sale.order"
-    _name = "quote_generator.quote_generator"
     #def action_quotation_sent(self):
      #   if self.filtered(lambda so: so.state != 'draft'):
       #      raise UserError(_('Custom Error Message to Prove Sucsess'))
