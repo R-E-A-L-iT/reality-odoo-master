66ee37dddb2af36a9b6adf22665609d23ebe7a9d
Ty Cyr <tyjcyr@gmail.com>
2021-06-28 21:02:27 +0000

    mtest

--- mtest/__manifest__.py
+++ mtest/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'website', 'sale_management'],
+    'depends': ['base', 'website', 'sale_management', 'sale'],
 
     # always loaded
     'data': [
