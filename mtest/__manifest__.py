0bc5a275e6744e1f1fd4490eccd601063f716bd6
Ty cyr <tyjcyr@gmail.com>
2021-06-29 12:16:19 +0000

    mtest

--- mtest/__manifest__.py
+++ mtest/__manifest__.py
@@ -25,11 +25,13 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
+        #'views/views.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    
+    'qweb': 'static/src/XML/proQuote.xml'
 }
