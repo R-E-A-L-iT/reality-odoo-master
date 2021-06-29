e651cc83c18f45541085dbafebd5c19b35494d40
Ty Cyr <tyjcyr@gmail.com>
2021-06-29 15:46:34 +0000

    mtest

--- mtest/models/.ipynb_checkpoints/models-checkpoint.py
+++ mtest/models/.ipynb_checkpoints/models-checkpoint.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 
-#from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-#class mtest(models.Model):
-#    _name = 'mtest.mtest'
-#    _description = 'mtest.mtest'
+class mtest(models.Model):
+    _inherit = 'sale.order.template'
 
-#    name = fields.Char()
-#    value = fields.Integer()
-#    value2 = fields.Float(compute="_value_pc", store=True)
-#    description = fields.Text()
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
 
-#    @api.depends('value')
-#    def _value_pc(self):
-#        for record in self:
-#            record.value2 = float(record.value) / 100
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
