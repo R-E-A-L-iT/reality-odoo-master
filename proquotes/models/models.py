2544f22bccb2c3927006c3b7ac8e6d5e673d75fd
Ty Cyr <tyjcyr@gmail.com>
2021-07-07 12:27:56 +0000

    proquotes

---
+++ proquotes/models/models.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+#from datetime import datetime, timedelta
+#from functools import partial
+#from itertools import groupby
+
+#from odoo import api, fields, models, SUPERUSER_ID, _
+#from odoo.exceptions import AccessError, UserError, ValidationError
+#from odoo.tools.misc import formatLang, get_lang
+#from odoo.osv import expression
+#from odoo.tools import float_is_zero, float_compare
+#from odoo import models, fields, api
+
+
+#class mtest(models.Model):
+#    _inherit = 'sale.order'
+#    _description = 'mtest.mtest'
+
+#    name = fields.Char()
+#    value = fields.Integer()
+#    value2 = fields.Float(compute="_value_pc", store=True)
+#    description = fields.Text()
+
+#    @api.depends('value')
+#    def _value_pc(self):
+#        for record in self:
+#            record.value2 = float(record.value) / 100
