30ebca6fcfbead0f0806ca78da9325d5329e64c8
Ty Cyr <tyjcyr@gmail.com>
2021-07-08 12:18:18 +0000

    proquotes

--- proquotes/static/src/JS/fold.js
+++ proquotes/static/src/JS/fold.js
@@ -3,7 +3,7 @@ var Widget = require(web.widget)
 var Counter = Widget.extend({
     template: 'proquotes.sale_order_portal_content',
     events: {
-        'fold': '_onClick',
+        'fold': '_onChange',
     },
     init: function (parent, value) {
         this._super(parent);
