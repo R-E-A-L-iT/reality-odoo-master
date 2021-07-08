7fc850b0742199d838916399678b5883f870fe7c
Ty Cyr <tyjcyr@gmail.com>
2021-07-08 15:16:30 +0000

    proquotes

--- proquotes/static/src/JS/fold.js
+++ proquotes/static/src/JS/fold.js
@@ -11,7 +11,7 @@ publicWidget.registry.fold = publicWidget.Widget.extend({
         this._super(parent);
     },
     _onChange: function (c) {
-        console.log(c);
+        console.log(c.target.value);
     },
 });
 });
