b44fdf91e1b480dce6fe628ddbe316221142995a
Ty Cyr <tyjcyr@gmail.com>
2021-07-08 14:03:18 +0000

    proquotes

--- proquotes/static/src/JS/.ipynb_checkpoints/fold-checkpoint.js
+++ proquotes/static/src/JS/.ipynb_checkpoints/fold-checkpoint.js
@@ -5,7 +5,7 @@ var publicWidget = require(web.publicWidget)
 publicWidget.registry.fold = publicWidget.Widget.extend({
     selector: '.o_portal_sale_sidebar',
     events: {
-        'click .fold': '_onChange',
+        'change .fold': '_onChange',
     },
     init: function (parent, value) {ss
         this._super(parent);
