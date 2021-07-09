6d6cc9254b0337a5feeef6d4c0e93477ee989b7e
Ty Cyr <tyjcyr@gmail.com>
2021-07-09 17:27:10 +0000

    proquotes

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -16,7 +16,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Sales',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -26,7 +26,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/quotes.xml',
-        'views/quotesBackend',
+        'views/quotesBackend.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
