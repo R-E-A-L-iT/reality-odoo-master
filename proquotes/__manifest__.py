64dcc4d222e59618bb97c284880872c82326bd96
Ty Cyr <tyjcyr@gmail.com>
2021-07-09 21:11:35 +0000

    proquotes

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -25,7 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/quotes.xml',
+        'views/quotesFrontend.xml',
         'views/quotesBackend.xml',
     ],
     # only loaded in demonstration mode
