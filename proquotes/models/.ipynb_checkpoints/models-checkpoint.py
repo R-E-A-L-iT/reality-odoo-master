a9af1a3db520d3ac87eb8328b4ea9a6170fa9747
Ty Cyr <tyjcyr@gmail.com>
2021-07-12 18:13:54 +0000

    proquotes

--- proquotes/models/.ipynb_checkpoints/models-checkpoint.py
+++ proquotes/models/.ipynb_checkpoints/models-checkpoint.py
@@ -14,4 +14,6 @@ from odoo import models, fields, api
 
 class mtest(models.Model):
     _inherit = 'sale.order.line'
-    special = 'multiple'
+    special = display_type = fields.Selection([
+        ('multiple', "Multiple"),
+        ('multiple', "Optional")], default=False, help="Technical field for UX purpose.")
