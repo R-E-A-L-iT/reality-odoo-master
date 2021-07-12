723ee0acb53b4d6f772f3c8f453a8b430be76b91
Ty Cyr <tyjcyr@gmail.com>
2021-07-12 15:47:17 +0000

    proquotes

--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -15,14 +15,3 @@ from odoo import models, fields, api
 class mtest(models.Model):
     _inherit = 'sale.order.line'
     special = 'multiple'
-
-
-#    name = fields.Char()
-#    value = fields.Integer()
-#    value2 = fields.Float(compute="_value_pc", store=True)
-#    description = fields.Text()
-
-#    @api.depends('value')
-#    def _value_pc(self):
-#        for record in self:
-#            record.value2 = float(record.value) / 100
