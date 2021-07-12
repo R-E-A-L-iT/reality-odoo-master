adf034aa7c6c4d20041ca1772a8113b68df3a910
Ty Cyr <tyjcyr@gmail.com>
2021-07-12 15:34:32 +0000

    proquotes

--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -1,20 +1,20 @@
 #-*- coding: utf-8 -*-
 
-#from datetime import datetime, timedelta
-#from functools import partial
-#from itertools import groupby
+from datetime import datetime, timedelta
+from functools import partial
+from itertools import groupby
 
-#from odoo import api, fields, models, SUPERUSER_ID, _
-#from odoo.exceptions import AccessError, UserError, ValidationError
-#from odoo.tools.misc import formatLang, get_lang
-#from odoo.osv import expression
-#from odoo.tools import float_is_zero, float_compare
-#from odoo import models, fields, api
+from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.tools.misc import formatLang, get_lang
+from odoo.osv import expression
+from odoo.tools import float_is_zero, float_compare
+from odoo import models, fields, api
 
 
-#class mtest(models.Model):
-#    _inherit = 'sale.order'
-#    _description = 'mtest.mtest'
+class mtest(models.Model):
+    _inherit = 'sale.order.line'
+
 
 #    name = fields.Char()
 #    value = fields.Integer()
