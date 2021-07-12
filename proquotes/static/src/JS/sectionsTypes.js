0761d2be2a0067a881305f58608c5657af5632a7
Ty Cyr <tyjcyr@gmail.com>
2021-07-12 13:24:12 +0000

    proquotes

--- proquotes/static/src/JS/sectionsTypes.js
+++ proquotes/static/src/JS/sectionsTypes.js
@@ -8,30 +8,31 @@ odoo.define('proquotes.section_types_backend', function (require) {
     var ListRenderer = require('web.ListRenderer');
     var CustomSectionRenderer = SectionAndNoteListRenderer.extend({
         _renderBodyCell: function (record, node, index, options) {
-        var $cell = this._super.apply(this, arguments);
+            var $cell = this._super.apply(this, arguments);
 
-        var isOptionalSection = record.data.display_type === 'line_option_section';
-        var isMultipleSection = record.data.display_type === 'line_multiple_section';
+            var isOptionalSection = record.data.display_type === 'line_option_section';
+            var isMultipleSection = record.data.display_type === 'line_multiple_section';
 
-        if (isOptionalSection || isMultipleSection) {
-            if (node.attrs.widget === "handle") {
-                return $cell;
-            } else if (node.attrs.name === "name") {
-                var nbrColumns = this._getNumberOfCols();
-                if (this.handleField) {
-                    nbrColumns--;
+            if (isOptionalSection || isMultipleSection) {
+                if (node.attrs.widget === "handle") {
+                    return $cell;
+                } else if (node.attrs.name === "name") {
+                    var nbrColumns = this._getNumberOfCols();
+                    if (this.handleField) {
+                        nbrColumns--;
+                    }
+                    if (this.addTrashIcon) {
+                        nbrColumns--;
+                    }
+                    $cell.attr('colspan', nbrColumns);
+                } else {
+                    $cell.removeClass('o_invisible_modifier');
+                    return $cell.addClass('o_hidden');
                 }
-                if (this.addTrashIcon) {
-                    nbrColumns--;
-                }
-                $cell.attr('colspan', nbrColumns);
-            } else {
-                $cell.removeClass('o_invisible_modifier');
-                return $cell.addClass('o_hidden');
             }
-        }
 
-        return $cell;
-    },
+            return $cell;
+        },
     
+    }
 }
