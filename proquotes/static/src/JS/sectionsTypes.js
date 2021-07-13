3d2aa8a09e37a0900ddfe9181dca6037128be3c4
Ty Cyr <tyjcyr@gmail.com>
2021-07-13 14:13:14 +0000

    proquotes

--- proquotes/static/src/JS/sectionsTypes.js
+++ proquotes/static/src/JS/sectionsTypes.js
@@ -14,7 +14,7 @@ var SectionAndNoteListRenderer = ListRenderer.extend({
 
         var is_section = record.data.display_type === 'line_section';
         var is_note = record.data.display_type === 'line_note';
-        alert(record.data.special);
+        console.log(record.data)
 
         if (is_section || is_note) {
             if (node.attrs.widget === "handle") {
