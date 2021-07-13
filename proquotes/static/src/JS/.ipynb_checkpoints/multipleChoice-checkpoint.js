980f0a3408ebcdc55ded0aee0b5b96337fffe7b4
Ty Cyr <tyjcyr@gmail.com>
2021-07-13 18:06:13 +0000

    proquotes

---
+++ proquotes/static/src/JS/.ipynb_checkpoints/multipleChoice-checkpoint.js
@@ -0,0 +1,45 @@
+odoo.define('proquotes.fold', function (require) {
+'use strict';
+var publicWidget = require('web.public.widget')
+
+publicWidget.registry.fold = publicWidget.Widget.extend({
+    selector: '.o_portal_sale_sidebar',
+    events: {
+    },
+    init: function (parent) {
+        this._super(parent);
+        this._onLoad();
+    },
+    
+    _onLoad: function () {
+        var TRstyle;
+        var cbl = document.getElementsByClassName("multipleSection");
+        for(var i = 0; i < cbl.length; i++){
+            var cb = cbl[i];
+            if(cb.checked){
+            TRstyle = "none";
+            } else {
+            TRstyle = "table-row";
+            }
+            var x = cb.parentNode.parentNode;
+            var y = x.nextElementSibling;
+            while(y != null && y != undefined){
+                if(y.className.includes("is-subtotal")){
+                    break;
+                } else {
+                    if(y.style != undefined && y.style != null){
+                        y.style.display = TRstyle;
+                    }
+                }
+            y = y.nextElementSibling;
+            }
+        }
+        var subTotalList = document.getElementsByClassName("subtotal-destination");
+        for(var i = 0; i < subTotalList.length; i++){
+            var subTotal = subTotalList[i];
+            subTotal.innerHTML = document.getElementsByClassName("subtotal-source")[i].innerHTML;
+            
+        }
+    },
+});
+});
