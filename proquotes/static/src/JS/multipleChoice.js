d503ee54743811412df1d90645c9ffccc43c506c
Ty Cyr <tyjcyr@gmail.com>
2021-07-13 19:37:35 +0000

    proquotes

--- proquotes/static/src/JS/multipleChoice.js
+++ proquotes/static/src/JS/multipleChoice.js
@@ -23,7 +23,7 @@ publicWidget.registry.multipleChoice = publicWidget.Widget.extend({
                     break;
                 } else {
                     var z = document.createElement("input");
-                    y.childNode[0].appendChild(z);
+                    y.childNodes[0].appendChild(z);
                     z.type = "radio";
                     z.name = ("multipleChoice" + i)
                     z.style.float = "left";
