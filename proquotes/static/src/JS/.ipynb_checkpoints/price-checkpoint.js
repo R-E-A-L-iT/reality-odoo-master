120fe38b6c53f7aa6044968b7c64a0bd52c01542
Ty Cyr <tyjcyr@gmail.com>
2021-07-14 17:51:51 +0000

    proquotes

--- proquotes/static/src/JS/.ipynb_checkpoints/price-checkpoint.js
+++ proquotes/static/src/JS/.ipynb_checkpoints/price-checkpoint.js
@@ -11,7 +11,7 @@ publicWidget.registry.price = publicWidget.Widget.extend({
     async start() {
         await this._super(...arguments);
         this.orderDetail = this.$el.find('table#sales_order_table').data();
-        console.log(this.orderDetail);
+        console.log(this.$el);
         this.elems = this._getUpdatableElements();
     },
     
