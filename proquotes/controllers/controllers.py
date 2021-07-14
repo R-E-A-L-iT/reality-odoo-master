e549ffd0ec0f92a2209e9cc81514b336df4d15f9
Ty Cyr <tyjcyr@gmail.com>
2021-07-14 20:26:55 +0000

    proquotes

--- proquotes/controllers/controllers.py
+++ proquotes/controllers/controllers.py
@@ -22,11 +22,11 @@ class CustomerPortal(CustomerPortal):
         
 
         select_sudo = request.env['sale.order.line'].sudo().browse(line_id)
-        raise UserError(_("Here"))
-        if order_sudo != option_sudo.order_id:
+        
+        if order_sudo != select_sudo.line_id:
             return request.redirect(order_sudo.get_portal_url())
 
-        option_sudo.add_option_to_order()
+        raise UserError(_("HERE"))
         results = self._get_portal_order_details(order_sudo)
         results['sale_template'] = request.env['ir.ui.view']._render_template("sale.sale_order_portal_content", {
             'sale_order': select_sudo.order_id,
