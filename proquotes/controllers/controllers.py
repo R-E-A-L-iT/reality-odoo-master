9c424a435455c36a183d9e307a696a898ab3e52d
Ty Cyr <tyjcyr@gmail.com>
2021-07-14 18:51:58 +0000

    proquotes

--- proquotes/controllers/controllers.py
+++ proquotes/controllers/controllers.py
@@ -4,7 +4,7 @@
 import binascii
 
 from odoo import fields, http, _
-from odoo.exceptions import AccessError, MissingError
+from odoo.exceptions import AccessError, MissingError, UserError
 from odoo.http import request
 from odoo.addons.payment.controllers.portal import PaymentProcessing
 from odoo.addons.portal.controllers.mail import _message_post_helper
@@ -12,5 +12,23 @@ from odoo.addons.portal.controllers.portal import CustomerPortal, pager as porta
 from odoo.osv import expression
 
 class CustomerPortal(CustomerPortal):
-    def somethifsdajfs():
-        print("Hello")
+    @http.route(["/my/orders/<int:order_id>/add_option/<int:option_id>"], type='json', auth="public", website=True)
+    def add(self, order_id, option_id, access_token=None, **post):
+        raise UserError(_('NO!'))
+        try:
+            order_sudo = self._document_check_access('sale.order', order_id, access_token=access_token)
+        except (AccessError, MissingError):
+            return request.redirect('/my')
+
+        option_sudo = request.env['sale.order.option'].sudo().browse(option_id)
+
+        if order_sudo != option_sudo.order_id:
+            return request.redirect(order_sudo.get_portal_url())
+
+        option_sudo.add_option_to_order()
+        results = self._get_portal_order_details(order_sudo)
+        results['sale_template'] = request.env['ir.ui.view']._render_template("sale.sale_order_portal_content", {
+            'sale_order': option_sudo.order_id,
+            'report_type': "html"
+        })
+        return results
