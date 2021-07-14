f083f053badc916d715c0472c9783dc4102475cc
Ty Cyr <tyjcyr@gmail.com>
2021-07-14 15:25:22 +0000

    proquotes

---
+++ proquotes/static/src/JS/.ipynb_checkpoints/price-checkpoint.js
@@ -0,0 +1,24 @@
+odoo.define('proquotes.price', function (require) {
+'use strict';
+var publicWidget = require('web.public.widget')
+
+publicWidget.registry.price = publicWidget.Widget.extend({
+    selector: '.o_portal_sale_sidebar',
+    events: {
+        'change .priceChange': '_updatePriceTotals',
+    },
+    init: function (parent) {
+        this._super(parent);
+        this._onLoad();
+    },
+    
+    _onLoad: function () {
+        //this._updatePriceTotals();
+    },
+    
+    _updatePriceTotals () {
+        alert("Price Total Changed");
+        
+    }
+});
+});
