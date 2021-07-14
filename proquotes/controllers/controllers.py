0aef8ca1b0c5d14a230c9d0d54d26a50d11c37a0
Ty Cyr <tyjcyr@gmail.com>
2021-07-14 19:05:17 +0000

    proquotes

--- proquotes/controllers/controllers.py
+++ proquotes/controllers/controllers.py
@@ -14,7 +14,7 @@ from odoo.osv import expression
 class CustomerPortal(CustomerPortal):
     @http.route(["/my/orders/<int:order_id>/select/<int:option_id>"], type='json', auth="public", website=True)
     def select(self, order_id, option_id, access_token=None, **post):
-        raise UserError(_('NO!'))
+        raise UserError(_('NO!' + order_id + "S" + option_id))
         try:
             order_sudo = self._document_check_access('sale.order', order_id, access_token=access_token)
         except (AccessError, MissingError):
