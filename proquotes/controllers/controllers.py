4d2e997d63bf2c06207a50eb178388ba07104b7d
Ty Cyr <tyjcyr@gmail.com>
2021-07-15 17:54:10 +0000

    proquotes

--- proquotes/controllers/controllers.py
+++ proquotes/controllers/controllers.py
@@ -12,7 +12,7 @@ from odoo.addons.portal.controllers.portal import CustomerPortal, pager as porta
 from odoo.osv import expression
 
 class CustomerPortal(CustomerPortal):
-    @http.route(["/my/orders/<int:order_id>/select/<int:line_id>"], type='http', auth="public", website=True)
+    @http.route(["/my/orders/<int:order_id>/select/<int:line_id>"], type='json', auth="public", website=True)
     def select(self, order_id, line_id, selected,  access_token=None, **post):
 
         try:
@@ -31,7 +31,8 @@ class CustomerPortal(CustomerPortal):
         results = self._get_portal_order_details(order_sudo)
         results['sale_template'] = request.env['ir.ui.view']._render_template("sale.sale_order_portal_content", {
             'sale_order': select_sudo.order_id,
-            'report_type': "html"
+            'report_type': "html",
+            'tag': "reload"
         })
         
         return results
