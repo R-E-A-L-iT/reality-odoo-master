939da36e0ff888eb07d6b9db11b558c25b10d173
Ty Cyr <tyjcyr@gmail.com>
2021-07-15 21:02:18 +0000

    proquotes

--- proquotes/controllers/.ipynb_checkpoints/controllers-checkpoint.py
+++ proquotes/controllers/.ipynb_checkpoints/controllers-checkpoint.py
@@ -14,11 +14,10 @@ from odoo.osv import expression
 class CustomerPortal(CustomerPortal):
     @http.route(["/my/orders/<int:order_id>/select"], type='json', auth="public", website=True)
     def select(self, order_id, line_ids, selected,  access_token=None, **post):
-
+        raise _UserError(_("Here"))
         try:
             order_sudo = self._document_check_access('sale.order', order_id, access_token=access_token)
         except (AccessError, MissingError):
-            raise UserError(_("Here"))
             return request.redirect('/my')
         
         i = 0
