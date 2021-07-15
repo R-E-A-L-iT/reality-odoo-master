8651d6b71f0761f26e5d44af1f83e731396ac695
Ty Cyr <tyjcyr@gmail.com>
2021-07-15 13:08:26 +0000

    proquotes

--- proquotes/controllers/controllers.py
+++ proquotes/controllers/controllers.py
@@ -39,7 +39,7 @@ class CustomerPortal(CustomerPortal):
         
         results = self._get_portal_order_details(order_sudo)
         results['sale_template'] = request.env['ir.ui.view']._render_template("sale.sale_order_portal_content", {
-            'sale_order': select_sudo.order_id,
+            'sale_order': order_id,
             'report_type': "html"
         })
         
