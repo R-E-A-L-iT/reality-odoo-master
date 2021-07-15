19fdb6b3fcddc24eb9eaea61e7e25846a61ab6dc
Ty Cyr <tyjcyr@gmail.com>
2021-07-15 21:26:35 +0000

    proquotes

--- proquotes/static/src/JS/.ipynb_checkpoints/price-checkpoint.js
+++ proquotes/static/src/JS/.ipynb_checkpoints/price-checkpoint.js
@@ -48,8 +48,79 @@ publicWidget.registry.price = publicWidget.Widget.extend({
         });
     },
     
+    _multipleChoiceView: function () {
+        var cbl = document.querySelectorAll(".multipleChoice");
+        for(var i = 0; i < cbl.length; i++){
+            var cb = cbl[i];
+            var x = cb.parentNode.parentNode;
+            var y = x.nextElementSibling;
+            var k = 0;
+            var firstChecked = null;
+            while(y != null && y != undefined){
+                if(y.className.includes("is-subtotal")){
+                    break;
+                } else {
+                    var z = y.querySelector("input[type='radio']");
+                    if(z.checked){
+                        if(firstChecked == null){
+                            firstChecked = ("multipleChoice" + i.toString() + "R" + k.toString());
+                        }
+                    }
+                    z.className = "priceChange";
+                    z.type = "radio";
+                    z.name = ("multipleChoice" + i.toString());
+                    z.id = ("multipleChoice" + i.toString() + "R" + k.toString());
+                    z.style.display="";
+                    
+                    
+                    
+                    var tdList = y.querySelectorAll("td");
+
+                    for(var j = 0; j < tdList.length; j++){
+                        var inner = tdList[j].innerHTML;
+                        var l = document.createElement("label");
+                        l.setAttribute("for", ("multipleChoice" + i.toString() + "R" + k.toString()));
+                        l.style.width = "100%";
+                        l.innerHTML = inner;
+                        tdList[j].innerHTML = "";
+                        tdList[j].append(l);
+                    }
+                }
+            k++;
+            y = y.nextElementSibling;
+            }
+            if(firstChecked != null){
+                document.getElementById(firstChecked).checked = true;
+            }
+        }
+    },
+    
+    _optionalView: function () {
+        var cbl = document.querySelectorAll("input[type=checkbox].priceChange");
+        for(var i = 0; i < cbl.length; i++){
+            var cb = cbl[i];
+            var row = cb.parentNode.parentNode;
+            cb.name = ("optional" + i.toString());
+            cb.id = ("optional" + i.toString() + "O");
+            
+            var tdList = row.querySelectorAll("td");
+
+            for(var j = 0; j < tdList.length; j++){
+                var inner = tdList[j].innerHTML;
+                var l = document.createElement("label");
+                l.setAttribute("for", ("optional" + i.toString() + "O"));
+                l.style.width = "100%";
+                l.innerHTML = inner;
+                tdList[j].innerHTML = "";
+                tdList[j].append(l);
+            }
+            
+        }
+    }
+    
     _updateView: function () {
-        //location.reload();
+        this._multipleChoiceView();
+        this._optionalView();
     },
 });
 });
