06fd6fdf73833aa7634ddd5323c9ca0f5798a519
Ty Cyr <tyjcyr@gmail.com>
2021-07-15 18:33:19 +0000

    proquotes

--- proquotes/models/.ipynb_checkpoints/models-checkpoint.py
+++ proquotes/models/.ipynb_checkpoints/models-checkpoint.py
@@ -26,6 +26,29 @@ class order(models.Model):
                 'amount_tax': amount_tax,
                 'amount_total': amount_untaxed + amount_tax,
             })
+            
+    def _amount_by_group(self):
+        for order in self:
+            currency = order.currency_id or order.company_id.currency_id
+            fmt = partial(formatLang, self.with_context(lang=order.partner_id.lang).env, currency_obj=currency)
+            res = {}
+            for line in order.order_line:
+                if(line.selected == 'true'):
+                    price_reduce = line.price_unit * (1.0 - line.discount / 100.0)
+                    taxes = line.tax_id.compute_all(price_reduce, quantity=line.product_uom_qty, product=line.product_id, partner=order.partner_shipping_id)['taxes']
+                    for tax in line.tax_id:
+                        group = tax.tax_group_id
+                        res.setdefault(group, {'amount': 0.0, 'base': 0.0})
+                        for t in taxes:
+                            if t['id'] == tax.id or t['id'] in tax.children_tax_ids.ids:
+                                res[group]['amount'] += t['amount']
+                                res[group]['base'] += t['base']
+                res = sorted(res.items(), key=lambda l: l[0].sequence)
+                order.amount_by_group = [(
+                    l[0].name, l[1]['amount'], l[1]['base'],
+                    fmt(l[1]['amount']), fmt(l[1]['base']),
+                    len(res),
+                ) for l in res]
 
 class proquotes(models.Model):
     _inherit = 'sale.order.line'
