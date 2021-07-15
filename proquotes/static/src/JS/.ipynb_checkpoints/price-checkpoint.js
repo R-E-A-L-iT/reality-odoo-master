dbf1c3467048110ec28f76d415b37dfe935525c2
Ty Cyr <tyjcyr@gmail.com>
2021-07-15 15:54:06 +0000

    proquotes

--- proquotes/static/src/JS/.ipynb_checkpoints/price-checkpoint.js
+++ proquotes/static/src/JS/.ipynb_checkpoints/price-checkpoint.js
@@ -32,9 +32,10 @@ publicWidget.registry.price = publicWidget.Widget.extend({
     _updatePriceTotals: function (target){
         var line_id = target.parentNode.parentNode.parentNode.querySelector("div").dataset["oeId"];        
         
-        this._rpc({
+        var res = this._rpc({
             route: "/my/orders/" + this.orderDetail.orderId + "/select/" + line_id,
             params: {access_token: this.orderDetail.token, 'selected': target.checked ? 'true' : 'false'}})
+        console.log(res);
     },
 });
 });
