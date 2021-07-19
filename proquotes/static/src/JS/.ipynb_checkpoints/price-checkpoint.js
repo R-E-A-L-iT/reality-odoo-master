e219c75914f0867e4c4849c7377f37acf53bffee
Ty Cyr <tyjcyr@gmail.com>
2021-07-19 14:51:47 +0000

    proquotes

--- proquotes/static/src/JS/.ipynb_checkpoints/price-checkpoint.js
+++ proquotes/static/src/JS/.ipynb_checkpoints/price-checkpoint.js
@@ -27,16 +27,16 @@ publicWidget.registry.price = publicWidget.Widget.extend({
         while(p.tagName != "TR"){
             p = p.parentNode;
         }
-        var lineId = p.querySelector("div").dataset["oeId"]);
+        var lineId = p.querySelector("div").dataset["oeId"];
         console.log(lineId);
-        /*return this._rpc({
+        return this._rpc({
             route: "my/orders/" + this.orderDetail.orderId + "/changeQuantity" + lineId,
             params: {access_token: this.orderDetail.token, line_id: lineId, quantity: qty}}).then((data) => {
             if (data) {
                 self.$('#portal_sale_content').html($(data['sale_inner_template']));
                 this._updateView(data['order_amount_total']);
             }
-        });*/
+        });
     },
     
     _updatePriceTotalsEvent: function () {
