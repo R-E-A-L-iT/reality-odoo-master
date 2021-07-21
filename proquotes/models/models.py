d4ed177efe831557d02d16037837f1f134c585b3
Ty Cyr <tyjcyr@gmail.com>
2021-07-21 20:47:24 +0000

    proquotes

--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -87,7 +87,7 @@ class orderLineProquotes(models.Model):
     product_description = fields.Text(compute='_compute_descripion', required=True, readonly=False, string='Product Description')
     
     def _compute_descripion(self):
-        return str(self.product_id.description_sale)
+        return str(self.product_id)
     
 class variant(models.Model):
     _name = 'proquotes.variant'
