4b6aee48fd0efa1d36e955ea179ac15242da1d69
Ty Cyr <tyjcyr@gmail.com>
2021-07-21 18:14:17 +0000

    proquotes

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -10,13 +10,12 @@
     """,
 
     'author': "Ty Cyr",
-    #'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Sales',
-    'version': '0.4',
+    'version': '0.5',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'website', 'sale_management', 'sale', 'digest'],
