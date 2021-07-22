483b14b81240aa1bf2d36f0dfe0eef2d49a644eb
Ty Cyr <tyjcyr@gmail.com>
2021-07-22 20:37:57 +0000

    sync

--- sync/__manifest__.py
+++ sync/__manifest__.py
@@ -15,13 +15,14 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Technical',
-    'version': '0.2',
+    'version': '0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'product'],
     
     # always loaded
     'data': [
+        'security/ir.model.access.csv',
         #'views/quotesFrontend.xml',
         #'views/quotesBackend.xml',
     ],
