ae31ddecaee9ae7807ffc98ea500bccff4789932
Ty Cyr <tyjcyr@gmail.com>
2021-07-22 15:35:33 +0000

    proquotes

--- proquotes/static/src/JS/price.js
+++ proquotes/static/src/JS/price.js
@@ -64,16 +64,32 @@ publicWidget.registry.price = publicWidget.Widget.extend({
         var target = ev.currentTarget;
         var checked = target.checked;
         var p = target;
+        var line_ids = [];
         while(p.tagName != "TR"){
             console.log(p.tagName)
             p = p.parentNode;
         }
         var y = p.nextElementSibling;
-        sectionId = p.querySelector(".line_id").id;
+        var section_id = p.querySelector(".line_id").id;
         while(y != null && y != undefined){
+            if(y.className.includes("is-subtotal")){
+                break;
+            }
+            line_ids.push(y.querySelector(".line_id").id);
             y = y.nextElementSibling;
-            console.log(y);
         }
+        let self = this;
+        
+        return this._rpc({
+            route: "/my/orders/" + this.orderDetail.orderId + "/sectionSelect",
+            params: {access_token: this.orderDetail.token, section_id: section_id, line_ids: line_ids, selected: checked}
+        }).then((data) =>{
+            if(data) {
+                self.$('#portal_sale_content').html($(data['sale_inner_template']));
+                this.updateView(data['order_amount_total'])
+            }
+            
+        });
     },
     
     _updatePriceTotals: function (targetsChecked, line_ids){
@@ -81,7 +97,7 @@ publicWidget.registry.price = publicWidget.Widget.extend({
         
         return this._rpc({
             route: "/my/orders/" + this.orderDetail.orderId + "/select",
-            params: {access_token: this.orderDetail.token, line_ids: line_ids,'selected': targetsChecked}}).then((data) => {
+            params: {access_token: this.orderDetail.token, line_ids: line_ids, selected: targetsChecked}}).then((data) => {
             if (data) {
                 self.$('#portal_sale_content').html($(data['sale_inner_template']));
                 this._updateView(data['order_amount_total']);
