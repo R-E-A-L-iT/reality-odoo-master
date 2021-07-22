441927fba90ee255bac2375c62ff62c658def909
Ty Cyr <tyjcyr@gmail.com>
2021-07-22 20:31:05 +0000

    sync

---
+++ sync/models/.ipynb_checkpoints/models-checkpoint.py
@@ -0,0 +1,21 @@
+#-*- coding: utf-8 -*-
+
+from datetime import datetime, timedelta
+from functools import partial
+from itertools import groupby
+
+from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.tools.misc import formatLang, get_lang
+from odoo.osv import expression
+from odoo.tools import float_is_zero, float_compare
+from odoo import models, fields, api
+
+
+class Sync(models.Model):
+    _name = "sync.sync"
+    _description = "Sync App"
+    
+    @api.model
+    def e(self):
+        raise UserError(_name)
