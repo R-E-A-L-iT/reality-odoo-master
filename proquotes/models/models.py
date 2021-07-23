c57bff2427136240343943afd32bd464433c7577
Ty Cyr <tyjcyr@gmail.com>
2021-07-23 14:30:18 +0000

    proquotes

--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -14,7 +14,7 @@ from odoo import models, fields, api
 class order(models.Model):
     _inherit = 'sale.order'
     
-    partner_ids = fields.Many2Many('res.partner')
+    partner_ids = fields.Many2many('res.partner')
     
     def _amount_all(self):
         for order in self:
