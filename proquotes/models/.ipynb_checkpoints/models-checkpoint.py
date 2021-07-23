82179d2d79a777d45b03c6432615e290b885eceb
Ty Cyr <tyjcyr@gmail.com>
2021-07-23 20:25:20 +0000

    proquotes

--- proquotes/models/.ipynb_checkpoints/models-checkpoint.py
+++ proquotes/models/.ipynb_checkpoints/models-checkpoint.py
@@ -120,7 +120,7 @@ class proquotesMail(models.TransientModel):
             result['partner_ids'] = partner_ids
         elif values.get('model') and values.get('res_id'):
             if(values.get('model') == 'sale.order'):
-                result['partner_ids'] = self.env[values.get('model')].browse(values.get('res_id')).partner_id
+                result['partner_ids'] = self.env[values.get('model')].browse(values.get('res_id')).partner_ids
                 raise UserError(_(str(result)))
             doc_name_get = self.env[values.get('model')].browse(values.get('res_id')).name_get()
             result['record_name'] = doc_name_get and doc_name_get[0][1] or ''
