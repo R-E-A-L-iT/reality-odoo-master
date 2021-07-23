127a23647c47cee041a4acbe150147a3bcf509d5
Ty Cyr <tyjcyr@gmail.com>
2021-07-23 19:45:59 +0000

    proquotes

--- proquotes/models/.ipynb_checkpoints/models-checkpoint.py
+++ proquotes/models/.ipynb_checkpoints/models-checkpoint.py
@@ -1,10 +1,14 @@
 #-*- coding: utf-8 -*-
 
+import ast
+import base64
+import re
+
 from datetime import datetime, timedelta
 from functools import partial
 from itertools import groupby
 
-from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo import api, fields, models, SUPERUSER_ID, _, tools
 from odoo.exceptions import AccessError, UserError, ValidationError
 from odoo.tools.misc import formatLang, get_lang
 from odoo.osv import expression
