4e0ae74156f9a01be6bb455db92c84611cf117ee
Ty Cyr <tyjcyr@gmail.com>
2021-07-26 12:33:44 +0000

    proquotes

--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -121,8 +121,9 @@ class proquotesMail(models.TransientModel):
         elif values.get('model') and values.get('res_id'):
             if(values.get('model') == 'sale.order'):
                 partner_ids = self.env[values.get('model')].browse(values.get('res_id')).partner_ids
-                raise UserError(_(str(self.partner_ids)))
+                
                 self.partner_ids = self.env[values.get('model')].browse(values.get('res_id')).partner_id + partner_ids
+                raise UserError(_(str(self.partner_ids)))
                 result['partner_ids'] = partner_ids
             doc_name_get = self.env[values.get('model')].browse(values.get('res_id')).name_get()
             result['record_name'] = doc_name_get and doc_name_get[0][1] or ''
