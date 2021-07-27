28838fe6a1cef0cdec09db8c15acac99de313548
Ty Cyr <tyjcyr@gmail.com>
2021-07-27 20:52:45 +0000

    proportal

---
+++ proportal/controllers/salePortal.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import binascii
+
+from odoo import fields, http, _
+from odoo.exceptions import AccessError, MissingError
+from odoo.http import request
+from odoo.addons.payment.controllers.portal import PaymentProcessing
+from odoo.addons.portal.controllers.mail import _message_post_helper
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
+from odoo.osv import expression
+
+
+class CustomerPortal(CustomerPortal):
+
+    #
+    # Quotations and Sales Orders
+    #
+
+    def portal_my_quotes(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
+        values = self._prepare_portal_layout_values()
+        partner = request.env.user.partner_id.parent_id
+        SaleOrder = request.env['sale.order']
+
+        domain = [
+            ('message_partner_ids', 'child_of', [partner.commercial_partner_id.id]),
+            ('state', 'in', ['sent', 'cancel'])
+        ]
+
+        searchbar_sortings = {
+            'date': {'label': _('Order Date'), 'order': 'date_order desc'},
+            'name': {'label': _('Reference'), 'order': 'name'},
+            'stage': {'label': _('Stage'), 'order': 'state'},
+        }
+
+        # default sortby order
+        if not sortby:
+            sortby = 'date'
+        sort_order = searchbar_sortings[sortby]['order']
+
+        if date_begin and date_end:
+            domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
+
+        # count for pager
+        quotation_count = SaleOrder.search_count(domain)
+        # make pager
+        pager = portal_pager(
+            url="/my/quotes",
+            url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby},
+            total=quotation_count,
+            page=page,
+            step=self._items_per_page
+        )
+        # search the count to display, according to the pager data
+        quotations = SaleOrder.search(domain, order=sort_order, limit=self._items_per_page, offset=pager['offset'])
+        request.session['my_quotations_history'] = quotations.ids[:100]
+
+        values.update({
+            'date': date_begin,
+            'quotations': quotations.sudo(),
+            'page_name': 'quote',
+            'pager': pager,
+            'default_url': '/my/quotes',
+            'searchbar_sortings': searchbar_sortings,
+            'sortby': sortby,
+        })
+        return request.render("sale.portal_my_quotations", values)
+
+    def portal_my_orders(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
+        values = self._prepare_portal_layout_values()
+        partner = request.env.user.partner_id.parent_id
+        SaleOrder = request.env['sale.order']
+
+        domain = [
+            ('message_partner_ids', 'child_of', [partner.commercial_partner_id.id]),
+            ('state', 'in', ['sale', 'done'])
+        ]
+
+        searchbar_sortings = {
+            'date': {'label': _('Order Date'), 'order': 'date_order desc'},
+            'name': {'label': _('Reference'), 'order': 'name'},
+            'stage': {'label': _('Stage'), 'order': 'state'},
+        }
+        # default sortby order
+        if not sortby:
+            sortby = 'date'
+        sort_order = searchbar_sortings[sortby]['order']
+
+        if date_begin and date_end:
+            domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
+
+        # count for pager
+        order_count = SaleOrder.search_count(domain)
+        # pager
+        pager = portal_pager(
+            url="/my/orders",
+            url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby},
+            total=order_count,
+            page=page,
+            step=self._items_per_page
+        )
+        # content according to pager
+        orders = SaleOrder.search(domain, order=sort_order, limit=self._items_per_page, offset=pager['offset'])
+        request.session['my_orders_history'] = orders.ids[:100]
+
+        values.update({
+            'date': date_begin,
+            'orders': orders.sudo(),
+            'page_name': 'order',
+            'pager': pager,
+            'default_url': '/my/orders',
+            'searchbar_sortings': searchbar_sortings,
+            'sortby': sortby,
+        })
+        return request.render("sale.portal_my_orders", values)
