1f1ca95050f864c5f869541e54a645def08e2f90
Ty Cyr <tyjcyr@gmail.com>
2021-07-27 19:02:40 +0000

    proportal

--- proportal/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ proportal/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -14,7 +14,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Sales',
-    'version': '0.1',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'stock_account', 'product', 'purchase', 'stock', 'portal'],
@@ -25,7 +25,7 @@
         'views/partnerView.xml',
         'views/productView.xml',
         'views/stockView.xml',
-        'views/customer_portal.xml'
+        #'views/customer_portal.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
