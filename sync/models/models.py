be2da84a7181333ba45e7c257ff0662716fd97ed
Ty Cyr <tyjcyr@gmail.com>
2021-07-29 20:47:28 +0000

    sync

--- sync/models/models.py
+++ sync/models/models.py
@@ -43,13 +43,13 @@ class sync(models.Model):
     def getCell(self):
         google_web_base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
         access_token = self.get_access_token()
-        apikey = "AIzaSyCeLQNH2fwGQ-yA6jnAS6YGnh4JraJe4Fg"
         sheetID = "1ZoT9NZ1pJEtYWRavImwsYPnccTxGB51e34qcDo9cclU"
         # Copy template in to drive with help of new access token
         #request_url = "https://www.googleapis.com/drive/v2/files/%s/?access_token=%s&alt=json" % (sheetID, access_token)
-        request_url = "https://sheets.googleapis.com/v4/spreadsheets/%s?includeGridData=true&ranges=Sheet1!a1:d2&fields=sheets.data.rowData.values.formattedValue&key%s" % (sheetID, apikey)
+        request_url = "https://sheets.googleapis.com/v4/spreadsheets/%s?includeGridData=true&ranges=Sheet1!a1:d2&fields=sheets.data.rowData.values.formattedValue" % (sheetID, accsess_token)
         headers = {
-            'Accept': 'application/json'
+            'Accept': 'application/json',
+            'Authorization': "Bearer %s" % (accsess_token)
         }
         try:
             res = requests.get(request_url,headers=headers, timeout=TIMEOUT)
