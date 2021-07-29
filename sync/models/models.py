ae7b2246ef49ada9623f614a2f24e882eaddf852
Ty Cyr <tyjcyr@gmail.com>
2021-07-29 13:13:32 +0000

    sync

--- sync/models/models.py
+++ sync/models/models.py
@@ -24,8 +24,21 @@ class sync(models.Model):
     _description = "Sync App"
     def start_sync(self):
         _logger.info("Starting Sync")
-        self.get_access_token()
+        self.
         _logger.info("Ending Sync")
+        
+    def test(self):
+        google_web_base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+        access_token = self.get_access_token()
+        # Copy template in to drive with help of new access token
+        request_url = "https://www.googleapis.com/drive/v2/files/%s?fields=parents/id&access_token=%s" % (template_id, access_token)
+        headers = {"Content-type": "application/x-www-form-urlencoded"}
+        try:
+            req = requests.get(request_url, headers=headers, timeout=TIMEOUT)
+            req.raise_for_status()
+            parents_dict = req.json()
+        except requests.HTTPError:
+            raise UserError(_("The Google Template cannot be found. Maybe it has been deleted."))
         
     def sync_products(self):
         raise UserError("Not Implemented")
