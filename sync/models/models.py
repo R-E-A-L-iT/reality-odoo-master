6c52ac6cee4356df9eb021a4dc523f57eb361d79
Ty Cyr <tyjcyr@gmail.com>
2021-07-29 17:03:56 +0000

    sync

--- sync/models/models.py
+++ sync/models/models.py
@@ -32,7 +32,7 @@ class sync(models.Model):
     
     _inherit = "google.drive.config"
     
-    DatabaseURL = fields.Char(default="https://docs.google.com/spreadsheets/d/14XrvJUaWddKFIEV3eYZvcCtAyzkvdNDswsREgUxiv_A/edit?usp=sharing", string="DatabaseURL")
+    DatabaseURL = fields.Char(default="https://docs.google.com/spreadsheets/d/14XrvJUaWddKFIEV3eYZvcCtAyzkvdNDswsREgUxiv_A/edit?usp=sharing")
     
     _description = "Sync App"
     def start_sync(self):
@@ -44,7 +44,7 @@ class sync(models.Model):
         google_web_base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
         access_token = self.get_access_token()
         # Copy template in to drive with help of new access token
-        request_url = "https://www.googleapis.com/drive/v2/files/%s?fields=parents/id&access_token=%s" % ("14XrvJUaWddKFIEV3eYZvcCtAyzkvdNDswsREgUxiv_A", access_token)
+        request_url = "https://www.googleapis.com/drive/v2/files/%s" % ("14XrvJUaWddKFIEV3eYZvcCtAyzkvdNDswsREgUxiv_A", access_token)
         try:
             res = requests.get(request_url, timeout=TIMEOUT)
             res.raise_for_status()
