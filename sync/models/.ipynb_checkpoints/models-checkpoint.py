4a65e1b1799f719049df925ac589f3a05bf6ced4
Ty Cyr <tyjcyr@gmail.com>
2021-07-29 14:07:24 +0000

    sync

--- sync/models/.ipynb_checkpoints/models-checkpoint.py
+++ sync/models/.ipynb_checkpoints/models-checkpoint.py
@@ -44,7 +44,7 @@ class sync(models.Model):
         google_web_base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
         access_token = self.get_access_token()
         # Copy template in to drive with help of new access token
-        request_url = "https://www.googleapis.com/drive/v2/files/%s?fields=parents/id&access_token=%s" % (template_id, access_token)
+        request_url = "https://www.googleapis.com/drive/v2/files/%s?fields=parents/id&access_token=%s" % ("14XrvJUaWddKFIEV3eYZvcCtAyzkvdNDswsREgUxiv_A", access_token)
         headers = {"Content-type": "application/x-www-form-urlencoded"}
         try:
             req = requests.get(request_url, headers=headers, timeout=TIMEOUT)
