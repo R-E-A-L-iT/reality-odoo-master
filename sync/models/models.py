31371ae8ecb3131c705318d35be62b2eaa5508be
Ty Cyr <tyjcyr@gmail.com>
2021-07-29 19:03:42 +0000

    sync

--- sync/models/models.py
+++ sync/models/models.py
@@ -43,10 +43,10 @@ class sync(models.Model):
     def getCell(self):
         google_web_base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
         access_token = self.get_access_token()
-        sheetID = "14XrvJUaWddKFIEV3eYZvcCtAyzkvdNDswsREgUxiv_A"
+        sheetID = "1ZoT9NZ1pJEtYWRavImwsYPnccTxGB51e34qcDo9cclU"
         # Copy template in to drive with help of new access token
         #request_url = "https://www.googleapis.com/drive/v2/files/%s/?access_token=%s&alt=json" % (sheetID, access_token)
-        request_url = "https://sheets.googleapis.com/v4/spreadsheets/%s?includeGridData=true&ranges=CCP!a1&access_token=%s" % (sheetID, access_token)
+        request_url = "https://sheets.googleapis.com/v4/spreadsheets/%s?includeGridData=true&ranges=Sheet1!a1%3Ad1&fields=sheets.data&access_token=%s" % (sheetID, access_token)
         headers = {
             'Accept': 'application/json'
         }
