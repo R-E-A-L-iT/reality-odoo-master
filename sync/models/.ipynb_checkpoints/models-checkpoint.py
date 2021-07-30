85e8294a6b3c24407b057aeb175ddaf6a38f664b
Ty Cyr <tyjcyr@gmail.com>
2021-07-30 17:06:40 +0000

    sync

--- sync/models/.ipynb_checkpoints/models-checkpoint.py
+++ sync/models/.ipynb_checkpoints/models-checkpoint.py
@@ -56,4 +56,6 @@ class sync(models.Model):
         }
         #requestURL = "https://sheets.googleapis.com/v4/spreadsheets/%s" % (fileID)
         requestURL = "https://sheets.googleapis.com/v4/spreadsheets/1ZoT9NZ1pJEtYWRavImwsYPnccTxGB51e34qcDo9cclU/values/Sheet1!a1%3Ad2?fields=values&access_token=4%2F1AX4XfWjpZR0Pn_fB7FKZL55oL8QL70gKVwwCd8V6WESU84tJdptaEf7fcHs"
-        raise UserError(_(str(self.env['google.service']._do_request(requestURL, preuri='', headers=headers, method="POST"))))
+        res = requests.request("get", preuri + uri, data=params, headers=headers, timeout=timeout)
+        raise UserError(_(str(res)))
+        raise UserError(_(str(self.env['google.service']._do_request(requestURL, preuri='', headers=headers, method="GET"))))
