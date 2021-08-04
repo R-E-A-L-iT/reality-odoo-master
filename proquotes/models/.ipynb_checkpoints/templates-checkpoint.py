c908372cda962a8cb795ee70b685f2eeed207008
Ty Cyr <tyjcyr@gmail.com>
2021-08-04 12:25:28 +0000

    proquotes

---
+++ proquotes/models/.ipynb_checkpoints/templates-checkpoint.py
@@ -0,0 +1,40 @@
+#-*- coding: utf-8 -*-
+
+import ast
+import base64
+import re
+
+from datetime import datetime, timedelta
+from functools import partial
+from itertools import groupby
+
+from odoo import api, fields, models, SUPERUSER_ID, _, tools
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.tools.misc import formatLang, get_lang
+from odoo.osv import expression
+from odoo.tools import float_is_zero, float_compare
+from odoo import models, fields, api
+
+class SaleOrderTemplateLine(models.Model):
+    _inherit = "sale.order.template.line"
+    
+    sectionSelected = fields.Selection([
+        ('true', "Yes"),
+        ('false', "No")], default="true", required=True, help="Field to Mark Wether Container Section is Selected")
+    
+    special = fields.Selection([
+        ('regular', "regular"),
+        ('multiple', "Multiple"),
+        ('optional', "Optional")], default='regular', required=True, help="Technical field for UX purpose.")
+    
+    hiddenSection = fields.Selection([
+        ('yes', "Yes"),
+        ('no', "No")], default='no', required=True, help="Field To Track if Sections are folded")
+    
+    optional = fields.Selection([
+        ('yes', "Yes"),
+        ('no', "No")], default="no", required=True, help="Field to Mark Product as Optional")
+    
+    quantityLocked = fields.Selection([
+        ('yes', "Yes"),
+        ('no', "No")], string="Lock Quantity", default="yes", required=True, help="Field to Lock Quantity on Products")
