086a3449f644f80df4f5a801b073fe3d9f35deb3
Ty Cyr <tyjcyr@gmail.com>
2021-08-05 20:57:35 +0000

    sync

--- sync/models/.ipynb_checkpoints/models-checkpoint.py
+++ sync/models/.ipynb_checkpoints/models-checkpoint.py
@@ -52,14 +52,14 @@ class sync(models.Model):
         google_web_base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
         access_token = self.get_access_token()
         # Copy template in to drive with help of new access token
-        request_url = "https://www.googleapis.com/drive/v2/files/%s?fields=embedLink&access_token=%s" % (template_id, access_token)
+        request_url = "https://spreadsheets.google.com/feeds/cells/%s/1/private/full?access_token=%s" % (template_id, access_token)
         headers = {"Content-type": "application/x-www-form-urlencoded"}
         try:
             req = requests.get(request_url, headers=headers, timeout=TIMEOUT)
             req.raise_for_status()
         except requests.httpsError:
             raise UserError(_("Invalid Document"))
-        
+        raise req.json()
         try:
             src = requests.get(req.json()["embedLink"], headers=headers, timeout=TIMEOUT)
             src.raise_for_status()
