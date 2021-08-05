b41c0d1d1e411979757fdd713f59c8d8c582b195
Ty Cyr <tyjcyr@gmail.com>
2021-08-05 21:45:36 +0000

    sync

--- sync/models/models.py
+++ sync/models/models.py
@@ -59,4 +59,5 @@ class sync(models.Model):
             req.raise_for_status()
         except requests.httpError:
             raise UserError(_("Invalid Document"))
+            
         raise UserError(_(str(req.json())))
