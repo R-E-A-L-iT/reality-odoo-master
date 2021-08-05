0616eac583e0af3314e33d18eb3a5b164cca20a4
Ty Cyr <tyjcyr@gmail.com>
2021-08-05 21:38:44 +0000

    sync

--- sync/models/.ipynb_checkpoints/models-checkpoint.py
+++ sync/models/.ipynb_checkpoints/models-checkpoint.py
@@ -52,7 +52,7 @@ class sync(models.Model):
         google_web_base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
         access_token = self.get_access_token()
         # Copy template in to drive with help of new access token
-        request_url = "https://spreadsheets.google.com/feeds/cells/%s/1/private/full/R3C1?access_token=%s&alt=json&fields=entry" % (template_id, access_token)
+        request_url = "https://spreadsheets.google.com/feeds/cells/%s/1/private/full/R3C1?access_token=%s&alt=json&field=entry" % (template_id, access_token)
         headers = {"Content-type": "application/x-www-form-urlencoded"}
         try:
             req = requests.get(request_url, headers=headers, timeout=TIMEOUT)
