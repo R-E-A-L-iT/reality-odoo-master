e687dae3072c9f10886177de300f146fce62e539
Ty Cyr <tyjcyr@gmail.com>
2021-08-05 18:09:47 +0000

    sync

--- sync/models/.ipynb_checkpoints/models-checkpoint.py
+++ sync/models/.ipynb_checkpoints/models-checkpoint.py
@@ -55,7 +55,8 @@ class sync(models.Model):
         request_url = "https://www.googleapis.com/drive/v2/files/%s/id?access_token=%s" % (template_id, access_token)
         headers = {"Content-type": "application/x-www-form-urlencoded"}
         req = requests.get(request_url, headers=headers, timeout=TIMEOUT)
-        raise UserError(_(req))
+        req.raise_for_status()
+        raise UserError(_(req.json()))
 #        DatabaseURL = fields.Char(default="")
 #        _logger.info("Start Sync")
 #        fileID = "1ZoT9NZ1pJEtYWRavImwsYPnccTxGB51e34qcDo9cclU"
