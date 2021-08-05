881296e0b996fd828d4f9a860ef752fa55b1fa1c
Ty Cyr <tyjcyr@gmail.com>
2021-08-05 21:03:59 +0000

    sync

--- sync/models/.ipynb_checkpoints/models-checkpoint.py
+++ sync/models/.ipynb_checkpoints/models-checkpoint.py
@@ -59,7 +59,7 @@ class sync(models.Model):
             req.raise_for_status()
         except requests.httpsError:
             raise UserError(_("Invalid Document"))
-        raise req.json()
+        raise UserError(_(str(req.text)))
         try:
             src = requests.get(req.json()["embedLink"], headers=headers, timeout=TIMEOUT)
             src.raise_for_status()
