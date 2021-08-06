61f40d585955a82892787388029e0b2d9853b6c9
Ty Cyr <tyjcyr@gmail.com>
2021-08-06 19:14:22 +0000

    sync

--- sync/models/sync.py
+++ sync/models/sync.py
@@ -114,8 +114,8 @@ class sync(models.Model):
         company.website = sheet[i * sheetWidth + 2]["content"]["$t"]
         company.street = sheet[i * sheetWidth + 3]["content"]["$t"]
         company.city = sheet[i * sheetWidth + 4]["content"]["$t"]
-        company.state_id = int(self.env['res.country.state'].search([('code','=',sheet[i * sheetWidth + 5]["content"]["$t"])])[0])
-        company.country_id = int(self.env['res.country'].search([('name','=',sheet[i * sheetWidth + 6]["content"]["$t"])])[0])
+        #company.state_id = int(self.env['res.country.state'].search([('code','=',sheet[i * sheetWidth + 5]["content"]["$t"])])[0])
+        #company.country_id = int(self.env['res.country'].search([('name','=',sheet[i * sheetWidth + 6]["content"]["$t"])])[0])
         company.zip = sheet[i * sheetWidth + 7]["content"]["$t"]
         company.lang = sheet[i * sheetWidth + 8]["content"]["$t"]
         company.email = sheet[i * sheetWidth + 9]["content"]["$t"]
