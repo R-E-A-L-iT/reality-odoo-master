59b3cd4ba8b33e002e51eb02c8c839f02da20f9f
Ty Cyr <tyjcyr@gmail.com>
2021-08-06 18:48:09 +0000

    sync_proquotes

--- sync/models/sync.py
+++ sync/models/sync.py
@@ -114,7 +114,7 @@ class sync(models.Model):
         company.website = sheet[i * sheetWidth + 2]["content"]["$t"]
         company.street = sheet[i * sheetWidth + 3]["content"]["$t"]
         company.city = sheet[i * sheetWidth + 4]["content"]["$t"]
-        company.state_id = sheet[i * sheetWidth + 5]["content"]["$t"]
+        company.state_id = self.env[].search([('code','=',sheet[i * sheetWidth + 5]["content"]["$t"])])
         company.country_id = int(sheet[i * sheetWidth + 6]["content"]["$t"])
         company.zip = sheet[i * sheetWidth + 7]["content"]["$t"]
         company.lang = sheet[i * sheetWidth + 8]["content"]["$t"]
