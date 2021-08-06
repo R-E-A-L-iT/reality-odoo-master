436c0644985d332bff555c4945762e702b924e7c
Ty Cyr <tyjcyr@gmail.com>
2021-08-06 15:55:21 +0000

    sync

--- sync/models/sync.py
+++ sync/models/sync.py
@@ -97,7 +97,9 @@ class sync(models.Model):
             if(str(sheet[i * sheetWidth + (sheetWidth - 1)]["content"]["$t"]) == "FALSE"):
                 break;
             external_id = str(sheet[i * sheetWidth + 12]["content"]["$t"])
-            company_id = int(self.env['ir.model.data'].search([('name','=', external_id)]).res_id)
+            company_id = self.env['ir.model.data'].search([('name','=', external_id)])
+            if(len(company_id.res_id) > 1):
+                raise UserError(_(str(company_id)))
             company = self.env['res.partner'].browse(company_id)
             company.name = str(sheet[i * sheetWidth + 0]["content"]["$t"])
             i = i + 1
