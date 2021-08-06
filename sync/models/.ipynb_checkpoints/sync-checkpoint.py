83ae2993e03ec9a627130bd7c1c3b1571f74df1a
Ty Cyr <tyjcyr@gmail.com>
2021-08-06 19:27:08 +0000

    sync

--- sync/models/.ipynb_checkpoints/sync-checkpoint.py
+++ sync/models/.ipynb_checkpoints/sync-checkpoint.py
@@ -112,13 +112,13 @@ class sync(models.Model):
         ext.res_id = company.id
         
         company.phone = sheet[i * sheetWidth + 1]["content"]["$t"]
-        #company.website = sheet[i * sheetWidth + 2]["content"]["$t"]
-        #company.street = sheet[i * sheetWidth + 3]["content"]["$t"]
-        #company.city = sheet[i * sheetWidth + 4]["content"]["$t"]
-        #company.state_id = int(self.env['res.country.state'].search([('code','=',sheet[i * sheetWidth + 5]["content"]["$t"])])[0])
-        #company.country_id = int(self.env['res.country'].search([('name','=',sheet[i * sheetWidth + 6]["content"]["$t"])])[0])
+        company.website = sheet[i * sheetWidth + 2]["content"]["$t"]
+        company.street = sheet[i * sheetWidth + 3]["content"]["$t"]
+        company.city = sheet[i * sheetWidth + 4]["content"]["$t"]
+        company.state_id = int(self.env['res.country.state'].search([('code','=',sheet[i * sheetWidth + 5]["content"]["$t"])])[0])
+        company.country_id = int(self.env['res.country'].search([('name','=',sheet[i * sheetWidth + 6]["content"]["$t"])])[0])
         company.zip = sheet[i * sheetWidth + 7]["content"]["$t"]
         company.lang = sheet[i * sheetWidth + 8]["content"]["$t"]
-        #company.email = sheet[i * sheetWidth + 9]["content"]["$t"]
-        company.property_product_pricelist = sheet[i * sheetWidth + 10]["content"]["$t"]
+        company.email = sheet[i * sheetWidth + 9]["content"]["$t"]
+        #company.property_product_pricelist = sheet[i * sheetWidth + 10]["content"]["$t"]
         company.is_company = True
