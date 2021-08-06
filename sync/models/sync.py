75092b75f98580a41156cb959853f991363c60be
Ty Cyr <tyjcyr@gmail.com>
2021-08-06 17:56:44 +0000

    sync

--- sync/models/sync.py
+++ sync/models/sync.py
@@ -105,8 +105,7 @@ class sync(models.Model):
             i = i + 1
             
     def updateCompany(self, sheet, id, sheetWidth, i):
-        if(self.env['res.partner'].browse(id)[0].country_id != 38):
-            raise UserError(_(str(self.env['res.partner'].browse(id)[0].country_id)))
+            logger.info(str(self.env['res.partner'].browse(id)[0].country_id))
         
     def createCompany(self, sheet, external_id, sheetWidth, i):
         ext = self.env['ir.model.data'].create({'name': external_id, 'model':"res.partner"})[0]
