8ec9180597c0abe2b780e2b1befcd9588b688572
Ty Cyr <tyjcyr@gmail.com>
2021-08-06 16:00:24 +0000

    sync

--- sync/models/.ipynb_checkpoints/sync-checkpoint.py
+++ sync/models/.ipynb_checkpoints/sync-checkpoint.py
@@ -99,7 +99,8 @@ class sync(models.Model):
             external_id = str(sheet[i * sheetWidth + 12]["content"]["$t"])
             company_id = self.env['ir.model.data'].search([('name','=', external_id)])
             if(len(company_id) > 1):
-                raise UserError(_(str(company_id)))
+                raise UserError(_(str(company_id[0].res_id) + str(company_id[1].res_id)))
+                
             company = self.env['res.partner'].browse(company_id.res_id)
             company.name = str(sheet[i * sheetWidth + 0]["content"]["$t"])
             i = i + 1
