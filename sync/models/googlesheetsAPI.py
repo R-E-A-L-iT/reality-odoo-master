9c17fc38de656cf502f86b9a1e98941b11fe4a60
Ty Cyr <tyjcyr@gmail.com>
2021-09-04 21:09:07 +0000

    sync

--- sync/models/googlesheetsAPI.py
+++ sync/models/googlesheetsAPI.py
@@ -17,6 +17,6 @@ GOOGLE_AUTH_ENDPOINT = 'https://accounts.google.com/o/oauth2/auth'
 GOOGLE_TOKEN_ENDPOINT = 'https://accounts.google.com/o/oauth2/token'
 GOOGLE_API_BASE_URL = 'https://www.googleapis.com'
 
-class googlesheetsAPI(models.AbstractModel):
+class googlesheetsAPI(models.Model):
     _name = "sync.sheets"
     _inherit = "google.drive.config"
