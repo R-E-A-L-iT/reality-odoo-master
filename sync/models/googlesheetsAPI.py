e0e6bd6928ae2669493f0f440306e286aab7a5d9
Ty Cyr <tyjcyr@gmail.com>
2021-09-04 21:32:01 +0000

    sync

--- sync/models/googlesheetsAPI.py
+++ sync/models/googlesheetsAPI.py
@@ -8,6 +8,7 @@ import requests
 from werkzeug import urls
 
 from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
@@ -21,3 +22,31 @@ class googlesheetsAPI(models.AbstractModel):
     _name = "sync.sheets"
     _inherit = "google.service"
     _description = "Googlesheets API Handler"
+    
+    def _get_google_tokens(self, authorize_code, service):
+        """ Call Google API to exchange authorization code against token, with POST request, to
+            not be redirected.
+        """
+        get_param = self.env['ir.config_parameter'].sudo().get_param
+        base_url = get_param('web.base.url', default='http://www.odoo.com?NoBaseUrl')
+        client_id = get_param('google_%s_client_id' % (service,), default=False)
+        client_secret = get_param('google_%s_client_secret' % (service,), default=False)
+
+        headers = {"content-type": "application/x-www-form-urlencoded"}
+        data = {
+            'code': authorize_code,
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'grant_type': 'authorization_code',
+            'redirect_uri': base_url + '/google_account/authentication'
+        }
+        try:
+            dummy, response, dummy = self._do_request(GOOGLE_TOKEN_ENDPOINT, params=data, headers=headers, method='POST', preuri='')
+            access_token = response.get('access_token')
+            refresh_token = response.get('refresh_token')
+            ttl = response.get('expires_in')
+            return access_token, refresh_token, ttl
+        except requests.HTTPError:
+            raise UserError(_(req.json()))
+            error_msg = _("Something went wrong during your token generation. Maybe your Authorization Code is invalid")
+            raise self.env['res.config.settings'].get_config_warning(error_msg)
