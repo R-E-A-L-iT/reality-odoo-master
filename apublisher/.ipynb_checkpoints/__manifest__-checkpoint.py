6f17f6a1fa6c243a7516f196547019d0970eb716
‘Ezekiel <ezekiel@deblois.me>
2021-09-16 20:48:22 +0000

    apublisher

--- apublisher/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ apublisher/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -16,7 +16,7 @@
     # for the full list
     'category': 'Technical',
 
-    'version': '0.2',
+    'version': '0.3',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'website', 'product', 'portal'],
