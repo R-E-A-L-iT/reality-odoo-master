91dbe58d9686c9cd0be644aadc5ae87cb5551cc1
Ty Cyr <tyjcyr@gmail.com>
2021-11-12 15:09:18 +0000

    proquotes

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -20,7 +20,7 @@
     # any module necessary for this one to work correctly
     'depends': ['base','web','mail', 'account', 'proportal', 'product' ,'website', 'sale_management', 'sale', 'digest', 'portal'],
     
-    'version': '0.9',
+    'version': '0.1',
     
     # always loaded
     'data': [
