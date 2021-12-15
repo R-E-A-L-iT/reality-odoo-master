652a9f35fe17136fa59cc2173b07db5f975fda89
Ty Cyr <tyjcyr@gmail.com>
2021-12-15 17:27:35 +0000

    proquotes

--- proquotes/models/.ipynb_checkpoints/models-checkpoint.py
+++ proquotes/models/.ipynb_checkpoints/models-checkpoint.py
@@ -27,7 +27,7 @@ class order(models.Model):
     
     customer_po_number = fields.Char(string="PO Number")
     
-#     company_id = fields.Char(related="company_id")
+    company_name = fields.Char(related="company_id")
     
     footer = fields.Selection([
         ('Abtech_Atlantic_Ryan', "Abtech_Atlantic_Ryan"),
@@ -36,7 +36,7 @@ class order(models.Model):
         ('Geoplus_Canada', "Geoplus_Canada"),
         ('Geoplus_America', "Geoplus_America"),
         ('Leica_Various_Ali', "Leica_Various_Ali"),
-#         ('company_name', str(company_id)),
+        ('company_name', str(company_name)),
         ('Reality_Various_Derek', "Reality_Various_Derek")], default='Reality_Various_Derek', required=True, help="Footer selection field")
     header = fields.Selection([
         ('ChurchXRAY', "ChurchXRAY"),
