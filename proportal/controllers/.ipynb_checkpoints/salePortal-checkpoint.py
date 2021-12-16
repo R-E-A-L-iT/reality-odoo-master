4f82083c356d8961f4d9f0722050f131cc5662b6
Ty Cyr <tyjcyr@gmail.com>
2021-12-16 13:25:06 +0000

    proportal

--- proportal/controllers/.ipynb_checkpoints/salePortal-checkpoint.py
+++ proportal/controllers/.ipynb_checkpoints/salePortal-checkpoint.py
@@ -7,7 +7,7 @@ from odoo import fields, http, _
 from odoo.exceptions import AccessError, MissingError, UserError
 from odoo.http import request
 from odoo.addons.portal.controllers.mail import _message_post_helper
-#from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
 from odoo.addons.sale.controllers.portal import CustomerPortal as sourcePortal
 from odoo.osv import expression
 
