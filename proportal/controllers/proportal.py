fec8d9e41bc7a31c7333a27b0343d411d109ec3a
Ty Cyr <tyjcyr@gmail.com>
2022-01-18 02:46:27 +0000

    proportal

--- proportal/controllers/proportal.py
+++ proportal/controllers/proportal.py
@@ -20,3 +20,53 @@ class CustomerPortal(CustomerPortal):
             'company': company
         }
         return request.render("proportal.portal_products", values)
+    
+    def portal_my_quotes(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
+        x = 5 / (3 - 3)
+        values = self._prepare_portal_layout_values()
+        partner = request.env.user.partner_id
+        SaleOrder = request.env['sale.order']
+
+        domain = [
+            ('message_partner_ids', 'child_of', [partner.commercial_partner_id.id]),
+            ('state', 'in', ['sent', 'cancel'])
+        ]
+
+        searchbar_sortings = {
+            'date': {'label': _('Order Date'), 'order': 'date_order desc'},
+            'name': {'label': _('Reference'), 'order': 'name'},
+            'stage': {'label': _('Stage'), 'order': 'state'},
+        }
+
+        # default sortby order
+        if not sortby:
+            sortby = 'date'
+        sort_order = searchbar_sortings[sortby]['order']
+
+        if date_begin and date_end:
+            domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
+
+        # count for pager
+        quotation_count = SaleOrder.search_count(domain)
+        # make pager
+        pager = portal_pager(
+            url="/my/quotes",
+            url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby},
+            total=quotation_count,
+            page=page,
+            step=self._items_per_page
+        )
+        # search the count to display, according to the pager data
+        quotations = SaleOrder.search(domain, order=sort_order, limit=self._items_per_page, offset=pager['offset'])
+        request.session['my_quotations_history'] = quotations.ids[:100]
+
+        values.update({
+            'date': date_begin,
+            'quotations': quotations.sudo(),
+            'page_name': 'quote',
+            'pager': pager,
+            'default_url': '/my/quotes',
+            'searchbar_sortings': searchbar_sortings,
+            'sortby': sortby,
+        })
+        return request.render("sale.portal_my_quotations", values)
