a241d7edb1b798d9fdefb757428ea39d159382b9
Braincrew Apps <info@braincrewapps.com>
2022-03-02 20:18:29 +0530

    Trying to resolve multi-company quote issue

--- proportal/controllers/salePortal.py
+++ proportal/controllers/salePortal.py
@@ -65,7 +65,7 @@ class CustomerPortal(sourcePortal):
     def portal_my_quotes(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
         values = self._prepare_portal_layout_values()
         partner = request.env.user.partner_id
-        SaleOrder = request.env['sale.order']
+        SaleOrder = request.env['sale.order'].sudo()
         
         domain = [
             ('message_partner_ids', 'child_of', [partner.commercial_partner_id.id]),
