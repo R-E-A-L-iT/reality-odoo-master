fb4eb57dd2e24efb636c03bcf04f24b64625dc8e
Ty Cyr <ty@r-e-a-l.it>
2022-06-29 10:33:12 -0400

    Style Change

--- proportal/__manifest__.py
+++ proportal/__manifest__.py
@@ -1,52 +1,52 @@
 {
-    'name': "ProPortal",
+	'name': "ProPortal",
 
-    'summary': """
-        Portal Upgrade Module that adds Advanced Features""",
+	'summary': """
+		Portal Upgrade Module that adds Advanced Features""",
 
-    'description': """
-        Module that allows expands Customer Portal
-    """,
+	'description': """
+		Module that allows expands Customer Portal
+	""",
 
-    'author': "Ty Cyr",
-    
-    'license': "LGPL-3",
+	'author': "Ty Cyr",
+	
+	'license': "LGPL-3",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Sales',
+	# Categories can be used to filter modules in modules listing
+	# Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+	# for the full list
+	'category': 'Sales',
 
-    'version': '0.4',
+	'version': '0.6',
 
-    # any module necessary for this one to work correctly
-    'depends': ['base', 'website', 'stock_account', 'product', 'purchase', 'stock', 'portal', 'website_sale'],
-    
-    'assets': {
-        'web.assets_frontend': [
-            'proportal/static/src/CSS/homepage.css',
-            'proportal/static/src/CSS/projectPortal.css',
-            'proportal/static/src/CSS/productMiniMenu.css',
-            'proportal/static/src/CSS/productWebsite.css',
-            'proportal/static/src/CSS/header.css',
-            'proportal/static/src/CSS/footer.css',
-        ]
-    },
-    
-    # always loaded
-    'data': [
-        #'security/ir.model.access.csv',
-        'views/partnerView.xml',
-        #'views/portalAccess.xml',
-        'views/stockView.xml',
-        'views/productView.xml',
-        'views/customer_portal.xml',
-        'views/portalProject.xml',
-        'views/navButton.xml',
-        'views/navbarCustom.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+	# any module necessary for this one to work correctly
+	'depends': ['base', 'website', 'stock_account', 'product', 'purchase', 'stock', 'portal', 'website_sale'],
+	
+	'assets': {
+		'web.assets_frontend': [
+			'proportal/static/src/CSS/homepage.css',
+			'proportal/static/src/CSS/projectPortal.css',
+			'proportal/static/src/CSS/productMiniMenu.css',
+			'proportal/static/src/CSS/productWebsite.css',
+			'proportal/static/src/CSS/header.css',
+			'proportal/static/src/CSS/footer.css',
+		]
+	},
+	
+	# always loaded
+	'data': [
+		#'security/ir.model.access.csv',
+		'views/partnerView.xml',
+		#'views/portalAccess.xml',
+		'views/stockView.xml',
+		'views/productView.xml',
+		'views/customer_portal.xml',
+		'views/portalProject.xml',
+		'views/navButton.xml',
+		'views/navbarCustom.xml',
+	],
+	# only loaded in demonstration mode
+	'demo': [
+		'demo/demo.xml',
+	],
 }
