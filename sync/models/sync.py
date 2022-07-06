e376ab52696e1f16d71e6293440a3626d3906348
Ty Cyr <ty@r-e-a-l.it>
2022-07-06 15:59:51 -0400

    Sync Update

--- sync/models/sync.py
+++ sync/models/sync.py
@@ -1036,6 +1036,11 @@ class sync(models.Model):
 				msg = self.buildMSG(msg, sheet, sheetWidth, i)
 				i = i + 1
 				continue
+
+			if(not sheet[i][columns["valid"]] == "TRUE"):
+				msg = self.buildMSG(msg, sheet, sheetWidth, i)
+				i = i + 1
+				continue
 			
 			try:
 				external_id = str(sheet[i][columns["id"]])
@@ -1046,6 +1051,7 @@ class sync(models.Model):
 				else:
 					msg = self.buildMSG(msg, sheet, sheetWidth, i)
 					_logger.info("Page Not Created")
+					return True, msg
 				i = i + 1
 			except Exception as e:
 				_logger.info(e)
