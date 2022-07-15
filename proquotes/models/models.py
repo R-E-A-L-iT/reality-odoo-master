bc06ea8377dfc6f2b8e55f8af7fb34e37ab192c6
Ty Cyr <ty@r-e-a-l.it>
2022-07-15 10:58:41 -0400

    Quote Update

--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -30,9 +30,9 @@ class order(models.Model):
     company_name = fields.Char(related="company_id.name", string="company_name", required=True)
     
     footer = fields.Selection([
-        ('Abtech_Atlantic_Ryan', "ABtechFooter_Atlantic_Ryan"),
-        ('Abtech_Ontario_Phil', "ABtechFooter_Ontario_Phil"),
-        ('Abtech_Quebec_Derek', "ABtechFooter_Quebec_Derek"),
+        ('ABtechFooter_Atlantic_Ryan', "Abtech_Atlantic_Ryan"),
+        ('ABtechFooter_Ontario_Phil', "Abtech_Ontario_Phil"),
+        ('ABtechFooter_Quebec_Derek', "Abtech_Quebec_Derek"),
         ('Geoplus_Canada', "Geoplus_Canada"),
         ('Geoplus_America', "Geoplus_America"),
         ('Leica_Various_Ali', "Leica_Various_Ali"),
