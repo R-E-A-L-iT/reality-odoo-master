e6f59a8b804c16848edb533c4f5708eceae9a9b5
Ty Cyr <ty@r-e-a-l.it>
2022-07-20 12:34:48 -0400

    Rental

--- proquotes/static/src/JS/rental.js
+++ proquotes/static/src/JS/rental.js
@@ -4,9 +4,18 @@ odoo.define("proquotes.rental", function (require) {
 
 	publicWidget.registry.rental = publicWidget.Widget.extend({
 		selector: ".o_portal_sale_sidebar",
+		events: {
+			"change #street": "_street",
+		},
+
 		async start() {
 			await this._super(...arguments);
 			this.orderDetail = this.$el.find("table#sales_order_table").data();
+		},
+		_street: function (ev) {
+			var target = ev.currentTarget;
+			var street = target.value;
+			console.log(street);
 		},
 	});
 });
