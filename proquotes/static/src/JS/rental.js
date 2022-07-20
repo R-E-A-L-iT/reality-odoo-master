49530b6f0777ff1235f75bec10a376a5d052e1ae
Ty Cyr <ty@r-e-a-l.it>
2022-07-20 12:31:40 -0400

    Rental JS

--- proquotes/static/src/JS/rental.js
+++ proquotes/static/src/JS/rental.js
@@ -2,5 +2,11 @@ odoo.define("proquotes.rental", function (require) {
 	"use strict";
 	var publicWidget = require("web.public.widget");
 
-	publicWidget.registry.rental = publicWidget.Widget.extend({});
+	publicWidget.registry.rental = publicWidget.Widget.extend({
+		selector: ".o_portal_sale_sidebar",
+		async start() {
+			await this._super(...arguments);
+			this.orderDetail = this.$el.find("table#sales_order_table").data();
+		},
+	});
 });
