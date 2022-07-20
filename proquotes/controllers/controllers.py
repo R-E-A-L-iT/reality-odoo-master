7a3a8ce54237da201b713568b2e3a0670cf58475
Ty Cyr <ty@r-e-a-l.it>
2022-07-20 15:54:53 -0400

    PoFile

--- proquotes/controllers/controllers.py
+++ proquotes/controllers/controllers.py
@@ -7,11 +7,12 @@ from odoo import fields, http, _
 from odoo.exceptions import AccessError, MissingError, UserError
 from odoo.http import request
 from odoo.addons.portal.controllers.mail import _message_post_helper
-from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
+from odoo.addons.portal.controllers.portal import CustomerPortal as cPortal
+from odoo.addons.portal.controllers.portal import pager as portal_pager
 from odoo.osv import expression
 
 
-class QuoteCustomerPortal(portal_pager):
+class QuoteCustomerPortal(cPortal):
 
     @http.route(["/my/orders/<int:order_id>/ponumber"], type='json', auth="public", website=True)
     def poNumber(self, order_id, ponumber, access_token=None, **post):
