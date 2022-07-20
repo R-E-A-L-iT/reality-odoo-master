6a0c638332ad0c94c2286442d74980531f7745e4
Ty Cyr <ty@r-e-a-l.it>
2022-07-20 12:23:18 -0400

    Code Style

--- proquotes/static/src/JS/rental.js
+++ proquotes/static/src/JS/rental.js
@@ -1,6 +1,6 @@
-odoo.define('proquotes.rental', function (require) {
-	'use strict';
-	var publicWidget = require('web.public.widget')
+odoo.define("proquotes.rental", function (require) {
+	"use strict";
+	var publicWidget = require("web.public.widget");
 
-	publicWidget.registry.rental = publicWidget.Widget.extend({})
+	publicWidget.registry.rental = publicWidget.Widget.extend({});
 });
