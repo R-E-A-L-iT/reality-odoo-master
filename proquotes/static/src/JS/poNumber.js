09bb803d55afda00327da361290e46705e1c2902
Ty Cyr <ty@r-e-a-l.it>
2022-07-20 15:58:24 -0400

    PoFile

--- proquotes/static/src/JS/poNumber.js
+++ proquotes/static/src/JS/poNumber.js
@@ -30,7 +30,7 @@ odoo.define("proquotes.ponumber", function (require) {
 			var target = ev.currentTarget;
 			var poFile = target.files;
 			return this._rpc({
-				route: "my/orders/" + this.orderDetail.orderId + "/poFile",
+				route: "/my/orders/" + this.orderDetail.orderId + "/poFile",
 				params: {
 					access_token: this.orderDetail.token,
 					poFile: poFile,
