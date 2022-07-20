0d26eb88d90a216282d38fbe91b2385872ec25a2
Ty Cyr <ty@r-e-a-l.it>
2022-07-20 16:17:35 -0400

    Po File

--- proquotes/static/src/JS/poNumber.js
+++ proquotes/static/src/JS/poNumber.js
@@ -34,7 +34,7 @@ odoo.define("proquotes.ponumber", function (require) {
 				route: "/my/orders/" + this.orderDetail.orderId + "/poFile",
 				params: {
 					access_token: this.orderDetail.token,
-					poFile: reader.readAsBinaryString(poFile[0]),
+					pofile: reader.readAsBinaryString(poFile[0]),
 				},
 			});
 		},
