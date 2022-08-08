c102e9a4f7ed8adf5e8bf6572ca31d6c6a6edaf2
Ty Cyr <ty@r-e-a-l.it>
2022-08-08 18:01:52 -0400

    PoFile

--- proquotes/static/src/JS/poNumber.js
+++ proquotes/static/src/JS/poNumber.js
@@ -45,7 +45,7 @@ odoo.define("proquotes.ponumber", function (require) {
 				});
 			});
 			if (poFile[0] != undefined) {
-				reader.readAsBinaryString(poFile[0]);
+				reader.readAsBinaryString(poFile[0], "utf-8");
 			} else {
 				return this._rpc({
 					route: "/my/orders/" + this.orderDetail.orderId + "/poFile",
