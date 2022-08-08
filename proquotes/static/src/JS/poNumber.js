69a70a7309f8bbd7f243b277b3749e308321f201
Ty Cyr <ty@r-e-a-l.it>
2022-08-08 13:15:20 -0400

    PoFile

--- proquotes/static/src/JS/poNumber.js
+++ proquotes/static/src/JS/poNumber.js
@@ -34,7 +34,6 @@ odoo.define("proquotes.ponumber", function (require) {
 			var reader = new FileReader();
 			reader.addEventListener("load", (event) => {
 				console.log("File Read");
-				console.log(reader.result);
 				return this._rpc({
 					route: "/my/orders/" + this.orderDetail.orderId + "/poFile",
 					params: {
