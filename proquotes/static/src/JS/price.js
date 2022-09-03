8f53e74aa47d1e405f9280c9728fc8453fc2a46c
Ty-J-Cyr <ty@r-e-a-l.it>
2022-09-03 13:00:21 -0400

    Approve

--- proquotes/static/src/JS/price.js
+++ proquotes/static/src/JS/price.js
@@ -27,7 +27,7 @@ odoo.define("proquotes.price", function (require) {
 			var linkItems = "";
 			for (var i = 0; i < items.length; i++) {
 				linkItems = linkItems + items[i].innerHTML;
-				if (i == 12) {
+				if (i == 13) {
 					break;
 				}
 				if (i < items.length - 1) {
