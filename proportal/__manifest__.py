f8d6087947203c492fe0cbadd752f56b5ef1f8ef
Ty-J-Cyr <ty@r-e-a-l.it>
2022-09-09 15:02:58 -0400

    Style

--- proportal/__manifest__.py
+++ proportal/__manifest__.py
@@ -17,7 +17,7 @@
     # for the full list
     'category': 'Sales',
 
-    'version': '0.9',
+    'version': '0.10',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'website', 'stock_account', 'product', 'purchase', 'stock', 'portal', 'website_sale'],
