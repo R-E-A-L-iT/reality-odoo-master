8250c8b534ba239a75310215e29de2068c41d051
Ty-J-Cyr <ty@r-e-a-l.it>
2022-09-09 13:22:36 -0400

    Cart

--- proportal/controllers/cart.py
+++ proportal/controllers/cart.py
@@ -18,8 +18,7 @@ _logger = logging.getLogger(__name__)
 class CustomerCart(CP):
     @http.route(['/shop/add-to-cart', '/shop/add-to-cart/<int:sku>'], type='http', auth="public", website=True)
     def add_to_cart(self, sku):
-        qty = 1
-        _logger.info("Add To Cart: " + str(sku))
+        _logger.info("Add To Cart" + str(sku))
         cr, uid, context, registry = request.cr, request.uid, request.context, request.registry
 
         # Check user input
@@ -29,8 +28,10 @@ class CustomerCart(CP):
         except:
             product_id = None
 
+        if product_id:
+            product_id = registry["product.product"].browse(
+                cr, uid, [product_id])
         _logger.info(product_id)
-        _logger.info(product_id.id)
 
         # Is the product ok
         if product_id and product_id.sale_ok and product_id.website_published:
