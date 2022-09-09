46f6cabd8719de9bbe2e4a2ee49b5f4b32286838
Ty-J-Cyr <ty@r-e-a-l.it>
2022-09-09 11:44:15 -0400

    Update

--- proportal/controllers/proportal.py
+++ proportal/controllers/proportal.py
@@ -7,12 +7,13 @@ from odoo import fields, http, _
 from odoo.exceptions import AccessError, MissingError, UserError
 from odoo.http import request
 from odoo.addons.portal.controllers.mail import _message_post_helper
-from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
+from odoo.addons.portal.controllers.portal import CustomerPortal as CP
+from odoo.addons.portal.controllers.portal import pager as portal_pager, get_records_pager
 from odoo.osv import expression
 
 
-class CustomerPortal(CustomerPortal):
-      
+class CustomerPortal(CP):
+
     @http.route(['/my/products', '/my/products/page/<int:page>'], type='http', auth="user", website=True)
     def products(self):
         company = request.env.user.partner_id.parent_id
@@ -20,5 +21,3 @@ class CustomerPortal(CustomerPortal):
             'company': company
         }
         return request.render("proportal.portal_products", values)
-    
-   
