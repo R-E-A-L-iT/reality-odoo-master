98fdc90b45a7105c5640ee0ff9cfec3bf36a35ed
Ty-J-Cyr <ty@r-e-a-l.it>
2022-09-09 13:05:39 -0400

    Cart

--- proportal/controllers/cart.py
+++ proportal/controllers/cart.py
@@ -29,6 +29,8 @@ class CustomerCart(CP):
         except:
             product_id = None
 
+            _logger.info(product_id)
+
         # Is the product ok
         if product_id and product_id.sale_ok and product_id.website_published:
             # Get the cart-sale-order
