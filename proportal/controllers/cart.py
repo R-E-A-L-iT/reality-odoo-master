06a1188e0a269f8a981190863541dbc189356fda
Ty-J-Cyr <ty@r-e-a-l.it>
2022-09-09 13:27:14 -0400

    Cart

--- proportal/controllers/cart.py
+++ proportal/controllers/cart.py
@@ -18,6 +18,7 @@ _logger = logging.getLogger(__name__)
 class CustomerCart(CP):
     @http.route(['/shop/add-to-cart', '/shop/add-to-cart/<int:sku>'], type='http', auth="public", website=True)
     def add_to_cart(self, sku):
+        qty = 1
         _logger.info("Add To Cart" + str(sku))
         cr, uid, context, registry = request.cr, request.uid, request.context, request.registry
 
