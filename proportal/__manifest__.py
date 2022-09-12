90125c0732a7d526af7d45ccfa1a5657a5b9a6a9
Ty-J-Cyr <ty@r-e-a-l.it>
2022-09-12 15:06:12 -0400

    Rebuild

--- proportal/__manifest__.py
+++ proportal/__manifest__.py
@@ -17,7 +17,7 @@
     # for the full list
     'category': 'Sales',
 
-    'version': '0.2',
+    'version': '0.3',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'website', 'stock_account', 'product', 'purchase', 'stock', 'portal', 'website_sale'],
@@ -30,8 +30,8 @@
                 'proportal/static/src/CSS/customWebsiteStyle.css',
                 'proportal/static/src/CSS/header.css',
                 'proportal/static/src/CSS/footer.css',
-                'proportal/static/src/CSS/mobileWebsite.css'
-                # 'proportal/static/src/CSS/cart.css'
+                'proportal/static/src/CSS/mobileWebsite.css',
+                'proportal/static/src/CSS/cart.css'
             ]
     },
 
