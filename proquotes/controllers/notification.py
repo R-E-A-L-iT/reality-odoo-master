92257a60b0df9d075a1f3b11abf8ae40419cda60
Ty-J-Cyr <ty@r-e-a-l.it>
2022-09-18 07:07:26 -0400

    Test

--- proquotes/controllers/notification.py
+++ proquotes/controllers/notification.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-class notification:
+class notifications:
     def something():
         x = 1 + 1
