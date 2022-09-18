7a434846786fe5f631cb16448678a8f6118dfce6
Ty-J-Cyr <ty@r-e-a-l.it>
2022-09-18 07:05:50 -0400

    Test

--- proquotes/controllers/notification.py
+++ proquotes/controllers/notification.py
@@ -1,20 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import binascii
-
-from odoo import fields, http, _
-from odoo.exceptions import AccessError, MissingError, UserError
-from odoo.http import request
-from odoo.osv import expression
-import logging
-
-# from odoo.mail import CustomerPortalINH as CP
-
-
-# class notification(CP):
-# pass
-
 class notification:
     def something():
         x = 1 + 1
