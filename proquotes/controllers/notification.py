c5887d80667aa6651dd61f5b57bc99e647387584
Ty-J-Cyr <ty@r-e-a-l.it>
2022-09-18 07:03:45 -0400

    Test

--- proquotes/controllers/notification.py
+++ proquotes/controllers/notification.py
@@ -6,9 +6,6 @@ import binascii
 from odoo import fields, http, _
 from odoo.exceptions import AccessError, MissingError, UserError
 from odoo.http import request
-from odoo.addons.portal.controllers.mail import _message_post_helper
-from odoo.addons.portal.controllers.portal import pager as portal_pager, get_records_pager
-from odoo.addons.sale.controllers.portal import CustomerPortal
 from odoo.osv import expression
 import logging
 
