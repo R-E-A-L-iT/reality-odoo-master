38f7b78993239aa843f1b7797d233575987170aa
Ty-J-Cyr <ty@r-e-a-l.it>
2022-09-23 12:04:54 -0400

    Test

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -37,7 +37,7 @@
             ]
     },
 
-    'version': '0.5',
+    'version': '0.7',
 
     # always loaded
     'data': [
@@ -46,7 +46,7 @@
         'views/quotesPDF.xml',
         'views/invoicePDF.xml',
         'views/quotesBackend.xml',
-        # 'views/tax.xml',
+        'views/tax.xml',
         'views/mail.xml',
         'views/quotesTemplates.xml',
         'views/quoteLogo.xml',
