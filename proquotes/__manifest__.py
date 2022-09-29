4ee8170ae2dfd234c49ab1b5dee74f96ae3778bf
Ty-J-Cyr <ty@r-e-a-l.it>
2022-09-29 07:56:31 -0400

    Test

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -37,7 +37,7 @@
             ]
     },
 
-    'version': '0.3',
+    'version': '0.4',
 
     # always loaded
     'data': [
@@ -53,7 +53,7 @@
         'views/renewalText.xml',
         'views/rentalTerms.xml',
         'views/invoiceBackend.xml',
-        # 'views/deliverPDF.xml',
+        'views/deliverPDF.xml',
         #         'models/quoteNotify.py'
     ],
     # only loaded in demonstration mode
