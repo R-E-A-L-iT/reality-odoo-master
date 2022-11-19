e9ee6fc5073ea0f5e3470db748453400d6ed92fa
Ty Cyr <ty@r-e-a-l.it>
2022-11-19 12:36:02 -0500

    Test

--- proquotes/static/src/JS/price.js
+++ proquotes/static/src/JS/price.js
@@ -114,7 +114,7 @@ odoo.define("proquotes.price", function (require) {
 				}
 			}
 
-			totalLanding.innerHTML = total.toFixed(2).toLocaleString() + "$";
+			totalLanding.innerHTML = total.toFixed(2) + "$";
 		},
 
 		_updateSectionSelectionEvent: function (ev) {
