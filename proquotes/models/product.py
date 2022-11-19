b15a73583beb01cf5c8701354e3d43817429745b
Ty Cyr <ty@r-e-a-l.it>
2022-11-19 11:16:09 -0500

    Restore

--- proquotes/models/product.py
+++ proquotes/models/product.py
@@ -22,5 +22,3 @@ class product(models.Model):
     # ('software', "Software")], default="equipment", required=True, string="Equipment/Software")
     cadVal = fields.Monetary(string="Canadian Product Value")
     usdVal = fields.Monetary(string="United States Product Value")
-    isSoftware = fields.Boolean(
-        string="Is Software", default=False)
