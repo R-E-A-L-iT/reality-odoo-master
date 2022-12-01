9f2f4ce0db84acb3908a3bdf918d9edb3d1ce42b
Ty Cyr <ty@r-e-a-l.it>
2022-12-01 14:20:00 -0500

    Test

--- proquotes/models/invoiceModels.py
+++ proquotes/models/invoiceModels.py
@@ -26,7 +26,8 @@ class InvoiceMain(models.Model):
     @api.onchange('pricelist_id')
     def _update_prices(self):
         for record in self.invoice_line_ids:
-            print(str(record.name))
+            _logger.info(str(record.name))
+            _logger.info("Prices Updated")
 
 
 class invoiceLine(models.Model):
