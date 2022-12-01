101ca88541a8c7b9124015df31abbbe350d5bd4e
Ty Cyr <ty@r-e-a-l.it>
2022-12-01 18:25:33 -0500

    Test

--- proquotes/models/invoiceModels.py
+++ proquotes/models/invoiceModels.py
@@ -37,7 +37,7 @@ class InvoiceMain(models.Model):
             priceResult = pricelist.item_ids.search(
                 [('product_id', '=', id)])
             _logger.info(pricelist.item_ids)
-            _logger.info("Search" + str(priceResult))
+            _logger.info(str(priceResult))
         _logger.info("Prices Updated")
 
 
