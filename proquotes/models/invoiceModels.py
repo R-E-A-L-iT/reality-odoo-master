cd2b54179b1f69f04cdf13b72cf5e41fec30ff1c
Ty Cyr <ty@r-e-a-l.it>
2022-12-05 11:41:59 -0500

    Test

--- proquotes/models/invoiceModels.py
+++ proquotes/models/invoiceModels.py
@@ -37,7 +37,7 @@ class InvoiceMain(models.Model):
             name = product.name
             _logger.info(str(name))
             priceResult = self.env['product.pricelist.item'].search(
-                ['base_pricelist_id.id', '=', self.pricelist_id.id('product_tmpl_id.sku', '=', sku)])
+                [('base_pricelist_id.id', '=', self.pricelist_id.id), ('product_tmpl_id.sku', '=', sku)])
             record.price_unit = priceResult[-1].fixed_price
             _logger.info(pricelist.name)
             _logger.info(priceResult)
