c9553732a0b24fd02387d54a0d14806c893f1122
Ty Cyr <ty@r-e-a-l.it>
2022-12-05 09:39:41 -0500

    Test

--- proquotes/models/invoiceModels.py
+++ proquotes/models/invoiceModels.py
@@ -39,6 +39,7 @@ class InvoiceMain(models.Model):
             priceResult = pricelist.item_ids.search(
                 [('product_tmpl_id.sku', '=', sku)])
             product.price = priceResult[-1].fixed_price
+            _logger.info(priceResult[-1].fixed_price)
         _logger.info("Prices Updated")
 
 
