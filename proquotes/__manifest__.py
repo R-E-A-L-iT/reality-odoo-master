ad73346576944707f30c46585ad87e74e206d444
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2022-12-13 13:34:15 -0500

    Test

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -23,21 +23,19 @@
     'depends': ['base', 'web', 'mail', 'account', 'proportal', 'product', 'website', 'sale_management', 'sale', 'digest', 'portal'],
 
     'assets': {
-            'web.asse_ts_frontend': [
-                'proquotes/static/src/CSS/foldProducts.css',
-                'proquotes/static/src/CSS/pdf.css',
-                'proquotes/static/src/CSS/user-info.css',
-                'proquotes/static/src/CSS/login.css',
-                'proquotes/static/src/CSS/quoteStyle.css',
-                'proquotes/static/src/CSS/quoteHeaderText.css',
-                'proquotes/static/src/JS/fold.js',
-                'proquotes/static/src/JS/poNumber.js',
-                'proquotes/static/src/JS/price.js',
-                'proquotes/static/src/JS/rental.js',
-            ]
+        'proquotes/static/src/CSS/foldProducts.css',
+        'proquotes/static/src/CSS/pdf.css',
+        'proquotes/static/src/CSS/user-info.css',
+        'proquotes/static/src/CSS/login.css',
+        'proquotes/static/src/CSS/quoteStyle.css',
+        'proquotes/static/src/CSS/quoteHeaderText.css',
+        'proquotes/static/src/JS/fold.js',
+        'proquotes/static/src/JS/poNumber.js',
+        'proquotes/static/src/JS/price.js',
+        'proquotes/static/src/JS/rental.js',
     },
 
-    'version': '0.2',
+    'version': '0.3',
 
 
     # always loaded
