828c4ab096b5a3ea63fbf114957396544101b573
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2022-12-16 09:36:55 -0500

    Reload

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -37,13 +37,13 @@
             ]
     },
 
-    'version': '0.3',
+    'version': '0.4',
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/Quote/quotesFrontend.xml',
-        # 'views/Quote/quotesBackend.xml',
+        'views/Quote/quotesBackend.xml',
         'views/Quote/quotesPDF.xml',
         'views/Quote/quoteLogo.xml',
         'views/Quote/quotesTemplates.xml',
