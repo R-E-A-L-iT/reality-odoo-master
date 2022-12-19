c08526d26e46e6c1e0c01437b86a6d024904a9fe
qwertypeople <olivier@r-e-a-l.it>
2022-12-19 18:31:44 -0500

    valueerror test

--- sync/tests/test_sync.py
+++ sync/tests/test_sync.py
@@ -44,8 +44,7 @@ class TestModuleDemo(TransactionCase):
         ]
         result = synce_model.getSheetIndex(sync_data, 1)
         self.assertEqual(10, result)
-        
-        with self.assertRaises(Exception) as context:
-            result = synce_model.getSheetIndex(sync_data, 6)
-        self.assertTrue('ValueError' in context.exception)             
+
+        self.assertRaises(ValueError, getSheetIndex, sync_data, 6)
+           
 
