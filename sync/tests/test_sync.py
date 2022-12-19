5fc56e018daeeb098c828d563f118241f3da0923
qwertypeople <olivier@r-e-a-l.it>
2022-12-19 13:55:38 -0500

    test with unitest

--- sync/tests/test_sync.py
+++ sync/tests/test_sync.py
@@ -6,12 +6,12 @@ _logger = logging.getLogger(__name__)
 
 # The CI will run these tests after all the modules are installed,
 # not right after installing the one defining it.
-@tagged('post_install', '-at_install')  # add `post_install` and remove `at_install`
+@tagged('standard') 
 class PostInstallTestCase(TransactionCase):
     def test_01(self):
        _logger.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! test_01")
        self.assertEqual('foo'.upper(), 'FOO') 
 
     def test_02(self):
-        _logger.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! test_02")
+       _logger.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! test_02")
        self.assertEqual('bar'.upper(), 'FOO')        
