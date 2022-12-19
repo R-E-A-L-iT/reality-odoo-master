923b8f7bd2b5ecd44a3101df40f5f59110223f37
qwertypeople <olivier@r-e-a-l.it>
2022-12-19 15:53:35 -0500

    test with sync module in test

--- sync/tests/test_sync.py
+++ sync/tests/test_sync.py
@@ -6,13 +6,16 @@ from odoo.addons.sync.models import sync
 
 class TestModuleDemo(TransactionCase):
 
+    #def setUp(self):
+    #    super(TestModuleDemo, self).setUp()
+
     def test_that_pass(self):
         self.assertEqual("AAA", "AAA")
         print('test_that_pass: Your test was successful!')
             
     def test_is_psw_empty(self):
-        s = sync()
-        print (s._name)
+        synce_model = self.env['sync.sync']
+        print (synce_model._name)
         self.assertEqual("AAA", "AAA")
         print('test_is_psw_empty: Your test was successful!')
            
