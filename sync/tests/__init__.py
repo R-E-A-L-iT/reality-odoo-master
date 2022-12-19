ee84a6b255a84118b29bdf7879d9d0ffa053fe1b
qwertypeople <olivier@r-e-a-l.it>
2022-12-19 15:00:08 -0500

    test again

--- sync/tests/__init__.py
+++ sync/tests/__init__.py
@@ -1,6 +1,2 @@
 from . import test_sync
 
-import logging
-_logger = logging.getLogger(__name__)
-_logger.info('********************************  init test_sync logger')
-
--- sync/tests/test_sync.py
+++ sync/tests/test_sync.py
@@ -1,15 +1,10 @@
-#from odoo.tests import common
-import logging
-_logger = logging.getLogger(__name__)
-_logger.info('********************************  test_sync logger')
-#
-#class TestModuleDemo(common.TransactionCase):
-#    def setUp(self):        
-#        super(TestModuleDemo, self).setUp()
-#        # Add test setUp code here
-#
-#    def test_some_action(self):
-#            self.assertEqual("AAA", "AAA")
-#            #self.assertEqual("AAA", "BBB")
-#            print('Your test was successful!')
-#            _logger.info('Your test was successful!')
+from odoo.tests import TransactionCase
+#from odoo.tests.common import TransactionCase
+
+
+class TestModuleDemo(TransactionCase):
+
+    def test_some_action(self):
+            self.assertEqual("AAA", "AAA")
+            #self.assertEqual("AAA", "BBB")
+            print('Your test was successful!')
