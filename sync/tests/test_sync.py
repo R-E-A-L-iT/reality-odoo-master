d6097ca134a72ae11d19ed5797bf47e8dbc7dba9
qwertypeople <olivier@r-e-a-l.it>
2022-12-19 14:28:17 -0500

    remove test

--- sync/tests/test_sync.py
+++ sync/tests/test_sync.py
@@ -1,16 +1,16 @@
-from odoo.tests import common
-import logging
-
-_logger = logging.getLogger(__name__)
-
-
-class TestModuleDemo(common.TransactionCase):
-    def setUp(self):        
-        super(TestModuleDemo, self).setUp()
-        # Add test setUp code here
-
-    def test_some_action(self):
-            self.assertEqual("AAA", "AAA")
-            #self.assertEqual("AAA", "BBB")
-            print('Your test was successful!')
-            _logger.info('Your test was successful!')
+#from odoo.tests import common
+#import logging
+#
+#_logger = logging.getLogger(__name__)
+#
+#
+#class TestModuleDemo(common.TransactionCase):
+#    def setUp(self):        
+#        super(TestModuleDemo, self).setUp()
+#        # Add test setUp code here
+#
+#    def test_some_action(self):
+#            self.assertEqual("AAA", "AAA")
+#            #self.assertEqual("AAA", "BBB")
+#            print('Your test was successful!')
+#            _logger.info('Your test was successful!')
