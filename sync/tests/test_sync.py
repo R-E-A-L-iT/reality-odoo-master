0d4700ca55a194899d07e6ccc601a11a3d19ddec
qwertypeople <olivier@r-e-a-l.it>
2022-12-19 15:29:14 -0500

    test to print sync name in test

--- sync/tests/test_sync.py
+++ sync/tests/test_sync.py
@@ -1,13 +1,18 @@
 from odoo.tests import TransactionCase
+from . import sync
+
 #from odoo.tests.common import TransactionCase
 
 
 class TestModuleDemo(TransactionCase):
 
     def test_that_pass(self):
-            self.assertEqual("AAA", "AAA")
-            print('test_that_pass: Your test was successful!')
-
-    def test_that_fail(self):
-            self.assertEqual("AAA", "BBB")
-            print('test_that_fail: Your test was successful!')            
+        self.assertEqual("AAA", "AAA")
+        print('test_that_pass: Your test was successful!')
+            
+    def test_is_psw_empty(self):
+        s = sync()
+        print (s._name)
+        self.assertEqual("AAA", "AAA")
+        print('test_is_psw_empty: Your test was successful!')
+           
