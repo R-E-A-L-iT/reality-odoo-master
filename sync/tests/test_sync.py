abcc1fc81e08f5c11ae9f3887f37089f6deaaaee
qwertypeople <olivier@r-e-a-l.it>
2022-12-19 14:22:10 -0500

    test

--- sync/tests/test_sync.py
+++ sync/tests/test_sync.py
@@ -1,26 +1,16 @@
-#from odoo.tests.common import TransactionCase
-#from odoo.tests import tagged
-
-import unittest
+from odoo.tests import common
 import logging
 
 _logger = logging.getLogger(__name__)
 
-# The CI will run these tests after all the modules are installed,
-# not right after installing the one defining it.
-#@tagged('standard') 
-#class StandardTestCase(TransactionCase):
 
+class TestModuleDemo(common.TransactionCase):
+    def setUp(self):
+        super(TestModuleDemo, self).setUp()
+        # Add test setUp code here
 
-_logger.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! UNIT TEST CLASS")
-class TestStringMethods(unittest.TestCase):
-    def test_01(self):
-       _logger.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! test_01")
-       self.assertEqual('foo'.upper(), 'FOO') 
-
-    def test_02(self):
-       _logger.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! test_02")
-       self.assertEqual('bar'.upper(), 'FOO')        
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_some_action(self):
+            self.assertEqual("AAA", "AAA")
+            self.assertEqual("AAA", "BBB")
+            print('Your test was successful!')
+            _logger.info('Your test was successful!')
