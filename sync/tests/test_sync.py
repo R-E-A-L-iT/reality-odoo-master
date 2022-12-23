635b2b3dbcb21feb2828556b831fb1b57c2ad5e4
qwertypeople <olivier@r-e-a-l.it>
2022-12-23 11:05:00 -0500

    dev

--- sync/tests/test_sync.py
+++ sync/tests/test_sync.py
@@ -1,5 +1,5 @@
 from odoo.tests import TransactionCase
-from odoo.addons.sync.models import sync
+#from odoo.addons.sync.models import sync
 
 #To run the test, open the console and type : 
 # odoo-bin --test-enable -i sync
