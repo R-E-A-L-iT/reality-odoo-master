b85c5071565951be048558c2e7d2ea46e7fdb10a
qwertypeople <olivier@r-e-a-l.it>
2022-12-23 11:49:21 -0500

    updated GS database

--- sync/models/googlesheetsAPI.py
+++ sync/models/googlesheetsAPI.py
@@ -16,10 +16,10 @@ class sheetsAPI(models.Model):
     # 1Tbo0NdMVpva8coych4sgjWo7Zi-EHNdl6EFx2DZ6bJ8
     # 
     # DEV R-E-A-L.iT Master Database
-    # 1BnAE-CwBcejnMAIIqQUxHHhb6sO2TeZoXIOwxIRtTfE
+    # 1j3nlH8QH-s8D_oGE0TwsHxoA4Uvb4QB05DriH4Zh5KM
 
     
-    _master_database_template_id = "1BnAE-CwBcejnMAIIqQUxHHhb6sO2TeZoXIOwxIRtTfE"
+    _master_database_template_id = "1j3nlH8QH-s8D_oGE0TwsHxoA4Uvb4QB05DriH4Zh5KM"
     
     def getDoc(self, psw, spreadsheetID, sheet_num):
         scope = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
