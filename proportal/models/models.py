53fa73e1eee6434fdbe80db3ae42fb9c0a72ef47
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2022-12-23 20:22:20 -0500

    Test

--- proportal/models/models.py
+++ proportal/models/models.py
@@ -48,9 +48,9 @@ class productInstance(models.Model):
 
     def _label(self):
         for i in self:
-            r = i.name + " " + " " + i.product_id.name
+            r = '#cpp\t' + i.name + "\t" + "\t" + i.product_id.name
             if (i.expire != False):
-                r = r + " Expiration: " + str(i.expire)
+                r = r + "\t" + str(i.expire)
             i.formated_label = r
             return
 
