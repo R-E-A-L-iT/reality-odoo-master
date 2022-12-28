f0fec834ee3f27d8a1caf01b537ab48f2a1dbcbd
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2022-12-28 18:54:23 -0500

    Test

--- proquotes/models/invoiceModels.py
+++ proquotes/models/invoiceModels.py
@@ -57,8 +57,8 @@ class invoiceLine(models.Model):
     applied_name = fields.Char(
         compute='get_applied_name', string="Applied Name")
 
-    def setPrice():
-        raise Exception("self.journal_id")
+    def setPrice(self):
+        raise Exception(f'{self.journal_id}')
 
     @api.onchange('price_unit')
     def init_price(self):
