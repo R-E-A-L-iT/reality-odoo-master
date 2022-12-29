293ffaa6c44d4d9f24b36162201d5a5e4c9e3e69
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2022-12-29 09:28:30 -0500

    Test

--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -240,7 +240,8 @@ class owner(models.Model):
     owner = fields.Many2one('res.partner', string="Owner")
 
     def copy_label(self):
-        pass
+        # Form Button Needs a Python Target Function
+        return
 
 
 # pdf footer
