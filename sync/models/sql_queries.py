644fa5527bd68e93173a33ec7a849d377dffd4a9
qwertypeople <olivier@r-e-a-l.it>
2023-01-04 10:16:43 -0500

    sql_queries test

--- sync/models/sql_queries.py
+++ sync/models/sql_queries.py
@@ -4,17 +4,18 @@ _logger = logging.getLogger(__name__)
 
 class sql_queries:
 
-    def __init__(self):
+    def __init__(self, db):
+        self.db = db
         pass
     
     def listSQLTables(self):
         _logger.info("customQuery test")
-        self.env.cr.execute("""
+        self.db.env.cr.execute("""
             SELECT table_name
             FROM INFORMATION_SCHEMA.TABLES
             WHERE table_type = 'BASE TABLE'
             """)
-        tables = self.env.cr.fetchall()
+        tables = self.db.env.cr.fetchall()
         res = "\n"
         for table in tables:
             res += str(table)
@@ -23,11 +24,11 @@ class sql_queries:
 
     def listAllSaleOrder(self):
         _logger.info("customQuery test")
-        self.env.cr.execute("""
+        self.db.env.cr.execute("""
             SELECT * 
             FROM sale_order
             """)
-        tables = self.env.cr.fetchall()
+        tables = self.db.env.cr.fetchall()
         res = "\n"
         for table in tables:
             res += str(table)
@@ -36,12 +37,12 @@ class sql_queries:
 
     def listSaleOrderAttributs(self):
         _logger.info("customQuery test")
-        self.env.cr.execute("""
+        self.db.env.cr.execute("""
         SELECT COLUMN_NAME 
         FROM information_schema.columns 
         WHERE table_name = 'sale_order'
         """)
-        tables = self.env.cr.fetchall()
+        tables = self.db.env.cr.fetchall()
         res = "\n"
         for table in tables:
             res += str(table)
