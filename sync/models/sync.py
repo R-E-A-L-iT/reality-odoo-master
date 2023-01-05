0660d9f9b4d71839e9fd64354611996ad228bed0
qwertypeople <olivier@r-e-a-l.it>
2023-01-05 14:37:51 -0500

    dev

--- sync/models/sync.py
+++ sync/models/sync.py
@@ -1297,6 +1297,7 @@ class sync(models.Model):
                         _logger.info("---------------")  
                         _logger.info("orders name: " + str(order.name))  
                         _logger.info("id in a sale order: " + str(product.id))        
+                        _logger.info("sku in a sale order: " + str(product.sku))
                         _logger.info("name in a sale order: " + str(product.name))  
                         _logger.info("---------------")  
 
