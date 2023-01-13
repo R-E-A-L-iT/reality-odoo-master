aa16890b5be125192ba96a7d6ebf407afbd6b29d
OlivierRealIT <qwertypeople@yahoo.com>
2023-01-13 08:57:29 -0500

    set dev setting

--- sync/models/variablesettings.py
+++ sync/models/variablesettings.py
@@ -6,4 +6,4 @@ class googlesheetsetting():
     # DEV R-E-A-L.iT Master Database
     # 1E454v0jC2NpkfTENpc-OT0Uh2EW4U3fFVZecwmFGDTc
 
-    _master_database_template_id = "1Tbo0NdMVpva8coych4sgjWo7Zi-EHNdl6EFx2DZ6bJ8"
+    _master_database_template_id = "1E454v0jC2NpkfTENpc-OT0Uh2EW4U3fFVZecwmFGDTc"
