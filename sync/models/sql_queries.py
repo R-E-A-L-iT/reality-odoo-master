48dde795440a203958e8b768609fba84626519b9
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-02-01 17:34:05 -0500

    Test

--- sync/models/sql_queries.py
+++ sync/models/sql_queries.py
@@ -166,7 +166,7 @@ class sql_queries(models.Model):
         self.env.cr.execute("""
             SELECT type 
             FROM ir_translation
-            WHERE type != 'model'
+            WHERE type = Null 
             """)
         tables = self.env.cr.fetchall()
         out = ""
