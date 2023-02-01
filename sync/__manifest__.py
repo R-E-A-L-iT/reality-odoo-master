a09d119c42e6bdc388523a33ddb0f8454979f21a
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-02-01 16:10:41 -0500

    Test

--- sync/__manifest__.py
+++ sync/__manifest__.py
@@ -18,7 +18,7 @@
     # for the full list
     'category': 'Technical',
 
-    'veision': '0.3',
+    'veision': '0.4',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'proportal', 'proquotes', 'product', 'google_account', 'google_drive'],
