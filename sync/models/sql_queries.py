76f18e55755b0e268d343b179bf4af05d328e399
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-02-01 17:43:41 -0500

    Test

--- sync/models/sql_queries.py
+++ sync/models/sql_queries.py
@@ -164,7 +164,7 @@ class sql_queries(models.Model):
 
         _logger.info("listSpecificLineFromTable")
         self.env.cr.execute("""
-            SELECT src, type, lang
+            SELECT value, type, lang
             FROM ir_translation
             WHERE type is NULL
             """)
