89e9f427a437d2e838e7a29ac7139b9b86eb0a8b
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-02-02 14:10:47 -0500

    Test

--- sync/models/sync.py
+++ sync/models/sync.py
@@ -223,7 +223,8 @@ class sync(models.Model):
         elif (syncType == "WebHTML"):
             syncer = syncWeb(sheetName, sheet, self)
             quit, msg = syncer.syncWebCode(sheet)
-            _logger.error(msg)
+            if msg != "":
+                _logger.error(msg)
 
         _logger.info("Done with " + syncType)
 
@@ -587,7 +588,7 @@ class sync(models.Model):
             {'name': sheet[i][columns["name"]]})[0]
         ext.res_id = contact.id
         self.updateContacts(contact, sheet, sheetWidth, i, columns)
-        
+
     def check_id(self, id):
         if (" " in id):
             _logger.info("ID: " + str(id))
