7522ed8192cc7486dd745c9a54ff3a7db9480697
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-02-04 14:41:21 -0500

    Test

--- sync/tests/test_pricelist.py
+++ sync/tests/test_pricelist.py
@@ -1,5 +1,5 @@
 from odoo.tests import TransactionCase
-import models.product_sync_common as product_sync_common
+import proquotes.models.product_common as product_sync_common
 # To run the test, open the console and type :
 # odoo-bin --test-enable -i sync
 
