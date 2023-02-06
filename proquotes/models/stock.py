4f7c9d9611d031a3643a564731a4334c0cc17fea
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-02-06 11:59:47 -0500

    Test

--- proquotes/models/stock.py
+++ proquotes/models/stock.py
@@ -35,8 +35,8 @@ class stock(models.Model):
         ('ABtechFooter_Quebec_Derek', "Abtech_Quebec_Derek"),
         ('GeoplusFooterCanada', "Geoplus_Canada"),
         ('GeoplusFooter_US', "Geoplus_America"),
-        ('Leica_Footer_Ali', "Leica Ali")
+        ('Leica_Footer_Ali', "Leica Ali"),
         ('REALiTFooter_Derek_US', "REALiTFooter_Derek_US"),
-        ('REALiTSOLUTIONSLLCFotter_Derek_US', "R-E-A-L.iT Solutions Derek")
-        ('REALiTFooter_Derek', "REALiTFooter_Derek")
+        ('REALiTSOLUTIONSLLCFotter_Derek_US', "R-E-A-L.iT Solutions Derek"),
+        ('REALiTFooter_Derek', "REALiTFooter_Derek"),
     ], default='REALiTFooter_Derek', required=True, help="Footer selection field")
