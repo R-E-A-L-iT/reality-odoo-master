f900e9afa2fa57bd82b648d4dc2d82d18f5151d5
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-02-06 17:24:37 -0500

    Test

--- sync/tests/test_product.py
+++ sync/tests/test_product.py
@@ -1,4 +1,31 @@
-class test():
+
+from odoo.tests import TransactionCase
+
+
+class product_test(TransactionCase):
+    def setUp(self):
+        self.sheet_index_30 = [
+            ['SKU', 	 'EN-Name', 	 'EN-Description', 		'FR-Name', 		'FR-Description', 	   'Price CAD', 'Price USD', 'Can Rental', 'US Rental', 'Store Image', 								   'Store Title', 	   'Store Description', 	 'Publish_CA', 'Publish_USA', 'Can_Be_Sold',
+                'E-Commerce_Website_Code', 'isSoftware', 'Product Type', 'Tracking', 'CAN PL SEL',    'CAN PL ID', 'USD PL SEL',    'US PL ID', 'CAN R SEL',  'CAN R ID', 	 'US R SEL',   'ECOM-FOLDER', 'ECOM-MEDIA', 'US R ID',   'Valid', 'Continue'],
+            ['SKU-1111', 'EN-Name-1111', 'EN-Description-1111', 'FR-Name-1111',	'FR-Description-1111', '', 		 	'', 		 '0.5', 		'0.25', 	'https://r-e-a-l.it/images/products/1111.png', 'Store Title-1111', 'Store Description-1111', 'FALSE',
+                'FALSE', 	  'TRUE', 	     '',   					    'FALSE',	  'product',      'serial',   'CAN Pricelist', 'CAN162200', 'USD Pricelist', 'US372200', 'CAN RENTAL', 'CANR939200', 'USD RENTAL', 'Leica', 	  '', 		    'USR127290', 'TRUE',  'TRUE'],
+            ['SKU-1112', 'EN-Name-1112', 'EN-Description-1112', 'FR-Name-1112',	'FR-Description-1112', '330', 		'255', 	  	 '', 			'', 		'https://r-e-a-l.it/images/products/1112.png', 'Store Title-1112', 'Store Description-1112', 'FALSE',
+                'FALSE', 	  'TRUE', 	     '',   					    'FALSE',	  'product',      'serial',   'CAN Pricelist', 'CAN165500', 'USD Pricelist', 'US375500', 'CAN RENTAL', 'CANR989500', 'USD RENTAL', 'Leica', 	  '', 		    'USR127590', 'TRUE',  'TRUE'],
+            ['SKU-1113', 'EN-Name-1113', 'EN-Description-1113', 'FR-Name-1113',	'FR-Description-1113', '580', 		'450', 	  	 '29', 			'22.5', 	'https://r-e-a-l.it/images/products/1113.png', 'Store Title-1113', 'Store Description-1113', 'FALSE',
+                'FALSE', 	  'TRUE', 	     '',   					    'FALSE',	  'product',      'serial',   'CAN Pricelist', 'CAN164752', 'USD Pricelist', 'US374752', 'CAN RENTAL', 'CANR999752', 'USD RENTAL', 'Leica', 	  '', 		    'USR127792', 'TRUE',  'TRUE']
+        ]
+
+        self.sheet_index_40 = [
+            ['SKU', 	 'EN-Name', 	 'EN-Description', 		'FR-Name', 		'FR-Description', 	   'Price CAD', 'Price USD', 'Can Rental', 'US Rental', 'Store Image', 								   'Store Title', 	   'Store Description', 	 'Publish_CA', 'Publish_USA', 'Can_Be_Sold',
+                'E-Commerce_Website_Code', 'isSoftware', 'Product Type', 'Tracking', 'CAN PL SEL',    'CAN PL ID', 'USD PL SEL',    'US PL ID',   'CAN R SEL',  'CAN R ID',   'US R SEL',   'ECOM-FOLDER', 'ECOM-MEDIA', 'US R ID',   'Valid', 'Continue'],
+            ['SKU-2221', 'EN-Name-2221', 'EN-Description-2221', 'FR-Name-2221',	'FR-Description-2221', '', 		 	'', 		 '2.5', 		'1.25', 	'https://r-e-a-l.it/images/products/2221.png', 'Store Title-2221', 'Store Description-2221', 'FALSE',
+                'FALSE', 	  'TRUE', 	     '',   					    'FALSE',	  'product',      'serial',   'CAN Pricelist', 'CAN33780',  'USD Pricelist', 'US389200',   'CAN RENTAL', 'CANR132290', 'USD RENTAL', 'Leica', 	    '', 		  'USR378907', 'TRUE',  'TRUE'],
+            ['SKU-2222', 'EN-Name-2222', 'EN-Description-2222', 'FR-Name-2222',	'FR-Description-2222', '850', 		'1105',	  	 '', 			'', 		'https://r-e-a-l.it/images/products/2222.png', 'Store Title-2222', 'Store Description-2222', 'FALSE',
+                'FALSE', 	  'TRUE', 	     '',   					    'FALSE',	  'product',      'serial',   'CAN Pricelist', 'CAN38780',  'USD Pricelist', 'US389500',   'CAN RENTAL', 'CANR185590', 'USD RENTAL', 'Leica', 	    '', 		  'USR378907', 'TRUE',  'TRUE'],
+            ['SKU-2223', 'EN-Name-2223', 'EN-Description-2223', 'FR-Name-2223',	'FR-Description-2223', '695', 		'905', 	  	 '58', 			'75.5', 	'https://r-e-a-l.it/images/products/2223.png', 'Store Title-2223', 'Store Description-2223', 'FALSE',
+                'FALSE', 	  'TRUE', 	     '',   					    'FALSE',	  'product',      'serial',   'CAN Pricelist', 'CAN39782',  'USD Pricelist', 'US389452',   'CAN RENTAL', 'CANR194792', 'USD RENTAL', 'Leica', 	    '', 		  'USR378957', 'TRUE',  'TRUE']
+        ]
+
     # Test to be executer befor creating a product
     # Input
     #   external_id: Sku to check if not existing
