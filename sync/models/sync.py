fd77e0d7253c27970d8953b24244004db2bc0fce
qwertypeople <qwertypeople@yahoo.com>
2023-02-22 14:58:08 -0500

    getSaleOrderByProductId 1.04

--- sync/models/sync.py
+++ sync/models/sync.py
@@ -1109,15 +1109,14 @@ class sync(models.Model):
         _logger.info("--------------- product_template.name: " + str(product_template.name))
                     
         for line in lines:    
-                for line_product_template in line.product_template_id:          
-                    #line.product_template_id: product.template(17172,), product_template.id: 561634                      
-                    _logger.info("--------------- line.product_template_id: " + str(line_product_template.id).ljust(20) + ", product_template.id: " + str(product_template.id))
-                    #if (line.product_template_id == product_template.id):
-                    #    sale = self.env['sale.order'].search([
-                    #        ('id', '=', line.order_id)])                           
-                    #    _logger.info("--------------- line.product_template_id: " + str(line.product_template_id).ljust(10) + 
-                    #                    ", sale.id: " + str(line.order_id).ljust(10) +
-                    #                    ", sale.name" + str(sale.name))      
+                for line_product_template in line.product_template_id:                                                 
+                    if (line_product_template.id == product_template.id):
+                        sale = self.env['sale.order'].search([
+                            ('id', '=', line.order_id)])                           
+                        #_logger.info("--------------- line.product_template_id: " + str(line_product_template.id).ljust(20) + ", product_template.id: " + str(product_template.id))
+                        _logger.info("--------------- line_product_template.id: " + str(line_product_template.id).ljust(20) + 
+                                        ", sale.id: " + str(line.order_id).ljust(10) +
+                                        ", sale.name" + str(sale.name))      
 
         _logger.info("--------------- END ---------------------------------------------")
 
