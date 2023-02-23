2c7384edcc01bd3c3da6b8de59872c087d234510
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-02-23 12:53:54 -0500

    Test

--- proquotes/controllers/rental.py
+++ proquotes/controllers/rental.py
@@ -29,7 +29,7 @@ class RentalCustomerPortal(cPortal):
         except (AccessError, MissingError):
             return request.redirect('/my')
 
-        if (str(newAdd) == True or str(newAdd) == False):
+        if (str(newAdd) == "True" or str(newAdd) == "False"):
             order_sudo.rental_diff_add = True if str(
                 newAdd) == "True" else False
 
