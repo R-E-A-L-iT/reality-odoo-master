e18e33300b4cd0c8753e7ea7ecd6d6a723d6d329
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-03-01 10:52:37 -0500

    Test

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -39,7 +39,7 @@
             ]
     },
 
-    'version': '1.0.683',
+    'version': '1.0.684',
 
     # always loaded
     'data': [
--- sync/models/googlesheetsAPI.py
+++ sync/models/googlesheetsAPI.py
@@ -30,7 +30,7 @@ class sheetsAPI(models.Model):
         _master_database_template_id_prod = "1Tbo0NdMVpva8coych4sgjWo7Zi-EHNdl6EFx2DZ6bJ8"
 
         # Dev Numbers Set Based on When Developer Joined
-        _master_database_template_id_dev1 = "1UT2h8r8ut3tB4icPKLJwO6d6qfEr2Oca8Vv6po3yYEc"
+        _master_database_template_id_dev1 = "1vUML53ydeihykHwVU9GA868_EPuFn0Id6EaDXtzlcns"
         _master_database_template_id_dev2 = "1xeLEapDAWwCh3COsKCQEcaPdYab_HA9FAiMF0FRvA1k"
 
         # Return the proper GoogleSheet Template ID base on the environement
