95eb0c7bc5e26a81ec2a73e5012174195ca603f6
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-03-09 17:24:30 -0500

    Test

--- proportal/__manifest__.py
+++ proportal/__manifest__.py
@@ -32,7 +32,7 @@
                 'proportal/static/src/CSS/footer.css',
                 'proportal/static/src/CSS/mobileWebsite.css',
                 'proportal/static/src/CSS/cart.css',
-                'proportal/static/src/CSS/calendar.css',
+                'proportal/static/src/CSS/calendar.css'
             ]
     },
 
