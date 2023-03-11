18608bddc8132d941f9e63ed70b5dfa4690f93f2
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-03-11 14:56:16 -0500

    Test

--- proportal/__manifest__.py
+++ proportal/__manifest__.py
@@ -17,7 +17,7 @@
     # for the full list
     'category': 'Sales',
 
-    'version': '0.134',
+    'version': '0.135',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'website', 'stock_account', 'product', 'purchase', 'stock', 'portal', 'website_sale'],
