c829cd25d500fd3185f926e03d15fecdcd756b2f
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-03-11 12:36:50 -0500

    Test

--- proquotes/models/invoiceModels.py
+++ proquotes/models/invoiceModels.py
@@ -26,7 +26,7 @@ class InvoiceMain(models.Model):
     _inherit = "account.move"
     pricelist_id = fields.Many2one('product.pricelist', string="Pricelist")
 
-    @api.onchange('pricelist_id')
+    @api.onchange('pricelist_id', 'invoice_line_ids')
     def _update_prices(self):
         pricelist = self.pricelist_id.id
 
