15594e31ed3605048bccb114e28351d9fc6ce7da
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-03-13 17:16:42 -0400

    Test

--- problog/__manifest__.py
+++ problog/__manifest__.py
@@ -19,7 +19,7 @@
     # for the full list
     'category': 'Website',
 
-    'version': '0.41',
+    'version': '0.42',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'website', 'website_blog'],
