57681375a1b5d54fa3af1e9eaa3ccd8be1df95fb
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-03-13 16:23:08 -0400

    Test

---
+++ problog/models/blog_post.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models, fields, _
+from odoo.addons.http_routing.models.ir_http import slug, unslug
+from odoo.addons.website.tools import text_from_html
+from odoo.tools.json import scriptsafe as json_scriptsafe
+from odoo.tools.translate import html_translate
+
+
+class BlogPost(models.Model):
+    _inherit = "blog.post"
