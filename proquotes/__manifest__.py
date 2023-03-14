cfa337749c7763615755d880bba0bac043db9028
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-03-13 22:51:53 -0400

    Test

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -39,7 +39,7 @@
             ]
     },
 
-    'version': '1.0.714',
+    'version': '1.0.715',
 
     # always loaded
     'data': [
--- sync/models/sync.py
+++ sync/models/sync.py
@@ -384,6 +384,7 @@ class sync(models.Model):
             # company.property_product_pricelist =
             test = self.env['product.pricelist'].search(
                 [('name', '=', sheet[i][columns["pricelist"]])])[0]
+            _logger.info(company.name)
             _logger.warning(company.country_id.name)
             _logger.warning(sheet[i][columns["pricelist"]])
             _logger.error(test.name)
