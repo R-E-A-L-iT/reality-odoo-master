7f7cbf7e86dd06e779728368a5637652f1c1d967
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-03-15 14:03:27 -0400

    Test

--- problog/controllers/news.py
+++ problog/controllers/news.py
@@ -29,7 +29,6 @@ class WebsiteNews(Blog):
     @http.route([
         '/news',
         '/news/page/<int:page>',
-        '/news/<int:page>'
         '/news/tag/<string:tag>',
         '/news/tag/<string:tag>/page/<int:page>',
         '''/news/<model("blog.blog"):blog>''',
@@ -38,5 +37,4 @@ class WebsiteNews(Blog):
         '''/news/<model("blog.blog"):blog>/tag/<string:tag>/page/<int:page>''',
     ], type='http', auth="public", website=True, sitemap=True)
     def news(self, blog=None, tag=None, page=1, search=None, **opt):
-        _logger.error("Here")
         return self.blog(blog=blog, tag=tag, page=page, serach=search, opt=opt)
