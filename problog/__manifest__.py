47b0579ce4a8f44bc1f15c28905becc86f544ec5
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-03-15 15:40:54 -0400

    Test

--- problog/__manifest__.py
+++ problog/__manifest__.py
@@ -19,7 +19,7 @@
     # for the full list
     'category': 'Website',
 
-    'version': '0.128',
+    'version': '0.129',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'website', 'website_blog'],
--- problog/controllers/news.py
+++ problog/controllers/news.py
@@ -35,12 +35,13 @@ class WebsiteNews(Blog):
         return self.blog(blog=blog, tag=tag, page=page, serach=search, opt=opt)
 
     @http.route([
-        '''/news/<model("blog.post"):blog_post>''',
+        '''/news/<blog_post>''',
     ], type='http', auth="public", website=True, sitemap=True)
     def news_post(self, blog, blog_post, tag_id=None, page=1, enable_editor=None, **post):
         blog_record = request.env['blog.blog'].search(
             [('name', '=', 'NEWS')])[0].name
         _logger.error(blog_record)
-        _logger.error(blog.name)
-        return self.blog_post(blog=blog, blog_post=blog_post, tag_id=tag_id,
-                              enable_editor=enable_editor, post=post)
+        # _logger.error(blog.name)
+        _logger.info(blog_post)
+        # return self.blog_post(blog=blog, blog_post=blog_post, tag_id=tag_id,
+        #   enable_editor = enable_editor, post = post)
