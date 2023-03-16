5f331bc55eaed7cb90745b8a1cfff43f35e835f7
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-03-16 14:16:45 -0400

    Test

--- proportal/__manifest__.py
+++ proportal/__manifest__.py
@@ -17,7 +17,7 @@
     # for the full list
     'category': 'Sales',
 
-    'version': '0.163',
+    'version': '0.164',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'website', 'stock_account', 'product', 'purchase', 'stock', 'portal', 'website_sale'],
@@ -51,7 +51,8 @@
         'views/cart.xml',
         'views/carousel.xml',
         'views/shopTile.xml',
-        'views/language_selector.xml'
+        'views/language_selector.xml',
+        'views/companyBackend',
     ],
     # only loaded in demonstration mode
     'demo': [
