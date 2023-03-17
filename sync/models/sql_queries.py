f1ead9e96d9b0f0ef43503ee55726e34788b2b6b
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-03-17 15:38:31 -0400

    Test

--- sync/models/sql_queries.py
+++ sync/models/sql_queries.py
@@ -71,6 +71,7 @@ class sql_queries(models.Model):
             res += str(table)
             res += "\n"
         _logger.info(res)
+        raise UserError(res)
 
     # Log a specific sale order
 
