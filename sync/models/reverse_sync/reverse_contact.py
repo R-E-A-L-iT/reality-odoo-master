ea15f167d9b4689af5f2e98d317acfe37b3cafab
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-03-22 19:38:54 -0400

    Test

--- sync/models/reverse_sync/reverse_contact.py
+++ sync/models/reverse_sync/reverse_contact.py
@@ -1,10 +1,13 @@
 from .googlesheetsAPI import sheetsAPI
+from odoo import api, fields, models, SUPERUSER_ID, _
 
 import logging
 _logger = logging.getLogger(__name__)
 
 
-class reverse_sync_contacts():
+class reverse_sync_contacts(models.Model):
+    _name = "sync.reverse_sync"
+
     def getSpreadSheetID(self):
         return "1pDPKv2bH8_Be5aCCyU4bqTuwJNcXsADlAYDAGG8P1Ls"
 
