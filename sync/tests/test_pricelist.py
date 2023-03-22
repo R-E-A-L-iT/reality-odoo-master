5324c92d85a834c21da3ebc1683998521e01171b
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-03-22 19:13:59 -0400

    Test

--- sync/tests/test_pricelist.py
+++ sync/tests/test_pricelist.py
@@ -1,5 +1,5 @@
 from odoo.tests import TransactionCase
-from odoo.addons.sync.models.pricelist import sync_pricelist
+from odoo.addons.sync.models.forward_sync.pricelist import sync_pricelist
 from odoo.addons.sync.models.product_common import product_sync_common
 
 # To run the test, open the console and type :
