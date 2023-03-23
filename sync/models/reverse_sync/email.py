b50e7527f0418ac0457f965bd3b276178daf2e19
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-03-23 13:58:02 -0400

    Test

--- sync/models/reverse_sync/email.py
+++ sync/models/reverse_sync/email.py
@@ -10,12 +10,13 @@ class reverse_sync_email():
         _logger.warning("Reverse Sync Report")
         values = {'subject': str(type) + ': Reverse Sync Report'}
         recipients = ["ty@r-e-a-l.it"]
-        for email in recipients:
+        for email_address in recipients:
             message = request.env['mail.message'].create(values)[0]
             values = {'mail_message_id': message.id}
 
+            email = request.env['mail.mail'].create(values)[0]
             email.body_html = msg
-            email.email_to = email
+            email.email_to = email_address
             email_id = {email.id}
             email.process_email_queue(email_id)
         return
