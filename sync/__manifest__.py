9771e2d111ff079b2096167ddf73888e518cb612
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-03-23 10:58:50 -0400

    Test

--- sync/__manifest__.py
+++ sync/__manifest__.py
@@ -18,7 +18,7 @@
     # for the full list
     'category': 'Technical',
 
-    'version': '0.0.190',
+    'version': '0.0.191',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'proportal', 'proquotes', 'product', 'google_account', 'google_drive'],
