ff2c2d9bf897594cb41945d70e5d8c3625af2577
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-03-25 16:24:25 -0400

    Test

--- proquotes/models/res_user_account.py
+++ proquotes/models/res_user_account.py
@@ -15,7 +15,7 @@ from odoo.tools.misc import formatLang, get_lang
 from odoo.osv import expression
 from odoo.tools import float_is_zero, float_compare
 from odoo import models, fields, api
-from odoo.addons.auth_signup.models.res_useres import ResUsers as res_users
+from odoo.models.auth_signup.models.res_useres import res_users as res_users
 
 import logging
 _logger = logging.getLogger(__name__)
