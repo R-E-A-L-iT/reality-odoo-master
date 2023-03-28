ee03cf1aaa017742b793e5cfd8d55ac2fd23a91f
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-03-28 15:49:21 -0400

    Test

--- proportal/models/livechat.py
+++ proportal/models/livechat.py
@@ -14,7 +14,7 @@ from odoo.exceptions import AccessError, UserError, ValidationError
 from odoo.tools.misc import formatLang, get_lang
 from odoo.osv import expression
 from odoo.tools import float_is_zero, float_compare
-from odoo.https import request
+from odoo.http import request
 from odoo import models, fields, api
 
 import logging
