315859cc045e8d787653d59b8071498ffec65964
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-03-30 14:34:45 -0400

    Test

---
+++ proquotes/models/mail.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import ast
+import base64
+import re
+
+from datetime import datetime, timedelta
+from functools import partial
+from itertools import groupby
+import logging
+
+from odoo import api, fields, models, SUPERUSER_ID, _, tools
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.tools.misc import formatLang, get_lang
+from odoo.osv import expression
+from odoo.tools import float_is_zero, float_compare
+from odoo import models, fields, api
+
+_logger = logging.getLogger(__name__)
+
+
+class mail(models.Model):
+    _inherit = "mail.group"
+
+    def action_send_guidelines(self, members=None):
+        _logger.error("MAIL 26")
+        return super().action.send_guidelines(members)
