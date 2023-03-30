25457ca8c008c4b628e2a33cf4a1c7bbb743bb0f
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-03-30 17:07:10 -0400

    Test

--- proquotes/models/mail.py
+++ proquotes/models/mail.py
@@ -25,6 +25,7 @@ class mail(models.TransientModel):
     def get_mail_values(self, res_ids):
         result = super().get_mail_values(res_ids)
         _logger.warning(result)
-        _logger.warning(result['email_from'])
-        _logger.warning(result['reply_to'])
+        _logger.error(len(result))
+        # _logger.warning(result['email_from'])
+        # _logger.warning(result['reply_to'])
         return result
