8921a111af7a366e572cb575aac75d5b7cae248d
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-03-31 14:32:04 -0400

    Test

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -39,7 +39,7 @@
             ]
     },
 
-    'version': '1.0.921',
+    'version': '1.0.922',
 
     # always loaded
     'data': [
--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -135,6 +135,7 @@ class order(models.Model):
         for product in self.products:
             _logger.warning(str(product.name) + ": " +
                             str(product.product_id.name))
+        print(self.order_lines)
         _logger.error("Renewal")
 
     def _amount_all(self):
