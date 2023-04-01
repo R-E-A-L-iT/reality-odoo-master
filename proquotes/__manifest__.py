f318734702a2d229e936ed70e781550d1e85a181
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-04-01 13:23:53 -0400

    Test

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -39,7 +39,7 @@
             ]
     },
 
-    'version': '1.0.952',
+    'version': '1.0.953',
 
     # always loaded
     'data': [
--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -147,7 +147,7 @@ class order(models.Model):
                     product.formated_label, special="multiple")
                 _logger.warning(dir(self.order_line))
                 _logger.error(type(self.order_line))
-                self.update({'order.lines': [section]})
+                self.update({'order_line': [section]})
 
     def _amount_all(self):
         for order in self:
