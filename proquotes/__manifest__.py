65bfddcb376c599b5f10b919dac135239da9e8cd
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-04-05 09:12:28 -0400

    Test

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -39,7 +39,7 @@
             ]
     },
 
-    'version': '1.0.1238',
+    'version': '1.0.1239',
 
     # always loaded
     'data': [
@@ -59,7 +59,7 @@
         'views/Other/pdf_boxed.xml',
         'views/Other/section_name.xml',
         'views/Other/internal_company_backend.xml',
-        'views/Other/renewal.xml',
+        # 'views/Other/renewal.xml',
         'views/Invoice/invoicePDF.xml',
         'views/Invoice/invoiceBackend.xml',
         'views/Invoice/invoice_lot.xml',
