00aa6dd39c12fdf3d2364ebec25cc95024ab554c
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-04-13 13:38:14 -0400

    Test

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -39,7 +39,7 @@
             ]
     },
 
-    'vesion': '1.0.1409',
+    'vesion': '1.0.1410',
 
     # always loaded
     'data': [
@@ -60,7 +60,7 @@
         'views/Other/section_name.xml',
         'views/Other/internal_company_backend.xml',
         'views/Other/renewal.xml',
-        'views/Other/header_footer.xml',
+        # 'views/Other/header_footer.xml',
         'views/Other/product_backend.xml',
         'views/Invoice/invoicePDF.xml',
         'views/Invoice/invoiceBackend.xml',
