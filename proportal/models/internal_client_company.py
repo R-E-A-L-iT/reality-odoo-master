3dd1344366a0bfafa0a705c957e5987263cb4bc8
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-04-17 15:09:27 -0400

    Test

---
+++ proportal/models/internal_client_company.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import ast
+import base64
+import re
+
+from datetime import datetime, timedelta
+from functools import partial
+from itertools import groupby
+from urllib import request
+
+from odoo import api, fields, models, SUPERUSER_ID, _, tools
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.tools.misc import formatLang, get_lang
+from odoo.osv import expression
+from odoo.tools import float_is_zero, float_compare
+from odoo import models, fields, api
+
+
+class company(models.Model):
+    _inherit = "res.partner"
+
+    def _calc_nick(self):
+        if (self.is_company == True):
+            self.company_nickname = False
+        else:
+            self.company_nickname = "_"
+
+    company_nickname = fields.Char(
+        string="Unique Company Nickname", required=True, default=_calc_nick)
