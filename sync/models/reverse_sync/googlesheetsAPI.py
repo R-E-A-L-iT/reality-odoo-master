88b318669cbaeb6d8b0f4c6b0e37997aa3221958
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-04-19 07:59:13 -0400

    Test

--- sync/models/reverse_sync/googlesheetsAPI.py
+++ sync/models/reverse_sync/googlesheetsAPI.py
@@ -20,6 +20,7 @@ class sheetsAPI:
         creds = sac.from_json_keyfile_dict(auth, scope)
         client = gspread.authorize(creds)
         doc = client.open_by_key(spreadSheetID)
+        _logger.error(dir(doc.worksheets()[0]))
         tabs = list(map(lambda item: item.name, doc.worksheets()))
         _logger.error(tabs)
         if sheetName not in tabs:
