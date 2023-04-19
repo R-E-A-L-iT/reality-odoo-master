5c6eb5c0b8f2fe66c2a2f56f88db687f1240bb7e
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-04-19 08:57:17 -0400

    Test

--- proportal/models/internal_client_company.py
+++ proportal/models/internal_client_company.py
@@ -24,42 +24,49 @@ class company(models.Model):
     _inherit = "res.partner"
 
     def _calc_nick(self):
-        if (self.is_company == True):
+        if self.is_company == True:
             self.company_nickname = False
         else:
             self.company_nickname = "_"
 
     def init_company_nickname(self):
         _logger.error("INIT NICKNAME")
-        for partner in self.env['res.partner'].search([('company_nickname', '=', False)]):
+        for partner in self.env["res.partner"].search(
+            [("company_nickname", "=", False)]
+        ):
             partner.company_nickname = "_"
 
-        for partner in self.env['res.partner'].search([('company_nickname', '=', False), ('active', '=', False)]):
+        for partner in self.env["res.partner"].search(
+            [("company_nickname", "=", False), ("active", "=", False)]
+        ):
             partner.company_nickname = "_"
 
-    company_nickname = fields.Char(
-        string="Unique Company Nickname")
+    company_nickname = fields.Char(string="Unique Company Nickname")
 
     @api.onchange("is_company")
     def type_change(self):
-        if (self.is_company == True and self.company_nickname == "_"):
+        if self.is_company == True and self.company_nickname == "_":
             self.company_nickname = False
-        elif (self.is_company == False and self.company_nickname == False):
+        elif self.is_company == False and self.company_nickname == False:
             self.company_nickname = "_"
 
     @api.onchange("company_nickname")
     def verify_unique(self):
-        if (self.is_company == False):
+        if self.is_company == False:
             return
-        if (self.company_nickname == False):
+        if self.company_nickname == False:
             return
-        if (self.is_company == True and self.company_nickname == "_"):
+        if self.is_company == True and self.company_nickname == "_":
             raise UserError("Company Nickname Cannot Be _")
-        records = self.env['res.partner'].search(
-            [('company_nickname', '=', self.company_nickname)])
-        if (len(records) > 1):
+        self.company_nickname = self.company_nickname.upper()
+        records = self.env["res.partner"].search(
+            [("company_nickname", "=", self.company_nickname)]
+        )
+        if len(records) > 1:
             raise ValidationError(
-                "Company Nickname already used: " + str(self.company_nickname))
-        if (len(records) == 1 and records[0].id != self.id):
+                "Company Nickname already used: " + str(self.company_nickname)
+            )
+        if len(records) == 1 and records[0].id != self.id:
             raise ValidationError(
-                "Company Nickname already used: " + str(self.company_nickname))
+                "Company Nickname already used: " + str(self.company_nickname)
+            )
