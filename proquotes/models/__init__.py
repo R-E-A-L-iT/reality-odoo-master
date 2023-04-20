0638f795334cf11ea2bdc0f45bac8554a055830d
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-04-20 14:44:20 -0400

    Test

--- proquotes/models/__init__.py
+++ proquotes/models/__init__.py
@@ -4,7 +4,8 @@ from . import models
 from . import invoiceModels
 from . import templates
 from . import product
-from . import stock
+
+# from . import stock
 from . import internal_company
 from . import internal_user
 from . import mail
