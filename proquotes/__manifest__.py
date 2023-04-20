7971bbf86412875ce3efe9772c866f21bb4b9312
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-04-20 11:34:13 -0400

    Test

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -44,7 +44,7 @@
             "proquotes/static/src/JS/rental.js",
         ]
     },
-    "version": "1.0.2049",
+    "version": "1.0.2050",
     # always loaded
     "data": [
         "security/ir.model.access.csv",
--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -83,6 +83,7 @@ class invoice(models.Model):
 
     @api.depends("company_id")
     def _get_default_footer(self):
+        _logger.error("Footer")
         company = None
         if self.company_id != False:
             company = self.company_id
