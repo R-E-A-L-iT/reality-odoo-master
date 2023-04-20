393992999e6c6eef43f7405f90a42f7381c291f6
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-04-20 19:42:49 -0400

    Test

--- proportal/__manifest__.py
+++ proportal/__manifest__.py
@@ -1,62 +1,62 @@
 {
-    'name': "ProPortal",
-
-    'summary': """
+    "name": "ProPortal",
+    "summary": """
 		Portal Upgrade Module that adds Advanced Features""",
-
-    'description': """
+    "description": """
 		Module that allows expands Customer Portal
 	""",
-
-    'author': "Ty Cyr",
-
-    'license': "LGPL-3",
-
+    "author": "Ty Cyr",
+    "license": "LGPL-3",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Sales',
-
-    'version': '0.242',
-
+    "category": "Sales",
+    "version": "0.243",
     # any module necessary for this one to work correctly
-    'depends': ['base', 'website', 'stock_account', 'product', 'purchase', 'stock', 'portal', 'website_sale'],
-
-    'assets': {
-            'web.assets_frontend': [
-                'proportal/static/src/CSS/customWebsiteStyle.css',
-                'proportal/static/src/CSS/homepage.css',
-                'proportal/static/src/CSS/calendar.css',
-                'proportal/static/src/CSS/projectPortal.css',
-                'proportal/static/src/CSS/productMiniMenu.css',
-                'proportal/static/src/CSS/header.css',
-                'proportal/static/src/CSS/footer.css',
-                'proportal/static/src/CSS/mobileWebsite.css',
-                'proportal/static/src/CSS/cart.css',
-            ]
+    "depends": [
+        "base",
+        "website",
+        "stock_account",
+        "product",
+        "purchase",
+        "stock",
+        "portal",
+        "website_sale",
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "proportal/static/src/CSS/customWebsiteStyle.css",
+            "proportal/static/src/CSS/homepage.css",
+            "proportal/static/src/CSS/calendar.css",
+            "proportal/static/src/CSS/projectPortal.css",
+            "proportal/static/src/CSS/productMiniMenu.css",
+            "proportal/static/src/CSS/header.css",
+            "proportal/static/src/CSS/footer.css",
+            "proportal/static/src/CSS/mobileWebsite.css",
+            "proportal/static/src/CSS/cart.css",
+        ]
     },
-
     # always loaded
-    'data': [
+    "data": [
         # 'security/ir.model.access.csv',
-        'views/web_base.xml',
-        'views/partnerView.xml',
+        "views/web_base.xml",
+        "views/partnerView.xml",
         # 'views/portalAccess.xml',
-        'views/stockView.xml',
-        'views/productView.xml',
-        'views/customer_portal.xml',
-        'views/portalProject.xml',
-        'views/navButton.xml',
-        'views/navbarCustom.xml',
-        'views/cart.xml',
-        'views/carousel.xml',
-        'views/shopTile.xml',
-        'views/language_selector.xml',
-        'views/companyBackend.xml',
-        'views/partner_internal.xml',
+        "views/stockView.xml",
+        "views/productView.xml",
+        "views/customer_portal.xml",
+        "views/portalProject.xml",
+        "views/navButton.xml",
+        "views/navbarCustom.xml",
+        "views/cart.xml",
+        "views/carousel.xml",
+        "views/shopTile.xml",
+        "views/language_selector.xml",
+        "views/companyBackend.xml",
+        "views/partner_internal.xml",
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+    "demo": [
+        "demo/demo.xml",
     ],
 }
