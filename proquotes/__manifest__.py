3a61845299f02af8c9f1123e5a41acdd76bf3bac
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-04-25 19:47:34 -0400

    Test

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -44,7 +44,7 @@
             "proquotes/static/src/JS/rental.js",
         ]
     },
-    "version": "1.0.2101",
+    "version": "1.0.2102",
     # always loaded
     "data": [
         "security/ir.model.access.csv",
