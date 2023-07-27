7b0cf60188321c72c45fec5636b5a2f0ae08210c
qwertypeople <qwertypeople@yahoo.com>
2023-07-27 14:05:54 -0400

    add costum contac module

---
+++ contact/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Custom Contact',
+    'version': '0.001',
+    'summary': 'Description of your module',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
---
+++ contact/models/models.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import ast
+import base64
+import re
+
+from datetime import datetime, timedelta
+from functools import partial
+from itertools import groupby
+from urllib import request
+
+from odoo import api, fields, models, SUPERUSER_ID, _, tools
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.tools.misc import formatLang, get_lang
+from odoo.osv import expression
+from odoo.tools import float_is_zero, float_compare
+from odoo import models, fields, api
+
+
+class individual(models.Model):
+    _inherit = 'res.partner'
+
+    linkedin_link = fields.One2many(
+        string="LinkedIn link"
+    )   
+
