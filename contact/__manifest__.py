bc5bfc5b05b9e70f5a88eeca782e1a423850b3eb
qwertypeople <qwertypeople@yahoo.com>
2023-07-27 14:42:50 -0400

    missing coma

--- contact/__manifest__.py
+++ contact/__manifest__.py
@@ -1,16 +1,14 @@
 {
-    "name": 'Custom Contact',
-    "summary": "Modifying contact to support custom need."
-    "description": """
-		Module that support LinkedIn link in the contact
-	""",
+    "name": "Custom Contact",
+    "summary": "Modifying contact to support custom need.",
+    "description": "Module that support LinkedIn link in the contact",
     "author": "Olivier Cote",
     "license": "LGPL-3",
-    'version': '0.004',
-    'depends': ['base'],
+    "version": "0.004",
+    "depends": ["base"],
     "data": [
         "views/contact_internal.xml"
     ],
-    'installable': True,
-    'application': True,
+    "installable": True,
+    "application": True,
 }
