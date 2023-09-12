bbb02695a175b1d6b9c38168aa11b43e78dfa5a5
qwertypeople <qwertypeople@yahoo.com>
2023-09-12 14:55:44 -0400

    test

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -45,7 +45,7 @@
         ]
     },
 
-    "version": "1.0.2148",
+    "version": "1.0.2149",
 
     # always loaded
     "data": [
@@ -59,6 +59,7 @@
         "views/Quote/renewalText.xml",
         "views/Quote/quoteRentalAddress.xml",
         "views/Quote/table-align.xml",
+        "views/Quote/testview.xml",
         "views/Other/tax.xml",
         "views/Other/mail.xml",
         "views/Other/deliverPDF.xml",
