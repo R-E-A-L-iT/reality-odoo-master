42e08ce5fa1bbd8106c434d1a6fe5948fb1d8819
qwertypeople <qwertypeople@yahoo.com>
2023-10-22 13:22:06 -0400

    display currency info

--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -411,11 +411,11 @@ class order(models.Model):
     def printTest(self):
         _logger.error("partner_id: " + str(self.partner_id))
         _logger.error("country_id: " + str(self.partner_id.country_id))
-        country = self.env["res.country"].search([("id", "=", "self.partner_id.country_id)")])
+        country = self.env["res.country"].search([("id", "=", self.partner_id.country_id)])
         _logger.error("country.name: " + str(country.name))
         
 
-        currency = self.env["res.currency"].search([("id", "=", "country.currency_id")])
+        currency = self.env["res.currency"].search([("id", "=", country.currency_id)])
         _logger.error("currency.id: " + str(currency.id))
         _logger.error("currency.name: " + str(currency.name))
 
