de7c43032fb57e95a847f71962bc14cb61a6322b
qwertypeople <qwertypeople@yahoo.com>
2023-11-14 18:22:00 -0500

    test8

--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -371,10 +371,10 @@ class order(models.Model):
 
 
     @api.model
-    def odoo_test_comm(self, orderId):#, p_startDateDate, p_endDateDate):
+    def odoo_test_comm(self, orderId, p_startDateValue):#, p_endDateDate):
         _logger.error("----------------odoo_test_comm ----------------------")
-        _logger.error("-!---------------" + str(orderId))
-        #_logger.error("----------------" + str(p_startDateDate))
+        _logger.error("-!-!---------------" + str(orderId))
+        #_logger.error("----------------" + str(p_startDateValue))
         #_logger.error("----------------" + str(p_endDateDate))
         return {
             'name': 'testing name'
