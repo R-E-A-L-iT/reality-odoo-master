5d0b81ce533638d755f989533633b0833d73cd80
qwertypeople <qwertypeople@yahoo.com>
2023-11-15 17:40:25 -0500

    test

--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -439,7 +439,7 @@ class order(models.Model):
     def setRentalDiscount(self):    
         _logger.error("-----------------setRentalDiscount: " + str(self.id))  
         try:        
-            sale_order_lines_head_item, sale_order_lines_to_be_discounted = self.get_rental_headItem_and_kitItems(self.id)
+            sale_order_lines_head_item, sale_order_lines_to_be_discounted = self.get_rental_headItem_and_kitItems()
         except Exception as e:
             _logger.error(str(e))             
 
@@ -463,8 +463,8 @@ class order(models.Model):
     # If it encounteur a line that start with # and that inclunde "RENTAL KIT", 
     # it will put the first item in a list
     # and all the folowing item in another.
-    def get_rental_headItem_and_kitItems(self, sale_order_id):
-        _logger.error("---------------------------------- get_rental_headItem_and_kitItems: "+ str(sale_order_id))
+    def get_rental_headItem_and_kitItems(self):
+        #_logger.error("---------------------------------- get_rental_headItem_and_kitItems: "+ str(sale_order_id))
 
         if(not self.is_rental):
             return
