246bc71ccbf181cfb72dcc60b78682c18c55f881
qwertypeople <qwertypeople@yahoo.com>
2023-11-15 14:24:05 -0500

    changind test DB

--- sync/models/forward_sync/googlesheetsAPI.py
+++ sync/models/forward_sync/googlesheetsAPI.py
@@ -33,7 +33,7 @@ class sheetsAPI(models.Model):
         
         
         # Dev Numbers Set Based on When Developer Joined
-        _master_database_template_id_dev_oli = ("12ZZZuSX0IemfPp8lCr3SYsbHfRzAfMoeo-WcD12Gapc")
+        _master_database_template_id_dev_oli = ("1Tbo0NdMVpva8coych4sgjWo7Zi-EHNdl6EFx2DZ6bJ8")
         _master_database_template_id_dev_zek = ("1PyiopFOHqamiM66tQYB8CFVJ9KN2GIxPHUGaF-33xnU")                
 
 
