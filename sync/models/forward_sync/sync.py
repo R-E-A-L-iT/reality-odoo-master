07382162cd2a9c0f8ce50b67c7c7ea3cf605289b
qwertypeople <qwertypeople@yahoo.com>
2024-03-14 16:34:50 -0400

    imd1.name = l[1]

--- sync/models/forward_sync/sync.py
+++ sync/models/forward_sync/sync.py
@@ -1043,7 +1043,7 @@ class sync(models.Model):
         for l in ccpSkus:
             _logger.info("-------------- " + str(l[0]))
             imd1 = imd.search([("name", "=", l[0])])
-            imd1.sku = l[1]
+            imd1.name = l[1]
 
         _logger.info("-------------- FINISH")
 
