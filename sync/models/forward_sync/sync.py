79b4b08f2e2bd6698de131bd1837d815d6982119
qwertypeople <qwertypeople@yahoo.com>
2024-03-15 11:53:59 -0400

    logging

--- sync/models/forward_sync/sync.py
+++ sync/models/forward_sync/sync.py
@@ -1021,14 +1021,14 @@ class sync(models.Model):
 
         for l in  ccpSkus:
             _logger.info("Looking for: " + str(l[1]))
-            
-            imd1 = imd.search([("name", "ilike", l[1])])  
-            for imd_ in imd1: 
-                pt1 = pt.search([("id", "=", imd_.res_id)]) 
-                if (pt1.name == False): 
-                    formatted_id = str(imd_.id).ljust(20)
-                    formatted_resid = str(imd_.res_id).ljust(20)
-                    _logger.info("ID: " + formatted_id + ", RES_ID: " + formatted_resid + ", NAME: " + str(imd_.name))
+
+            #imd1 = imd.search([("name", "ilike", l[1])])  
+            #for imd_ in imd1: 
+            #    pt1 = pt.search([("id", "=", imd_.res_id)]) 
+            #    if (pt1.name == False): 
+            #        formatted_id = str(imd_.id).ljust(20)
+            #        formatted_resid = str(imd_.res_id).ljust(20)
+            #        _logger.info("ID: " + formatted_id + ", RES_ID: " + formatted_resid + ", NAME: " + str(imd_.name))
                     #deletedIMD.append("ID: " + formatted_id + ", RES_ID: " + formatted_resid + ", NAME: " + str(imd_.name))                    
                     #imd_.unlink() 
 
