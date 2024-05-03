5513535a0d1f4642efdf5bc61a15225a1df28c99
OlivierRealIT <qwertypeople@yahoo.com>
2024-05-03 12:15:03 -0400

    proCRM module

---
+++ procrm/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "ProCRM",
+    "summary": "Modifying contact to support custom need.",
+    "description": """Module that support 
+        -Opportunity Status
+        -Opportunity SN
+    """,
+    "author": "Olivier Cote",
+    "license": "LGPL-3",
+    "version": "0.002",
+    "depends": ["base"],
+    "data": [
+        "views/crm_opportunity.xml"
+    ],
+    "installable": True,
+    "application": True,
+}
