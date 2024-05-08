fbd5a29037066b4f5f18cd359fd567066ac75490
OlivierRealIT <qwertypeople@yahoo.com>
2024-05-08 16:05:31 -0400

    1125

--- sync/models/forward_sync/sync.py
+++ sync/models/forward_sync/sync.py
@@ -1104,7 +1104,7 @@ class sync(models.Model):
         for sale in all_so:            
             i+=1
 
-            if (i < 792):
+            if (i < 875):
                 continue
 
             if (sale.id in (
@@ -1122,7 +1122,8 @@ class sync(models.Model):
                 1382,
                 837,
                 1257,
-                1218)):
+                1218,
+                1125)):
                 continue
 
 
