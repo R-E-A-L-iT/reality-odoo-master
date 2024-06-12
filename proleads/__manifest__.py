9ac76bf4b0d47d475ab23e25e7b27ebc0d3b6cd9
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-06-12 16:37:15 -0400

    Update __manifest__.py

--- proleads/__manifest__.py
+++ proleads/__manifest__.py
@@ -2,5 +2,13 @@
 {
     'name': 'ProLeads',
     'version': '1.1',
-    'description': 'Among other things, adds opportunity notes field for opportunities and adds integration with automatic lead logging with Leica.'
+    'summary': 'Adds automation for lead registration',
+    'description': 'Among other things, adds opportunity notes field for opportunities and adds integration with automatic lead logging with Leica.',
+    'depends': [
+        'base_setup',
+        'contacts'
+    ],
+    'installable': 'true',
+    'application': 'true',
+    'auto_install': 'true'
 }
