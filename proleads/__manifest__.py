da5eed775c96c8ce5b7ee88ad8bd0b8dbb283b33
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-06-12 16:24:01 -0400

    Module creation

---
+++ proleads/__manifest__.py
@@ -0,0 +1,6 @@
+
+{
+    'name': 'ProLeads',
+    'version': '1.1',
+    'description': 'Among other things, adds opportunity notes field for opportunities and adds integration with automatic lead logging with Leica.'
+}
