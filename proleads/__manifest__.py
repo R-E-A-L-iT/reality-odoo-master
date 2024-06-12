73b3a28830eb2a6dc7c382a47eda324eb5a92bd2
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-06-12 17:34:02 -0400

    Update __manifest__.py

--- proleads/__manifest__.py
+++ proleads/__manifest__.py
@@ -3,6 +3,7 @@
     'name': 'ProLeads',
     'author': 'Ezekiel J. deBlois',
     'version': '1.1',
+    "license": "LGPL-3",
     'summary': 'Adds automation for lead registration',
     'description': ' ',
     'depends': ['base',],
