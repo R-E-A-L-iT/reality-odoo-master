f2ade0f8203ab7b7546cde946fffe7c38cedde5f
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-07-09 12:07:37 -0400

    Perhaps it will work now

--- ba_customer_portal_extension/models/customer.py
+++ ba_customer_portal_extension/models/customer.py
@@ -40,22 +40,20 @@ class Contacts(models.Model):
 
     portal_companies = fields.Many2many('res.partner', relation='res_partner_companies_rel', column1='res_partner_id', column2='id', string='Portal Companies', domain=[('active', '=', True), ('is_company', '!=', False)])
 
-    portal_companies_2 = fields.Many2many('res.partner', relation='res_partner_companies_rel', column1='res_partner_id', column2='id', string='Portal Companies 2', domain=[('active', '=', True), ('is_company', '!=', False)])
-
     # contact_relationship = fields.Selection(
     #     [("technical_buyer", "Technical Buyer"), ("economic_buyer", "Economic Buyer"), ("decision_maker", "Decision Maker")]
     # )
 
-    # contact_relationship =  fields.Selection([
-    #     ('technical_buyer','Technical Buyer'),
-    #     ('economic_buyer','Economic Buyer'),
-    #     ('decision_maker','Decision Maker'),
-    #     ('champion','Champion'),
-    #     ('influencer','Influencer'),
-    #     ('end_user','End User'),
-    #     ('blocker','Blocker'),
-    #     ('executive_sponsor','Executive Sponsor'),
-    #     ('legal_compliance','Legal and Compliance'),
-    #     ('budget_holder','Budget Holder'),
-    #     ('other','Other'),
-    #     ],string="Contact Relationship")
+    contact_relationship =  fields.Selection([
+        ('technical_buyer','Technical Buyer'),
+        ('economic_buyer','Economic Buyer'),
+        ('decision_maker','Decision Maker'),
+        ('champion','Champion'),
+        ('influencer','Influencer'),
+        ('end_user','End User'),
+        ('blocker','Blocker'),
+        ('executive_sponsor','Executive Sponsor'),
+        ('legal_compliance','Legal and Compliance'),
+        ('budget_holder','Budget Holder'),
+        ('other','Other'),
+        ],string="Contact Relationship")
