2033d329b121e0f1032e36ac6b1e55992cd239e3
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-07-16 09:58:26 -0400

    ProSite and ProQuotes updates
    Building foundations of ProSite module and attempting to add a new section to PO reports

---
+++ prosite/__manifest__.py
@@ -0,0 +1,16 @@
+
+{
+    'name': 'ProSite',
+    'author': 'Ezekiel J. deBlois',
+    'version': '1.1',
+    "license": "LGPL-3",
+    'summary': 'Reforms the web page creation process and makes the R-E-A-L.iT style consistent.',
+    'description': ' ',
+    'depends': ['base', 'crm'],
+    "data": [
+        "views/looper.xml"
+        "views/snippets.xml"
+    ],
+    'installable': True,
+    'application': True,
+}
