0fca7afcd5ef33bbd532dac0f1330bae803ffccb
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-07-17 11:50:48 -0400

    Configuring default columns

--- proquotes/models/shipping_costs.py
+++ proquotes/models/shipping_costs.py
@@ -5,4 +5,18 @@ class shipping_costs(models.Model):
     
     # name of package
     package_name = fields.Char("Package Name")
+    # item in package
     items = fields.Many2one("product.product", string="Items")
+    # warehouse location
+    warehouse_location = fields.Many2one("stock.warehouse", string="Warehouse Location")
+    # delivery location
+    delivery_location = fields.Many2one("res.partner", string="Delivery Location")
+    
+    _columns = {
+        'package_name': package_name,
+        'items': items,
+        'warehouse_location': warehouse_location,
+        'delivery_location': delivery_location,
+    }
+
+    # get rate button [todo]
