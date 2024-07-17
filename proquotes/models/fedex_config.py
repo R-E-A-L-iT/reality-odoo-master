7e389773c5651dc596525ab5159304a6b550ca21
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-07-17 15:40:28 -0400

    Added FedEx api call

---
+++ proquotes/models/fedex_config.py
@@ -0,0 +1,9 @@
+from fedex.config import FedexConfig
+
+# Change these values to match your testing account/meter number.
+CONFIG_OBJ = FedexConfig(key='Y7sYXAat3mbnjkzs',
+                         password='r02Q3lIgB0TuqSoOctdO7eA0y',
+                         account_number='510087100',
+                         meter_number='	256572651',
+                        #  freight_account_number='xxxxxxxxxxx',
+                         use_test_server=True)
--- proquotes/models/shipping_costs.py
+++ proquotes/models/shipping_costs.py
@@ -1,4 +1,15 @@
+import logging
+import binascii
+import datetime
+import sys
 from odoo import models, fields
+
+from fedex_config import FEDEX_CREDENTIALS
+from fedex.services.ship_service import FedexProcessShipmentRequest
+from fedex.tools.conversion import sobject_to_dict
+
+
+# https://github.com/python-fedex-devs/python-fedex examples
 
 class shipping_costs(models.Model):
     _name = "proquotes.shipping_costs"
@@ -11,6 +22,108 @@ class shipping_costs(models.Model):
     warehouse_location = fields.Many2one("stock.warehouse", string="Warehouse Location")
     # delivery location
     delivery_location = fields.Many2one("res.partner", string="Delivery Location")
+    
+    
+    # debug
+    
+    _logger.info("")
+    
+    _logger.info(self.env["shipping_costs.items"])
+    
+    
+    #
+    #   items in package: list of odoo product objects
+    #   start_address: odoo contact object of r-e-a-l.it address it's shipped from
+    #   finish_address: odoo contact object of customer's address it's shipped to
+    #   units: string "KG" or "LB" only
+    #
+    def getShipmentRate(items_in_package, start_address, finish_address, units):
+        
+        rate_request = FedexRateServiceRequest(CONFIG_OBJ)
+        
+        # settings
+        rate_request.RequestedShipment.DropoffType = 'REGULAR_PICKUP'
+        rate_request.RequestedShipment.ServiceType = 'FEDEX_GROUND'
+        # not fedex box because we package it
+        rate_request.RequestedShipment.PackagingType = 'YOUR_PACKAGING'
+        
+        # warehouse address
+        state_id = start_address.state_id
+        country_id = start_address.country_id
+        postal_code = start_address.zip
+        
+        rate_request.RequestedShipment.Shipper.Address.StateOrProvinceCode = state_id
+        rate_request.RequestedShipment.Shipper.Address.PostalCode = postal_code
+        rate_request.RequestedShipment.Shipper.Address.CountryCode = country_id
+        # might need to make this configurable
+        rate_request.RequestedShipment.Shipper.Address.Residential = False
+        
+        # delivery address
+        state_id = finish_address.state_id
+        country_id = finish_address.country_id
+        postal_code = finish_address.zip
+        
+        rate_request.RequestedShipment.Recipient.Address.StateOrProvinceCode = state_id
+        rate_request.RequestedShipment.Recipient.Address.PostalCode = postal_code
+        rate_request.RequestedShipment.Recipient.Address.CountryCode = country_id
+        # This is needed to ensure an accurate rate quote with the response.
+        # rate_request.RequestedShipment.Recipient.Address.Residential = True
+        # include estimated duties and taxes in rate quote, can be ALL or NONE  
+        rate_request.RequestedShipment.EdtRequestType = 'NONE'
+        
+        rate_request.RequestedShipment.ShippingChargesPayment.PaymentType = 'RECIPIENT'
+        
+        packageWeight = 0
+        
+        for item in items_in_package:
+            
+            packageWeight += item.weight
+            
+        fedex_weight = rate_request.create_wsdl_object_of_type('Weight')
+        fedex_weight.Value = packageWeight
+        fedex_weight.Units = units
+        
+        package = rate_request.create_wsdl_object_of_type('RequestedPackageLineItem')
+        package.Weight = fedex_weight
+        
+        package1.PhysicalPackaging = 'BOX'
+        
+        # Required, but according to FedEx docs:
+        # "Used only with PACKAGE_GROUPS, as a count of packages within a
+        # group of identical packages". In practice you can use this to get rates
+        # for a shipment with multiple packages of an identical package size/weight
+        # on rate request without creating multiple RequestedPackageLineItem elements.
+        # You can OPTIONALLY specify a package group:
+        # package1.GroupNumber = 0  # default is 0
+        # The result will be found in RatedPackageDetail, with specified GroupNumber.
+        package1.GroupPackageCount = 1
+        # Un-comment this to see the other variables you may set on a package.
+        # print(package1)
+        
+        rate_request.add_package(package)
+        rate_request.send_request()
+        
+        rate = rate_request.response
+        
+        # This will show the reply to your rate_request being sent. You can access the
+        # attributes through the response attribute on the request object. This is
+        # good to un-comment to see the variables returned by the FedEx reply.
+        # print(rate_request.response)
+
+        # This will convert the response to a python dict object. To
+        # make it easier to work with.
+        # from fedex.tools.conversion import basic_sobject_to_dict
+        # print(basic_sobject_to_dict(rate_request.response))
+
+        # This will dump the response data dict to json.
+        # from fedex.tools.conversion import sobject_to_json
+        # print(sobject_to_json(rate_request.response))
+        
+        return rate
+    
+    def createShipment(weights, start_address, finish_address):
+        return
+    
     
     # _columns = {
     #     'package_name': fields.Char("Package Name"),
