f10b5760d0a3c31ba7495385efa4a72b4bbf044f
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-07-17 12:16:24 -0400

    items one2many -> many2many

--- proquotes/models/shipping_costs.py
+++ proquotes/models/shipping_costs.py
@@ -6,17 +6,17 @@ class shipping_costs(models.Model):
     # name of package
     package_name = fields.Char("Package Name")
     # item in package
-    items = fields.Many2one("product.product", string="Items")
+    items = fields.Many2many("product.product", string="Items")
     # warehouse location
     warehouse_location = fields.Many2one("stock.warehouse", string="Warehouse Location")
     # delivery location
     delivery_location = fields.Many2one("res.partner", string="Delivery Location")
     
-    _columns = {
-        'package_name': fields.Char("Package Name"),
-        'items': fields.Many2one("product.product", string="Items"),
-        'warehouse_location': fields.Many2one("stock.warehouse", string="Warehouse Location"),
-        'delivery_location': fields.Many2one("res.partner", string="Delivery Location"),
-    }
+    # _columns = {
+    #     'package_name': fields.Char("Package Name"),
+    #     'items': fields.Many2one("product.product", string="Items"),
+    #     'warehouse_location': fields.Many2one("stock.warehouse", string="Warehouse Location"),
+    #     'delivery_location': fields.Many2one("res.partner", string="Delivery Location"),
+    # }
 
     # get rate button [todo]
