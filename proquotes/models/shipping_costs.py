2fc13b710860dd417ef0ce2089aaaf09d8c78a3c
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-07-17 11:54:40 -0400

    Update shipping_costs.py

--- proquotes/models/shipping_costs.py
+++ proquotes/models/shipping_costs.py
@@ -13,10 +13,10 @@ class shipping_costs(models.Model):
     delivery_location = fields.Many2one("res.partner", string="Delivery Location")
     
     _columns = {
-        'package_name': package_name,
-        'items': items,
-        'warehouse_location': warehouse_location,
-        'delivery_location': delivery_location,
+        'package_name': fields.Char("Package Name"),
+        'items': fields.Many2one("product.product", string="Items"),
+        'warehouse_location': fields.Many2one("stock.warehouse", string="Warehouse Location"),
+        'delivery_location': fields.Many2one("res.partner", string="Delivery Location"),
     }
 
     # get rate button [todo]
