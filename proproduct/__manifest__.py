ae833f88dd6f84d13e2b4e860d75655ad3de3332
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-07-18 16:05:10 -0400

    Added proproduct module

---
+++ proproduct/__manifest__.py
@@ -0,0 +1,15 @@
+
+{
+    'name': 'ProProduct',
+    'author': 'Ezekiel J. deBlois',
+    'version': '1.1',
+    "license": "LGPL-3",
+    'summary': 'Adds custom fields to the product menu and enhances some eccomerce functions',
+    'description': ' ',
+    'depends': ['base', 'crm', 'product'],
+    "data": [
+        "views/productBackend.xml"
+    ],
+    'installable': True,
+    'application': True,
+}
