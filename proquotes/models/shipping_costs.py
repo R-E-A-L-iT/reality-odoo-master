636b733802590ed20f8e1ab1fa53df1bc1d591ef
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-08-12 10:46:20 -0400

    Update shipping_costs.py

--- proquotes/models/shipping_costs.py
+++ proquotes/models/shipping_costs.py
@@ -5,7 +5,9 @@ import sys
 
 from odoo import models, fields
 
-import fedex-python as fedex
+# import fedex-python as fedex
+import importlib  
+fedex = importlib.import_module("fedex-python")
 
 # from . import fedex_config
 from fedex.services.ship_service import FedexProcessShipmentRequest
