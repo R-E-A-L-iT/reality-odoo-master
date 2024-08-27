a8cd92b9e5a2ae5fdfe766fc70d7d3d2c407a5bd
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-08-27 14:10:56 -0400

    Update models.py

--- proportal/models/models.py
+++ proportal/models/models.py
@@ -191,7 +191,7 @@ class AppointmentView(models.Model):
                 return False
     
     def removeConflictingTimes():
-        for slot in slot_ids:
+        for slot in self.slot_ids:
             for event in self.env['calendar.event'].sudo().search([]):
                 if overlap(slot.start_datetime, slot.end_datetime, event.start, event.stop):
                     _logger.info("CALENDAR EVENT OVERLAP: TRUE")
