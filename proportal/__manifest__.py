62f94594a46ac15fd4b3eb02fe20918ba9d63cdf
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-08-29 13:26:53 -0400

    added text

--- proportal/__manifest__.py
+++ proportal/__manifest__.py
@@ -5,7 +5,7 @@
     "description": """
 		Module that allows expands Customer Portal
 	""",
-    "author": "Ty Cyr",
+    "author": "Ty Cyr & Ezekiel J. deBlois",
     "license": "LGPL-3",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
