808879cd50a37c4e515533655ba96a4c93c12dc4
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-10-07 11:19:53 -0400

    Update models.py

--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -679,7 +679,7 @@ class order(models.Model):
     def _onchange_pricelist_id(self):
         if self.pricelist_id:
             # Check if the selected pricelist contains the word "RENTAL"
-            if 'rental' in self.pricelist_id.name.lower():
+            if 'RENTAL' in self.pricelist_id.name.upper():
                 self.is_rental = True
             else:
                 self.is_rental = False
@@ -688,7 +688,7 @@ class order(models.Model):
     def _onchange_sale_order_template_id(self):
         if self.sale_order_template_id:
             # Check if the selected quote template contains the word "RENTAL"
-            if 'RENTAL' in self.sale_order_template_id.name.lower():
+            if 'RENTAL' in self.sale_order_template_id.name.upper():
                 self.is_rental = True
             else:
                 self.is_rental = False
