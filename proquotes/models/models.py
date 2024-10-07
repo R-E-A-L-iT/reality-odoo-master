921f4251e4f8a9629fe49d2486cb83b4d800a117
harsh-braincrewapps <146069912+harsh-braincrewapps@users.noreply.github.com>
2024-10-07 16:29:25 +0530

    Add latest code for quote links in emails issue.

--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -1086,6 +1086,55 @@ class order(models.Model):
             )
             _logger.info('>>>>>>>>>>>>>>>>. order.tax_totals: %s,', order.tax_totals)
 
+    def _notify_get_recipients_groups(self, message, model_description, msg_vals=None):
+        """ Give access button to users and portal customer as portal is integrated
+        in sale. Customer and portal group have probably no right to see
+        the document so they don't have the access button. """
+        groups = super()._notify_get_recipients_groups(
+            message, model_description, msg_vals=msg_vals
+        )
+        if not self:
+            return groups
+
+        self.ensure_one()
+        if self._context.get('proforma'):
+            for group in [g for g in groups if g[0] in ('portal_customer', 'portal', 'follower', 'customer')]:
+                group[2]['has_button_access'] = False
+            return groups
+
+        local_msg_vals = dict(msg_vals or {})
+        try:
+            customer_portal_group = next(group for group in groups if group[0] == 'portal_customer')
+        except StopIteration:
+            pass
+        else:
+            access_opt = customer_portal_group[2].setdefault('button_access', {})
+            is_tx_pending = self.get_portal_last_transaction().state == 'pending'
+            if self._has_to_be_signed():
+                if self._has_to_be_paid():
+                    access_opt['title'] = _("View Quotation") if is_tx_pending else _("Sign & Pay Quotation")
+                else:
+                    access_opt['title'] = _("Accept & Sign Quotation")
+            elif self._has_to_be_paid() and not is_tx_pending:
+                access_opt['title'] = _("Accept & Pay Quotation")
+            elif self.state in ('draft', 'sent'):
+                access_opt['title'] = _("View Quotation")
+
+            base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+            access_opt['url'] = base_url + self.get_portal_url()
+
+        follower_group = next(group for group in groups if group[0] == 'follower')
+        follower_group[2]['active'] = True
+        follower_group[2]['has_button_access'] = False
+        access_opt = follower_group[2].setdefault('button_access', {})
+        if self.state in ('draft', 'sent'):
+            access_opt['title'] = _("View Quotation")
+        else:
+            access_opt['title'] = _("View Order")
+        access_opt['url'] = base_url + self.get_portal_url()
+        email = base_url + self.get_portal_url()
+        return groups
+
     def _amount_all(self):
         # Ensure sale order lines are selected to included in calculation
         for order in self:
