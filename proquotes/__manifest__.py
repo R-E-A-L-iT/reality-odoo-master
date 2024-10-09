b8d7a37ad88f87650a98ebb177ab14c51f59db78
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-10-09 11:41:43 -0400

    Update __manifest__.py

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -87,6 +87,7 @@
         "views/PO/PO_Frontend.xml",
         "views/PO/PO_Backend.xml",
         "views/PO/PO_PDF.xml",
+        "views/Other/quote_mail_link.xml",
         #         'models/quoteNotify.py'
     ],
     # only loaded in demonstration mode
