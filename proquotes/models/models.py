9d5d3a0148f121679df2ee9629d3f6d8e7193a8e
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-10-09 13:15:35 -0400

    Update models.py

--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -1216,7 +1216,7 @@ class order(models.Model):
                 'partner_id': self.partner_id.id,
                 'email': self.partner_id.email,
                 'name': self.partner_id.name,
-                'type': 'user',
+                'type': 'customer',
                 'is_follower': False,
             }
 
