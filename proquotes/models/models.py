224087b0ce66c51eacc423408385b17c56fd9210
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-10-10 14:07:49 -0400

    Update models.py

--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -1408,7 +1408,7 @@ class MailComposeMessage(models.TransientModel):
             # set recipients
             order = self.env['sale.order'].search([('id', '=', self.env.context.get('default_res_id'))], limit=1)
             if order and order.email_contacts:
-                res['partner_ids'] = [(6, 0, order.email_contacts.ids)]
+                res['partner_ids'] = [(4, order.user_id)]
         
         return res
     
