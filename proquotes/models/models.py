40de51db0f74e2ae9ea14944ea66a615c4fb99a5
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-10-18 16:55:35 -0400

    Changed phases for opportunity and fixed bug with adding contacts

--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -710,8 +710,9 @@ class order(models.Model):
     @api.onchange('email_contacts')
     def _onchange_email_contacts(self):
         for contact in self.email_contacts:
-            if contact not in self.partner_ids:
-                self.partner_ids.append(contact.id)
+            if self.partner_ids:
+                if contact not in self.partner_ids:
+                    self.partner_ids.append(contact.id)
 
     @api.onchange('partner_id')
     def _onchange_partner_id(self):
