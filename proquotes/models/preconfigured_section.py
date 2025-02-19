65c0535a119b4ac21427e326f19b34599a2efd07
harsh-braincrewapps <146069912+harsh-braincrewapps@users.noreply.github.com>
2025-02-19 14:00:07 +0530

    Add latest code for pre-configured sections.

--- proquotes/models/preconfigured_section.py
+++ proquotes/models/preconfigured_section.py
@@ -1,37 +1,49 @@
 from odoo import models, fields, api
 
+
 class PreconfiguredSection(models.Model):
     _name = 'preconfigured.section'
     _description = 'Preconfigured Sections'
+    _rec_name = 'section_name'
 
     section_name = fields.Char(string='Section Name', required=True)
     section_description = fields.Text(string='Section Description')
-    product_ids = fields.Many2many('product.product', string='Products')
+    product_line_ids = fields.One2many('preconfigured.section.line', 'section_id')
     number_of_products = fields.Integer(string='Number of Products', compute='_compute_number_of_products')
 
-    @api.depends('product_ids')
+    @api.depends('product_line_ids')
     def _compute_number_of_products(self):
         for record in self:
-            record.number_of_products = len(record.product_ids)
+            record.number_of_products = 0
+            record.number_of_products = len(record.product_line_ids)
+
 
 class PreconfiguredSectionLine(models.Model):
     _name = 'preconfigured.section.line'
     _description = 'Preconfigured Section Line'
+    _rec_name = 'product_name'
 
-    section_id = fields.Many2one('preconfigured.section', string='Preconfigured Section', required=True, ondelete='cascade')
+    section_id = fields.Many2one('preconfigured.section', string='Preconfigured Section')
     product_id = fields.Many2one('product.product', string='Product', required=True)
-    name = fields.Char(string='Product Name', related='product_id.name', store=True, readonly=True)
+    product_name = fields.Char(string='Name')
     optional = fields.Boolean(string='Optional')
     selected = fields.Boolean(string='Selected', default=True)
     quantity_locked = fields.Boolean(string='Quantity Locked')
-    price_unit = fields.Float(string='Unit Price', related='product_id.list_price', store=True, readonly=True)
+    price_unit = fields.Float(string='Unit Price')
 
+    @api.onchange('product_id')
+    def _onchange_product_name(self):
+        if self.product_id:
+            self.product_name = self.product_id.name
+        else:
+            self.product_name = False
 
 
 class PreconfigSaleOrder(models.Model):
     _inherit = 'sale.order'
 
     preconfigured_section_ids = fields.Many2many('preconfigured.section', string='Preconfigured Sections')
+
 
 class PreconfigSaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
