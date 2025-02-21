c16f03fc3e4d6eb7a5ca26e4bca64e6637621d93
harsh-braincrewapps <146069912+harsh-braincrewapps@users.noreply.github.com>
2025-02-21 12:51:36 +0530

    Add latest code for Add notifications by email for live chat sessions starting. and Unselected items still appear on documents created from sales order.

--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -1673,6 +1673,55 @@ class orderLineProquotes(models.Model):
         required=True,
         help="Field to Lock Quantity on Products",
     )
+    is_optional = fields.Boolean(
+        required=True, string="Optional",
+        help="Field to Mark Product as Optional",
+    )
+    is_selected = fields.Boolean(
+        required=True, string="Selected",
+        help="Field to Mark Wether Customer has Selected Product",
+    )
+    is_quantityLocked = fields.Boolean(
+        string="Lock Quantity",
+        required=True,
+        help="Field to Lock Quantity on Products",
+    )
+
+    demo_selected = fields.Boolean(string="Selected", compute="_check_selected_line",
+                                   help="Field to Mark Wether Customer has Selected Product",
+                                   )
+
+    @api.onchange('is_selected', 'is_quantityLocked', 'is_optional')
+    def _onchange_selected_line(self):
+        if self.is_selected:
+            self.selected = 'true'
+        else:
+            self.selected = 'false'
+        if self.quantityLocked == 'yes':
+            self.is_quantityLocked = True
+        else:
+            self.is_quantityLocked = False
+        if self.optional == 'yes':
+            self.is_optional = True
+        else:
+            self.is_optional = False
+
+    def _check_selected_line(self):
+        for rec in self:
+            rec.demo_selected = False
+            rec.is_quantityLocked = False
+            if rec.selected == 'true':
+                rec.is_selected = True
+            else:
+                rec.is_selected = False
+            if rec.optional == 'yes':
+                rec.is_optional = True
+            else:
+                rec.is_optional = False
+            if rec.quantityLocked == 'yes':
+                rec.is_quantityLocked = True
+            else:
+                rec.is_quantityLocked = False
 
     def get_applied_name(self):
         return True
