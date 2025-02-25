4eaf92b051f5144cffab6fdaba903fc5d5d3d442
harsh-braincrewapps <146069912+harsh-braincrewapps@users.noreply.github.com>
2025-02-25 14:35:39 +0530

    Add latest code for Restrict Timesheets.

--- ba_timesheet_restrict/models/account_analytic_line.py
+++ ba_timesheet_restrict/models/account_analytic_line.py
@@ -49,13 +49,9 @@ class AccountAnalyticLine(models.Model):
             if allowed_past_days > 0:
                 min_date = today - timedelta(days=allowed_past_days)
                 if record.date < min_date:
-                    raise exceptions.ValidationError(
-                        f"You can only log timesheets up to {allowed_past_days} days in the past."
-                    )
+                    raise ValidationError(_(f"You can only log timesheets up to {allowed_past_days} days in the past."))
 
             if allowed_future_days > 0:
                 max_date = today + timedelta(days=allowed_future_days)
                 if record.date > max_date:
-                    raise exceptions.ValidationError(
-                        f"You can only log timesheets up to {allowed_future_days} days in the future."
-                    )
+                    raise ValidationError(_(f"You can only log timesheets up to {allowed_future_days} days in the future."))
