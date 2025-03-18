d15ee313ab3684a4ae2208150347cc2573f162ae
harsh-braincrewapps <146069912+harsh-braincrewapps@users.noreply.github.com>
2025-03-18 19:00:12 +0530

    Add latest code for Invoices don't autofill only selected items.

--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -23,7 +23,9 @@ from collections import defaultdict
 from odoo.http import request
 from odoo.http import Response as Responseht
 from odoo.http import FutureResponse as FutureResponseht
-
+from odoo.fields import Command
+from odoo.osv import expression
+from odoo.tools import float_is_zero, float_compare, float_round, format_date, groupby
 from .translation import name_translation
 from odoo.tools import (
     clean_context, config, CountingStream, date_utils, discardattr,
@@ -1786,6 +1788,48 @@ class orderLineProquotes(models.Model):
                 'price_tax': amount_tax,
                 'price_total': amount_untaxed + amount_tax,
             })
+
+
+    def _prepare_invoice_line(self, **optional_values):
+        """Prepare the values to create the new invoice line for a sales order line.
+
+        :param optional_values: any parameter that should be added to the returned invoice line
+        :rtype: dict
+        """
+        if self.is_selected:
+            self.ensure_one()
+            res = {
+                'display_type': self.display_type or 'product',
+                'sequence': self.sequence,
+                'name': self.name,
+                'product_id': self.product_id.id,
+                'product_uom_id': self.product_uom.id,
+                'quantity': self.qty_to_invoice,
+                'discount': self.discount,
+                'price_unit': self.price_unit,
+                'tax_ids': [Command.set(self.tax_id.ids)],
+                'sale_line_ids': [Command.link(self.id)],
+                'is_downpayment': self.is_downpayment,
+            }
+        else:
+            res = {}
+        analytic_account_id = self.order_id.analytic_account_id.id
+        if self.analytic_distribution and not self.display_type:
+            res['analytic_distribution'] = self.analytic_distribution
+        if analytic_account_id and not self.display_type:
+            analytic_account_id = str(analytic_account_id)
+            if 'analytic_distribution' in res:
+                res['analytic_distribution'][analytic_account_id] = res['analytic_distribution'].get(
+                    analytic_account_id, 0) + 100
+            else:
+                res['analytic_distribution'] = {analytic_account_id: 100}
+        if optional_values:
+            res.update(optional_values)
+        if self.display_type:
+            res['account_id'] = False
+        return res
+
+
 
 class proquotesMail(models.TransientModel):
     _inherit = "mail.compose.message"
