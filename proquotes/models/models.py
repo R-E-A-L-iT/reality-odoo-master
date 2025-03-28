45733379b747d2cc91d28acf71ff8a0fb2a0153d
harsh-braincrewapps <146069912+harsh-braincrewapps@users.noreply.github.com>
2025-03-28 13:23:15 +0530

    Add latest code for  Invoices don't autofill only selected items.

--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -832,30 +832,14 @@ class order(models.Model):
     )
 
     def _get_invoiceable_lines(self, final=False):
-        """Return the invoiceable lines for order `self`."""
         down_payment_line_ids = []
         invoiceable_line_ids = []
         pending_section = None
         precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
-
-        for line in self.order_line:
-            if line.is_selected:
-                if line.display_type == 'line_section':
-                    # Only invoice the section if one of its lines is invoiceable
-                    pending_section = line
-                    continue
-                if line.display_type != 'line_note' and float_is_zero(line.qty_to_invoice, precision_digits=precision):
-                    continue
-                if line.qty_to_invoice > 0 or (line.qty_to_invoice < 0 and final) or line.display_type == 'line_note':
-                    if line.is_downpayment:
-                        # Keep down payment lines separately, to put them together
-                        # at the end of the invoice, in a specific dedicated section.
-                        down_payment_line_ids.append(line.id)
-                        continue
-                    if pending_section:
-                        invoiceable_line_ids.append(pending_section.id)
-                        pending_section = None
-                    invoiceable_line_ids.append(line.id)
+        is_selected_sales_order = self.order_line.search(
+            [('is_selected', '=', True), ('order_id', '=', self.id), ('product_id', '!=', False)])
+        for line in is_selected_sales_order:
+            invoiceable_line_ids.append(line.id)
 
         return self.env['sale.order.line'].browse(invoiceable_line_ids + down_payment_line_ids)
     
