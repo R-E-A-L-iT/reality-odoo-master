8ea064ec1102c99d06ea8e29a9dbd5334da42276
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-04-07 15:53:24 -0700

    Remove product description field.

--- proproduct/models/product_bundle.py
+++ proproduct/models/product_bundle.py
@@ -8,18 +8,7 @@ class ProductBundleLine(models.Model):
 
     bundle_id = fields.Many2one('product.bundle', string='Bundle', required=True, ondelete='cascade')
     product_id = fields.Many2one('product.template', string='Product', required=True)
-    # product_description = fields.Text(string='Sales Description', related='product_id.description_sale', store=True)
     quantity = fields.Float(string='Quantity', default=1.0)
-
-    product_description = fields.Text(string='Description', compute='_compute_product_description', store=True)
-
-    @api.depends('product_id')
-    def _compute_product_description(self):
-        for line in self:
-            if line.product_id:
-                line.product_description = line.product_id.name or ''
-            else:
-                line.product_description = ''
 
 
 class ProductBundle(models.Model):
