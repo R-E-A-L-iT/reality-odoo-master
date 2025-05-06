ae79f89613ca1f1b8e3cad04880d4c6b47e2f40d
harsh-braincrewapps <146069912+harsh-braincrewapps@users.noreply.github.com>
2025-05-06 12:32:04 +0530

    Add latest code for Can't proceed with creating/sending invoice without assigned company email.

--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -613,7 +613,19 @@ class invoice(models.Model):
         string="Footer OLD",
         help="Footer selection field",
     )
-    
+
+    @api.model
+    def create(self, vals):
+        res = super(invoice, self).create(vals)
+        if res.partner_id and not res.partner_id.email:
+            raise UserError("Your company must have an email address set.")
+        return res
+
+    @api.onchange('partner_id')
+    def _onchange_partner_email_check(self):
+        if self.partner_id and not self.partner_id.email:
+            raise UserError("Your company must have an email address set.")
+
     def get_translated_term(self, title, lang):
         if "translate" in title:
 
