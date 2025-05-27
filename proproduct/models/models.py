a6782613e85f9e750d545bb89130936dfb39fb1f
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-05-27 09:28:20 -0400

    Update add bundle wizard and function

--- proproduct/models/models.py
+++ proproduct/models/models.py
@@ -12,3 +12,19 @@ class StockLot(models.Model):
         string="Bundle Instance",
         help="Optional link to a product bundle instance"
     )
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    def action_open_bundle_wizard(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'product.bundle.wizard',
+            'view_mode': 'form',
+            'target': 'new',
+            'context': {
+                'default_model': self._name,
+                'default_res_id': self.id,
+            }
+        }
+
--- proproduct/models/product_bundle_wizard.py
+++ proproduct/models/product_bundle_wizard.py
@@ -6,50 +6,91 @@ class ProductBundleWizard(models.TransientModel):
 
     bundle_id = fields.Many2one('product.bundle', string='Bundle to Add', required=True)
 
-    def action_add_bundle_to_quote(self):
-        sale_order = self.env['sale.order'].browse(self.env.context.get('active_id'))
+    @api.model
+    def action_add_bundle_to_document(self):
+        
+        active_model = self._context.get('default_model')
+        active_id = self._context.get('default_res_id')
+        doc = self.env[active_model].browse(active_id)
 
-        # Determine bundle price based on pricelist and rental status
-        currency = sale_order.pricelist_id.currency_id
-        is_rental = getattr(sale_order, 'is_rental', False)  # handle case where field may not exist
+        # Insert section + sub lines based on bundle
+        if active_model == 'sale.order':
 
-        if is_rental:
-            price = self.bundle_id.rental_price_usd if currency.name == 'USD' else self.bundle_id.rental_price_cad
-        else:
-            price = self.bundle_id.price_usd if currency.name == 'USD' else self.bundle_id.price_cad
 
-        # Construct section title
-        section_title = f"#bundle+{self.bundle_id.name}+{self.bundle_id.id}"
+            # Determine bundle price based on pricelist and rental status
+            currency = sale_order.pricelist_id.currency_id
+            is_rental = getattr(sale_order, 'is_rental', False)  # handle case where field may not exist
 
-        # Add the bundle section line
-        section_line = sale_order.order_line.create({
-            'order_id': sale_order.id,
-            'display_type': 'line_section',
-            'name': section_title,
-        })
+            if is_rental:
+                price = self.bundle_id.rental_price_usd if currency.name == 'USD' else self.bundle_id.rental_price_cad
+            else:
+                price = self.bundle_id.price_usd if currency.name == 'USD' else self.bundle_id.price_cad
 
-        # Add the bundle helper pricing line using the dummy product
-        bundle_price_line = sale_order.order_line.create({
-            'order_id': sale_order.id,
-            'product_id': 564922,  # Dummy product ID
-            'product_uom_qty': 1,
-            'price_unit': price,
-            'name': f"$$bundle_helper$$:{self.bundle_id.name}",
-            'sequence': section_line.sequence + 1,
-            'is_selected': True,
-        })
+            # Construct section title
+            section_title = f"#bundle+{self.bundle_id.name}+{self.bundle_id.id}"
 
-        # Add actual product lines
-        sequence = bundle_price_line.sequence + 1
-        for line in self.bundle_id.product_lines:
-            sale_order.order_line.create({
+            # Add the bundle section line
+            section_line = sale_order.order_line.create({
                 'order_id': sale_order.id,
-                'product_id': line.product_id.product_variant_id.id,
-                'product_uom_qty': line.quantity,
-                'name': line.product_id.name,
-                'sequence': sequence,
-                'is_selected': False,
+                'display_type': 'line_section',
+                'name': section_title,
             })
-            sequence += 1
 
-        return {'type': 'ir.actions.act_window_close'}
+            # Add the bundle helper pricing line using the dummy product
+            bundle_price_line = sale_order.order_line.create({
+                'order_id': sale_order.id,
+                'product_id': 564922,  # Dummy product ID
+                'product_uom_qty': 1,
+                'price_unit': price,
+                'name': f"$$bundle_helper$$:{self.bundle_id.name}",
+                'sequence': section_line.sequence + 1,
+                'is_selected': True,
+            })
+
+            # Add actual product lines
+            sequence = bundle_price_line.sequence + 1
+            for line in self.bundle_id.product_lines:
+                sale_order.order_line.create({
+                    'order_id': sale_order.id,
+                    'product_id': line.product_id.product_variant_id.id,
+                    'product_uom_qty': line.quantity,
+                    'name': line.product_id.name,
+                    'sequence': sequence,
+                    'is_selected': False,
+                })
+                sequence += 1
+
+            return {'type': 'ir.actions.act_window_close'}
+
+
+        elif active_model == 'account.move':
+
+            # Insert into invoice lines
+            doc.invoice_line_ids = [(0, 0, {
+                'name': self.bundle_id.name,
+                'display_type': 'line_section',
+            })]
+            for line in self.bundle_id.product_lines:
+                doc.invoice_line_ids += [(0, 0, {
+                    'product_id': line.product_id.id,
+                    'quantity': line.quantity,
+                    'price_unit': line.product_id.list_price,
+                    'name': line.product_id.name,
+                })]
+
+
+        elif active_model == 'purchase.order':
+
+            # Insert into PO lines
+            doc.order_line = [(0, 0, {
+                'name': self.bundle_id.name,
+                'display_type': 'line_section',
+            })]
+            for line in self.bundle_id.product_lines:
+                doc.order_line += [(0, 0, {
+                    'product_id': line.product_id.id,
+                    'product_qty': line.quantity,
+                    'price_unit': line.product_id.standard_price,
+                    'name': line.product_id.name,
+                })]
+
