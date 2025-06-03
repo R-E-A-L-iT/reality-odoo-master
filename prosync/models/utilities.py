8e2d8cab407ead4e9ab91626ae8064e0d0e6cee7
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-06-03 18:12:07 -0400

    Update utilities.py

--- prosync/models/utilities.py
+++ prosync/models/utilities.py
@@ -22,6 +22,7 @@ class utilities:
     #   many2one, many2many, one2many
 
 
+
     # char normalization function
     def normalize_char(self, value):
         _logger.log("ProSync | Normalizing char value: %s", value)
@@ -34,6 +35,38 @@ class utilities:
     def normalize_html(self, value):
         _logger.log("ProSync | Normalizing html value: %s", value)
 
+
+
     # integer normalization function
     def normalize_integer(self, value):
         _logger.log("ProSync | Normalizing integer value: %s", value)
+
+    # float normalization function
+    def normalize_float(self, value):
+        _logger.log("ProSync | Normalizing float value: %s", value)
+
+    # integer normalization function
+    def normalize_boolean(self, value):
+        _logger.log("ProSync | Normalizing boolean value: %s", value)
+
+
+
+    # date normalization function
+    def normalize_date(self, value):
+        _logger.log("ProSync | Normalizing date value: %s", value)
+
+    # datetime normalization function
+    def normalize_datetime(self, value):
+        _logger.log("ProSync | Normalizing datetime value: %s", value)
+
+    # monetary normalization function
+    def normalize_monetary(self, value):
+        _logger.log("ProSync | Normalizing monetary value: %s", value)
+
+    # binary normalization function
+    def normalize_binary(self, value):
+        _logger.log("ProSync | Normalizing binary value: %s", value)
+
+
+
+    # in the future maybe add limited support for Many2one, Many2many, One2many fields (like product_type for example)
