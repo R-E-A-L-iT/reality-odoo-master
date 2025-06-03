b5c95b20cbc6a0b520cd7dee54542a82dc81e626
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-06-03 18:56:50 -0400

    Update product.py

--- sync/models/forward_sync/product.py
+++ sync/models/forward_sync/product.py
@@ -204,7 +204,7 @@ class sync_products:
         product.sale_ok = True
         ext.res_id = product.id
 
-        _logger.info(f"PRODUCT.PY: Created product {product.name} with company_id {company_id}.")
+        _logger.info(f"PRODUCT.PY: Created product {product.name}.")
         return product
 
 
