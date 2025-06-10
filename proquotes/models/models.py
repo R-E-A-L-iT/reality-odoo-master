2c0c6808bfef49f76bd80ea9971b2c0da9ae4620
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-06-10 15:13:09 -0400

    Add default header/footer fields for quotation templates
    Add default header/footer template values
    
    Update quotesTemplates.xml
    
    Update quotesTemplates.xml

--- proquotes/models/models.py
+++ proquotes/models/models.py
@@ -856,6 +856,12 @@ class AccountMoveSend(models.TransientModel):
 
     #     return res
 
+class SaleOrderTemplate(models.Model):
+    _inherit = 'sale.order.template'
+
+    header_id = fields.Many2one('header.footer', string="Default Header")
+    footer_id = fields.Many2one('header.footer', string="Default Footer")
+
 class order(models.Model):
     _inherit = "sale.order"
 
@@ -925,6 +931,12 @@ class order(models.Model):
         string="Header OLD",
         help="Header selection field",
     )
+
+    @api.onchange('sale_order_template_id')
+    def _onchange_sale_order_template_id_set_header_footer(self):
+        if self.sale_order_template_id:
+            self.header_id = self.sale_order_template_id.header_id
+            self.footer_id = self.sale_order_template_id.footer_id
     
     # this function adds sales@r-e-a-l.it as a follower automatically upon creation so it receives all the relevant emails
     @api.model
