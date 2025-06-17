fb2d875c04ee51b1e924a25e02014196bea9389b
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-06-17 11:24:31 -0400

    Add ProRentals module for custom calculation

---
+++ prorentals/__init__.py
@@ -0,0 +1,1 @@
+from . import product_rental_pricing
---
+++ prorentals/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "ProRentals",
+    "version": "17.0.1.0.0",
+    "summary": "Adds custom calculated rental period pricing",
+    "depends": ["sale_renting"],
+    "author": "Ezekiel J. deBlois",
+    "license": "LGPL-3",
+    "assets": {},
+    "data": [
+        "views/product_rental_pricing_views.xml",
+    ],
+    "installable": True,
+}
