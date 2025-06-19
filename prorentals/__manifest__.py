0378ee4d5331a99b724d55876169c21ce7e344fd
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-06-19 11:17:52 -0400

    Add prorentals module

---
+++ prorentals/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "ProRentals",
+    "version": "17.0.1.0.0",
+    "summary": "Adds custom calculated rental period pricing",
+    "depends": ["sale_renting"],
+    "author": "Ezekiel J. deBlois",
+    "license": "LGPL-3",
+    "assets": {},
+    "data": [
+        # "views/product_rental_pricing_views.xml",
+        "data/calculated_recurrence.xml",
+    ],
+    "installable": True,
+}
