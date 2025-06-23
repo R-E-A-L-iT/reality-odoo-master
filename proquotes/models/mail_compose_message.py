e350fb777f9bd94b00085f8f6888493eb02b98a3
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-06-23 15:16:51 -0400

    Remove unnessecary imports and remove mail.thread notify_recipient_groups function

--- proquotes/models/mail_compose_message.py
+++ proquotes/models/mail_compose_message.py
@@ -1,20 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import ast
-import base64
-from email.policy import default
-import re
-
-from datetime import datetime, timedelta
-from functools import partial
-from itertools import groupby
 import logging
 
-from odoo import api, fields, models, SUPERUSER_ID, _, tools
-from odoo.exceptions import AccessError, UserError, ValidationError
-from odoo.tools.misc import formatLang, get_lang
-from odoo.osv import expression
-from odoo.tools import float_is_zero, float_compare
+from odoo import api, fields, models, _
 from odoo import models, fields, api
 
 _logger = logging.getLogger(__name__)
@@ -45,7 +33,6 @@ class MailComposeMessage(models.TransientModel):
             
             if template:
                 defaults['template_id'] = template.id
-                # defaults['use_template'] = True
 
         elif model in ['account.move']:
             if 'partner_ids' in template_model._fields:
@@ -64,18 +51,6 @@ class MailComposeMessage(models.TransientModel):
 
             if template:
                 defaults['template_id'] = template.id
-                # defaults['use_template'] = True
                 defaults['attachment_ids'] = [(5, 0, 0)]
 
         return defaults
-
-class mail(models.TransientModel):
-    _inherit = "mail.compose.message"
-
-    def get_mail_values(self, res_ids):
-        # Force 'reply_to' to be the same as 'email_from'
-        result = super().get_mail_values(res_ids)
-        for key in result:
-            result[key]["reply_to"] = result[key]["email_from"]
-            result[key]["reply_to_force_new"] = True
-        return result
