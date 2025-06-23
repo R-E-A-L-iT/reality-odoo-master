a9ab2d8c9b0337fb3eb4c101cee428d9ca9effab
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-06-23 10:29:20 -0400

    Test change

--- proportal/__manifest__.py
+++ proportal/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": """
 		Portal Upgrade Module that adds Advanced Features""",
     "description": """
-		Module that allows expands Customer Portal 2
+		Module that allows expands Customer Portal Test
 	""",
     "author": "Ty Cyr",
     "license": "LGPL-3",
