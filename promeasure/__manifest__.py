2a77396e9bd91da7dd420f441d3478239226f07f
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-06-23 08:45:01 -0400

    Revert "Remove module"
    This reverts commit b886bf8718b741dff035fd8a1dbe8b11a194e525.

---
+++ promeasure/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    "name": "ProMeasure",
+    "summary": """
+		Portal Upgrade Module that adds Advanced Features""",
+    "description": """
+		Module that expands website visitor and crm reveal data records
+	""",
+    "author": "Joshua Brodie",
+    "license": "LGPL-3",
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    "category": "Sales",
+    "version": "17.2",
+    # any module necessary for this one to work correctly
+    "depends": [
+        "base",
+        "website",
+        'website_sale',
+        "crm",
+        'website_crm_iap_reveal'
+    ],
+    # always loaded
+    "data": [
+        "views/website_visitor_views.xml"
+    ],
+    'installable': True,
+    'application': False
+}
