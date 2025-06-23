f773f299a5aa4d781eda4752e6008d5fe952691d
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-06-23 15:53:33 -0400

    Test new function for giving access to non portal/internal user followers

--- proquotes/models/mail_thread.py
+++ proquotes/models/mail_thread.py
@@ -21,6 +21,34 @@ _logger = logging.getLogger(__name__)
 class MailThread(models.AbstractModel):
     _inherit = 'mail.thread'
 
+    def _notify_get_recipients_groups(self, message, model_description, msg_vals=None):
+        groups = super()._notify_get_recipients_groups(message, model_description, msg_vals=msg_vals)
+
+        if not self:
+            return groups
+
+        self.ensure_one()
+
+        # Fetch access link (requires portal.mixin inheritance)
+        if not isinstance(self, self.env.registry['portal.mixin']):
+            return groups
+
+        access_token = self._portal_ensure_token()
+        access_link = self.get_portal_url()
+
+        for group in groups:
+            group_name, match_func, options = group
+
+            if group_name == 'follower':
+                options['active'] = True
+                options['has_button_access'] = True
+                options['button_access'] = {
+                    'url': access_link,
+                    'title': _('View Invoice') if self._name == 'account.move' else _('View Document')
+                }
+
+        return groups
+
     # def _notify_get_recipients_groups(self, message, model_description, msg_vals=None):
     #     groups = [
     #         [
