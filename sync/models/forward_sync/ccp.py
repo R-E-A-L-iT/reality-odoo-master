d8661df14d60456ee6fdbbfc6b90a52bc050c83e
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-06-26 09:34:45 -0400

    Small sync module change

--- sync/models/forward_sync/ccp.py
+++ sync/models/forward_sync/ccp.py
@@ -246,13 +246,13 @@ class sync_ccp:
                                         "updateCCP: Field 'owner' changed for CCP EID/SN %s. Old Value: '%s', New Value: '%s'.",
                                         eidsn, ccp.owner.company_nickname if ccp.owner else None, owner_nickname
                                     )
-                                    ccp.owner = owner.id
+                                    ccp.sudo().write({'owner': owner.id})
                                 else:
                                     _logger.info(
                                         "updateCCP: Field 'owner' for CCP EID/SN %s is unchanged. No update needed. Owner [Odoo]: %s. Owner [Sheets]: %s. CCP ID: %s",
                                         eidsn, ccp.owner.company_nickname if ccp.owner else "None", owner_nickname, ccp_id
                                     )
-                                    ccp.owner = owner.id
+                                    ccp.sudo().write({'owner': owner.id})
 
 
                         # directly update sku (char field in odoo)
