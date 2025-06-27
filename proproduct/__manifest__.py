e819b88ed914cddbbd5de393245860d76299ba0e
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-06-27 16:00:50 -0400

    Add proproduct module

---
+++ proproduct/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "ProProduct",
+    "summary": """
+		Product upgrade module that adds approve financing availability and other Ecommerce features.""",
+    "description": """
+		Product upgrade module that adds approve financing availability and other Ecommerce features.
+	""",
+    "author": "Ezekiel deBlois",
+    "license": "LGPL-3",
+    "category": "Sales",
+    "version": "17.0",
+    "depends": [
+        "base",
+        "website",
+        "product",
+        "website_sale",
+    ],
+    "data": [
+        "views/website_sale_product.xml",
+    ],
+}
---
+++ proproduct/models/product_template.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from odoo import api, fields, models
+
+_logger = logging.getLogger(__name__)
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    approve_financing = fields.Boolean("Approve Financing", default=False, help="If checked, the eCommerce page will show an APPROVE financing section.")
