4e783ab5f4a52cfd233053a1cc4456cb40281015
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-06-27 16:51:39 -0400

    Add previous product module functionality to proproduct

--- proproduct/models/product_template.py
+++ proproduct/models/product_template.py
@@ -9,3 +9,12 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     approve_financing = fields.Boolean("Approve Financing", default=False, help="If checked, the eCommerce page will show an APPROVE financing section.")
+
+    is_us = fields.Boolean(string='US')
+    is_ca = fields.Boolean(string='CA')
+
+    def action_is_us_toggle(self):
+        self.is_us = not self.is_us
+
+    def action_is_ca_toggle(self):
+        self.is_ca = not self.is_ca
