cf7715d4614f5e68d8c5deaafb735e33c2d9679f
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-06-28 14:47:02 -0400

    Make add to cart button toggleable

--- proproduct/models/product_template.py
+++ proproduct/models/product_template.py
@@ -9,6 +9,7 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     approve_financing = fields.Boolean("Approve Financing", default=False, help="If checked, the eCommerce page will show an APPROVE financing section.")
+    show_add_to_cart = fields.Boolean("Show Add to Cart Button", default=False, help="If checked, the eCommerce page will show an Add To Cart button.")
 
     is_us = fields.Boolean(string='Is Published in US')
     is_ca = fields.Boolean(string='Is Published in CA')
