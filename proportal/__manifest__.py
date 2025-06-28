c4e47eb79a934eb3a0bbb8d1265e0a32f816ddef
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-06-28 12:49:01 -0400

    Add header language selector override

--- proportal/__manifest__.py
+++ proportal/__manifest__.py
@@ -45,6 +45,7 @@
         "views/portalProject.xml",
         "views/partner_internal.xml",
         "views/productInstance.xml",
+        "views/header_language_selector.xml",
     ],
     # only loaded in demonstration mode
     "demo": [
