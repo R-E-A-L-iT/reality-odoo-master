41427e6502fd8dae9026f540b29d91ead38491e6
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-07-03 11:55:58 -0500

    Controller fix

--- proproduct/controllers/website_sale.py
+++ proproduct/controllers/website_sale.py
@@ -3,22 +3,29 @@ from odoo import http
 from odoo.http import request
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
-class WebsiteSaleInherit(WebsiteSale):
+import logging
+_logger = logging.getLogger(__name__)
 
-    def _get_search_domain(self, search, category, attrib_values, **kwargs):
-        
-        # get default domain
-        domain = super(WebsiteSaleInherit, self)._get_search_domain(search, category, attrib_values, **kwargs)
-        
-        # get current pricelist
-        pricelist = request.website.get_current_pricelist()
-        if pricelist.currency_id: 
-            currency = pricelist.currency_id
+class WebsiteSaleCurrencyFilter(WebsiteSale):
 
-            # append filters
-            if currency.name == 'USD':
-                domain += [('is_us', '=', True)]
-            elif currency.name == 'CAD':
-                domain += [('is_ca', '=', True)]
-                
+    def _get_shop_domain(self, search, category, attrib_values, search_in_description=True):
+        # Get the default domain from Odoo
+        domain = super()._get_shop_domain(search, category, attrib_values, search_in_description=search_in_description)
+        
+        website = request.env['website'].get_current_website()
+        pricelist = website.get_current_pricelist()
+        
+        if pricelist and pricelist.currency_id:
+            currency = pricelist.currency_id.name
+            _logger.info(f"[proproduct] Active pricelist currency: {currency}")
+            if currency == 'USD':
+                _logger.info("[proproduct] Adding domain filter: ('is_us', '=', True)")
+                domain.append(('is_us', '=', True))
+            elif currency == 'CAD':
+                _logger.info("[proproduct] Adding domain filter: ('is_ca', '=', True)")
+                domain.append(('is_ca', '=', True))
+            else:
+                _logger.info("[proproduct] No currency-based filtering applied.")
+
+        _logger.info(f"[proproduct] Final computed shop domain: {domain}")
         return domain
