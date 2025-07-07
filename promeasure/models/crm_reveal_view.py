7d20612529c136b9d1d628a1c1521f8d853fde91
pooja-shivlab <pooja.ahuja@shivlab.com>
2025-07-07 13:07:55 +0530

    fix: trying to add visitor_id in reveal_view

--- promeasure/models/crm_reveal_view.py
+++ promeasure/models/crm_reveal_view.py
@@ -35,3 +35,32 @@ class CrmRevealView(models.Model):
                     )
 
         return super().create(vals_list)
+        
+    def _create_reveal_view(self, website_id, url, ip_address, country_code, state_code, rules_excluded):
+        rules = self.env['crm.reveal.rule']._match_url(website_id, url, country_code, state_code, rules_excluded)
+
+        if rules:
+            visitor_id = None
+            try:
+                visitor = request.env['website.visitor']._get_visitor_from_request()
+                visitor_id = visitor.id if visitor else None
+            except Exception as e:
+                _logger.debug("Unable to get visitor in _create_reveal_view: %s", e)
+
+            for rule in rules:
+                if str(rule['id']) in rules_excluded:
+                    continue
+
+                query = """
+                    INSERT INTO crm_reveal_view (reveal_ip, reveal_rule_id, reveal_state, create_date, visitor_id)
+                    VALUES (%s, %s, 'to_process', now() at time zone 'UTC', %s)
+                    ON CONFLICT DO NOTHING;
+                """
+                params = (ip_address, rule['id'], visitor_id)
+                self.env.cr.execute(query, params)
+
+                rules_excluded.append(str(rule['id']))
+
+            return rules_excluded
+
+        return False
