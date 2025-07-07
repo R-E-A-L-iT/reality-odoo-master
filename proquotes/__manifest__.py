4d547a9d8564f0bc3b82ff2cc105bcce3e72877b
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-07-07 16:52:36 -0400

    Add create rental quote form action
    Update controllers.py

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -60,6 +60,9 @@
         'web.assets_backend': [
             "proquotes/static/src/JS/website_preview.js",
         ],
+        'website.assets_wysiwyg': [
+            'proquotes/static/src/JS/rental_quote_form_editor.js',
+        ],
     },
 
     "version": "17.0",
@@ -67,6 +70,7 @@
     # always loaded
     "data": [
         "security/ir.model.access.csv",
+        "data/rental_quote_data.xml",
         "views/Other/tax.xml",
         "views/Quote/quotesBackend.xml",
         "views/Quote/quotesFrontend.xml",
---
+++ proquotes/static/src/JS/rental_quote_form_editor.js
@@ -0,0 +1,55 @@
+/** @odoo-module **/
+
+import { _t } from "@web/core/l10n/translation";
+import FormEditorRegistry from "@website/js/form_editor_registry";
+
+FormEditorRegistry.add('create_rental_quote', {
+    formFields: [{
+        type: 'date',
+        modelRequired: true,
+        name: 'rental_start',
+        fillWith: 'rental_start',
+        string: _t('Start of Rental period'),
+    }, {
+        type: 'date',
+        required: true,
+        fillWith: 'rental_end',
+        name: 'rental_end',
+        string: _t('End of Rental period'),
+    }, {
+        type: 'char',
+        required: true,
+        fillWith: 'company_name',
+        name: 'company_name',
+        string: _t('Company Name'),
+    }, {
+        type: 'char',
+        required: true,
+        fillWith: 'partner_name',
+        name: 'partner_name',
+        string: _t('Name'),
+    }, {
+        type: 'char',
+        required: true,
+        fillWith: 'rental_email',
+        name: 'rental_email',
+        string: _t('Email'),
+    },],
+    fields: [{
+        name: 'sale_order_template_id',
+        type: 'many2one',
+        relation: 'sale.order.template',
+        string: _t('Quotation Template'),
+    }, {
+        name: 'user_id',
+        type: 'many2one',
+        relation: 'res.users',
+        string: _t('Saleperson'),
+    }, {
+        name: 'team_id',
+        type: 'many2one',
+        relation: 'crm.team',
+        string: _t('Sales Team'),
+}],
+    // successPage: '/job-thank-you',
+});
