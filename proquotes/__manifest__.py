285e230f2c9b8c64f9401cbad3e508e64542b6f6
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-07-09 14:51:02 -0400

    Revert "Add create rental quote form action"
    This reverts commit a332a91d13a331659271a39aaf031a6c25d6726e.

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -62,9 +62,6 @@
         'web.assets_backend': [
             "proquotes/static/src/JS/website_preview.js",
         ],
-        'website.assets_wysiwyg': [
-            'proquotes/static/src/JS/rental_quote_form_editor.js',
-        ],
     },
 
     "version": "17.0",
@@ -72,7 +69,6 @@
     # always loaded
     "data": [
         "security/ir.model.access.csv",
-        "data/rental_quote_data.xml",
         "views/Other/tax.xml",
         "views/Quote/quotesBackend.xml",
         "views/Quote/quotesFrontend.xml",
