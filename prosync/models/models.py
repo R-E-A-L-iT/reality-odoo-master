d608258fff0cb3307b7226377daf5d1472d23c26
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-07-15 10:03:46 -0400

    Update models.py

--- prosync/models/models.py
+++ prosync/models/models.py
@@ -4,3 +4,13 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     sku = fields.Char(string="SKU", readonly=False, index=True, help="Stock Keeping Unit")
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    pricelist_id = fields.Many2one("product.pricelist", "Pricelist_Sync")
+
+    @api.depends("pricelist_id")
+    def _compute_product_pricelist(self):
+        for p in self:
+            p.property_product_pricelist = p.pricelist_id
