1fea60e1b0cd7c0e4187c6c9feab2d4ce80f6835
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-07-28 17:07:36 -0400

    Update portal_main.py

--- proportal/controllers/portal_main.py
+++ proportal/controllers/portal_main.py
@@ -7,9 +7,13 @@ class CustomCustomerPortal(CustomerPortal):
     def _prepare_portal_layout_values(self):
         values = super()._prepare_portal_layout_values()
         partner = request.env.user.partner_id
+        company_ids = partner.portal_companies_ids.ids
+        if partner.parent_id:
+            company_ids.append(partner.parent_id.id)
 
-        # Count products linked to portal_companies_ids or parent
-        companies = partner.portal_companies_ids | partner.parent_id
-        product_count = request.env['res.partner'].browse(companies.ids).mapped('products')
-        values['product_count'] = len(product_count)
+        product_count = request.env['stock.lot'].sudo().search_count([
+            ('owner_id', 'in', company_ids)
+        ])
+        values['product_count'] = product_count
         return values
+
