48440521f048bd02edeebdf9a09d9cd71666c865
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-07-28 16:18:34 -0400

    Fix 404 not found issue

--- proportal/controllers/portal_main.py
+++ proportal/controllers/portal_main.py
@@ -5,20 +5,24 @@ from odoo.exceptions import AccessError
 
 class CustomPortalOrders(CustomerPortal):
 
-    @http.route(['/my/orders/company/<int:order_id>/<int:partner_id>'], type='http', auth="user", website=True)
-    def portal_my_order_custom(self, order_id=None, partner_id=None, **kw):
-        order = request.env['sale.order'].sudo().browse(order_id)
+    @http.route([ '/my/orders/<int:order_id>', '/my/orders/company/<int:order_id>/<int:partner_company_id>' ], type='http', auth="user", website=True)
+    def portal_order_company_page(self, order_id, partner_company_id=None, **kw):
+        order_sudo = request.env['sale.order'].sudo().browse(order_id)
         partner = request.env.user.partner_id
 
-        # Only allow if the partner is the customer OR a follower
-        if not order.exists() or not (
-            partner.id == order.partner_id.id or
-            partner.id in order.message_partner_ids.ids or
-            partner.id in order.partner_id.child_ids.ids
+        if not order_sudo.exists() or not (
+            partner.id == order_sudo.partner_id.id or
+            partner.id in order_sudo.message_partner_ids.ids or
+            partner.id in order_sudo.partner_id.child_ids.ids
         ):
             raise AccessError("You do not have permission to view this order.")
 
-        return request.render("sale.portal_my_order", {
-            'order': order,
+        values = {
+            'sale_order': order_sudo,
+            'partner_company_id': partner_company_id,
             'message': False,
-        })
+            'bootstrap_formatting': True,
+            'action': order_sudo._get_portal_return_action(),
+            'report_type': 'html',
+        }
+        return request.render('sale.sale_order_portal_template', values)
