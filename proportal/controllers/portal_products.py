c156adff9f327aa05bdcf2ffd20797cc60d2e672
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-07-28 17:05:30 -0400

    Fix errors

--- proportal/controllers/portal_products.py
+++ proportal/controllers/portal_products.py
@@ -7,12 +7,13 @@ class CustomerPortalProduct(CustomerPortal):
     @http.route(['/my/products'], type='http', auth="user", website=True)
     def portal_my_products(self, **kwargs):
         partner = request.env.user.partner_id
+        company_ids = partner.portal_companies_ids.ids
+        if partner.parent_id:
+            company_ids.append(partner.parent_id.id)
 
-        # All companies we should match: portal_companies_ids + parent_id
-        partner_companies = partner.portal_companies_ids | partner.parent_id
-
-        # Find products owned by those companies
-        products = request.env['res.partner'].browse(partner_companies.ids).mapped('products')
+        products = request.env['stock.lot'].sudo().search([
+            ('owner_id', 'in', company_ids),
+        ])
 
         values = self._prepare_portal_layout_values()
         values.update({
