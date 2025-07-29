1d323cd13223e88f7f536cc0b1f48d6a764f558b
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-07-29 17:10:07 -0400

    Update portal_main.py

--- proportal/controllers/portal_main.py
+++ proportal/controllers/portal_main.py
@@ -4,32 +4,19 @@ from odoo.addons.portal.controllers.portal import CustomerPortal
 
 class CustomCustomerPortal(CustomerPortal):
 
-    def _prepare_home_portal_values(self):
+    def _prepare_home_portal_values(self, counters):
         values = super()._prepare_home_portal_values(counters)
         partner = request.env.user.partner_id
-        company_ids = partner.portal_companies_ids.ids
 
-        if partner.parent_id:
-            company_ids.append(partner.parent_id.id)
-
-        StockLot = request.env['stock.lot']
         if 'product_count' in counters:
-            values['product_count'] = StockLot.search_count(self._prepare_quotations_domain(partner)) \
-                if StockLot.check_access_rights('read', raise_exception=False) else 0
+            company_ids = partner.portal_companies_ids.ids
+            if partner.parent_id:
+                company_ids.append(partner.parent_id.id)
+
+            product_count = request.env['stock.lot'].sudo().search_count([
+                ('owner', 'in', company_ids)
+            ])
+            values['product_count'] = product_count
 
         return values
-
-    # def _prepare_home_portal_values(self, counters):
-    #     values = super()._prepare_home_portal_values(counters)
-    #     partner = request.env.user.partner_id
-
-    #     SaleOrder = request.env['sale.order']
-    #     if 'quotation_count' in counters:
-    #         values['quotation_count'] = SaleOrder.search_count(self._prepare_quotations_domain(partner)) \
-    #             if SaleOrder.check_access_rights('read', raise_exception=False) else 0
-    #     if 'order_count' in counters:
-    #         values['order_count'] = SaleOrder.search_count(self._prepare_orders_domain(partner), limit=1) \
-    #             if SaleOrder.check_access_rights('read', raise_exception=False) else 0
-
-    #     return values
 
