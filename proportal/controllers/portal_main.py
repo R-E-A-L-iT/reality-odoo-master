a0bd07c3959c507bc6d9428595c41161c1787582
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-07-29 17:03:31 -0400

    Try new function

--- proportal/controllers/portal_main.py
+++ proportal/controllers/portal_main.py
@@ -5,16 +5,18 @@ from odoo.addons.portal.controllers.portal import CustomerPortal
 class CustomCustomerPortal(CustomerPortal):
 
     def _prepare_home_portal_values(self):
-        values = super()._prepare_home_portal_values()
+        values = super()._prepare_home_portal_values(counters)
         partner = request.env.user.partner_id
         company_ids = partner.portal_companies_ids.ids
+
         if partner.parent_id:
             company_ids.append(partner.parent_id.id)
 
-        product_count = request.env['stock.lot'].sudo().search_count([
-            ('owner', 'in', company_ids)
-        ])
-        values['product_count'] = product_count
+        StockLot = request.env['stock.lot']
+        if 'product_count' in counters:
+            values['product_count'] = StockLot.search_count(self._prepare_quotations_domain(partner)) \
+                if StockLot.check_access_rights('read', raise_exception=False) else 0
+
         return values
 
     # def _prepare_home_portal_values(self, counters):
