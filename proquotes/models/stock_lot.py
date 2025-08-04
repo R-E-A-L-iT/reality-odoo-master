54825e8f4fbdc8b18c97b9bcfa892b0a9ea62531
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-08-04 11:38:36 -0400

    Add PDF to stock.lot
    Add PDF field to stock.lot
    
    Update __manifest__.py

--- proquotes/models/stock_lot.py
+++ proquotes/models/stock_lot.py
@@ -24,6 +24,9 @@ class owner(models.Model):
 
     owner = fields.Many2one("res.partner", string="Owner")
 
+    document_pdf = fields.Binary(string="Attached PDF")
+    document_pdf_filename = fields.Char(string="Filename")
+
     def copy_label(self):
         # Form Button Needs a Python Target Function
         return
