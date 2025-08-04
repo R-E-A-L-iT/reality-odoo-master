0711f21a8772356af89a76ba15904e5d315e6a98
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-08-04 11:38:36 -0400

    Add PDF to stock.lot
    Add PDF field to stock.lot
    
    Update __manifest__.py

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -80,6 +80,8 @@
         "views/Purchase/purchase_preview.xml",
         "views/Purchase/purchase_internal.xml",
         # other
+        "views/Other/stock_lot.xml",
+        "views/Other/product_template.xml",
         "views/Other/tax.xml",
         "views/Other/rentalTerms.xml",
         "views/Other/normalTerms.xml",
