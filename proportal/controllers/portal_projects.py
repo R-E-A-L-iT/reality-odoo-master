4f6fee1c99dc238e88d244cbedd239c9dbc898e3
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-08-26 13:44:17 -0400

    Update controller

--- proportal/controllers/portal_projects.py
+++ proportal/controllers/portal_projects.py
@@ -10,24 +10,67 @@ def _project_collab_domain_for_user(user):
     partner_ids = [commercial.id] + commercial.child_ids.ids
 
     Project = request.env["project.project"]
-    model_fields = Project._fields
-
-    cond1 = ('message_partner_ids', 'child_of', commercial.id)
-    cond2 = ('partner_id', 'child_of', commercial.id)
-
-    if 'collaborator_ids' in model_fields:
-        cond3 = ('collaborator_ids', 'in', partner_ids)
-    elif 'collaborator_user_ids' in model_fields:
-        cond3 = ('collaborator_user_ids', 'in', user.id)
+    f1 = ('message_partner_ids', 'child_of', commercial.id)
+    f2 = ('partner_id', 'child_of', commercial.id)
+    if 'collaborator_ids' in Project._fields:
+        f3 = ('collaborator_ids', 'in', partner_ids)
+    elif 'collaborator_user_ids' in Project._fields:
+        f3 = ('collaborator_user_ids', 'in', user.id)
     else:
-        cond3 = ('id', '=', 0)
-
-    dom = ['|', '|', cond1, cond2, cond3]
-
-    return dom
+        f3 = ('id', '=', 0)
+    return ['|','|', f1, f2, f3]
 
 
 class CustomerPortal(ProjectPortal):
+
+    @http.route(
+        ["/my/project/<int:project_id>", "/my/projects/<int:project_id>"],
+        type="http", auth="user", website=True)
+    def portal_my_project(self, project_id, access_token=None, page=1, sortby=None, **kw):
+        Project = request.env["project.project"].sudo()
+        # Allow if (a) access_token is present OR (b) collaborator/follower/customer
+        allowed = bool(access_token)
+        if not allowed:
+            dom = ['&', ('id', '=', project_id)] + _project_collab_domain_for_user(request.env.user)
+            allowed = bool(Project.search_count(dom))
+        if not allowed:
+            return request.redirect("/my")
+
+        project = Project.browse(project_id)
+
+        # Tasks listing (basic parity with Odoo’s defaults)
+        Task = request.env["project.task"].sudo()
+        searchbar_sortings = {
+            "date": {"label": "Newest", "order": "create_date desc"},
+            "name": {"label": "Name", "order": "name asc"},
+        }
+        sortby = sortby if sortby in searchbar_sortings else "date"
+        order = searchbar_sortings[sortby]["order"]
+
+        items_per_page = getattr(self, "_items_per_page", 20)
+        task_domain = [('project_id', '=', project_id)]
+        task_count = Task.search_count(task_domain)
+        pager = portal_pager(
+            url=f"/my/project/{project_id}",
+            url_args={"sortby": sortby},
+            total=task_count,
+            page=page,
+            step=items_per_page,
+        )
+        tasks = Task.search(task_domain, order=order, limit=items_per_page, offset=pager["offset"])
+
+        values = self._prepare_portal_layout_values()
+        values.update({
+            "project": project,
+            "tasks": tasks,
+            "pager": pager,
+            "page_name": "project",
+            "default_url": f"/my/project/{project_id}",
+            "searchbar_sortings": searchbar_sortings,
+            "sortby": sortby,
+        })
+        # stock template for project detail:
+        return request.render("project.portal_my_project", values)
 
     # Count for the portal tiles/navbar
     def _prepare_home_portal_values(self, counters):
