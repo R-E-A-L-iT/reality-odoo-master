c9200a72b7f6e241108d6b67c333be38a4c473b1
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-08-27 11:09:12 -0400

    Add ability to send masked emails from CRM Leads

---
+++ chatter_google_message/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Chatter Google Message Button',
+    'version': '17.0.1.0.0',
+    'category': 'Mail',
+    'summary': 'Adds a Google Message button to the chatter for CRM leads',
+    'description': '''
+        This module adds a new "Send Google Message" button to the chatter
+        for CRM leads. When the simple_email_layout field is True, emails
+        are sent without headers and footers.
+    ''',
+    'author': 'RealIT Custom',
+    'depends': ['mail', 'crm'],
+    'data': [
+        'views/res_users_extend.xml',
+    ],
+    "assets": {
+        'web.assets_backend': [
+            "chatter_google_message/static/src/xml/chatter_templates.xml",
+            "chatter_google_message/static/src/js/chatter_patch.js",
+        ],
+    },
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ chatter_google_message/models/crm_lead.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+
+    def prepare_google_message(self):
+        """
+        Prepare lead for Google message by setting simple_email_layout to True
+        and returning the original value for restoration.
+        """
+        original_value = self.simple_email_layout
+        
+        if not self.simple_email_layout:
+            self.write({'simple_email_layout': True})
+        
+        return {'original_value': original_value}
+
+    def restore_google_message_settings(self, original_value):
+        """
+        Restore the original simple_email_layout value after sending.
+        """
+        if self.simple_email_layout != original_value:
+            self.write({'simple_email_layout': original_value})
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    ba_mass_signature = fields.Html(string='Mass Signature')
