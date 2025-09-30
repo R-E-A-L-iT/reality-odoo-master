c62b125e03d18530799c5a137562e0b7f0e6ba25
umesh-braincrewapps <146071849+umesh-braincrewapps@users.noreply.github.com>
2025-09-30 11:22:08 +0530

    leads to assign correct leads to US company [update code]
     leads to assign correct leads to US company [update code]

--- proquotes/models/__init__.py
+++ proquotes/models/__init__.py
@@ -3,6 +3,7 @@
 from . import account_move_line
 from . import account_move_send
 from . import account_move
+from . import crm_lead
 from . import header_footer
 from . import helpdesk_ticket
 from . import ir_ui_view
