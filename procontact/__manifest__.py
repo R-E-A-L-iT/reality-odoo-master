a860aa01519b1655a6df3327129c9c00f02fa01a
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-10-02 14:30:19 -0400

    Remove unused view

--- procontact/__manifest__.py
+++ procontact/__manifest__.py
@@ -8,7 +8,7 @@
     "depends": ["base"],
     "data": [
         "views/contact_internal.xml",
-        "views/contact_list.xml"
+        # "views/contact_list.xml"
     ],
     "installable": True,
     "application": True,
