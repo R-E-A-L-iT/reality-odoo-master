531e11efbe32f70d0e00f4617a4457dead8991de
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-10-09 13:30:55 -0400

    Fix subtasks default state being In Progress

--- project_task_stage_customization/models/project_task.py
+++ project_task_stage_customization/models/project_task.py
@@ -8,3 +8,12 @@ class ProjectTask(models.Model):
 
     state = fields.Selection(selection_add=[('011_not_started', 'Not started')],default='011_not_started',ondelete={'011_not_started': 'cascade'})
 
+    # set default state in case task is a subtask
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get('parent_id'):
+                vals['state'] = '011_not_started'
+            else:
+                vals.setdefault('state', '011_not_started')
+        return super().create(vals_list)
