65e6344e15e84abc481c66d58c1e1c19eb6d5d01
Ezekiel J. deBlois <ezekiel@deblois.me>
2025-10-09 15:04:44 -0400

    Fix for reveal_ip issue

--- proquotes/models/crm_lead.py
+++ proquotes/models/crm_lead.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import logging
-from odoo import api, models
+from odoo import api, models, fields
 
 _logger = logging.getLogger(__name__)
 
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
+
+    reveal_ip_public = fields.Char(store=True, compute='_compute_reveal_ip_public')
+
+    @api.depends('reveal_ip')
+    def _compute_reveal_ip_public(self):
+        for rec in self:
+            rec.reveal_ip_public = rec.sudo().reveal_ip or False
 
     @api.model
     def create(self, vals):
