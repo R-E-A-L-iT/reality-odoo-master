583d202df75b9203c49c0b6c4d65fd27f8311428
umesh-braincrewapps <146071849+umesh-braincrewapps@users.noreply.github.com>
2025-10-14 15:36:02 +0530

    Updated code
    Updated code

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -102,7 +102,7 @@
         "views/Other/header_footer.xml",
         "views/Invoice/invoice_lot.xml",
         "views/Other/stock_picking.xml",
-        # "views/Other/quoteEmailFooter.xml",
+        "views/Other/quoteEmailFooter.xml",
         "views/Other/helpdeskTicket.xml",
         "views/Other/header_footer_values.xml",
         "views/Other/preconfigured_sections.xml",
