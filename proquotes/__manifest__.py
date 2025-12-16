d720ca1195a3dab803778d320962dc345b4a6475
umesh-braincrewapps <146071849+umesh-braincrewapps@users.noreply.github.com>
2025-12-16 16:31:48 +0530

    Email template related code
    Email template related code
    
    Revert "Email template related code"
    
    This reverts commit 8d50bd5762c2997ec4c7dbed33c958368e99e691.
    
    update code[Email Template]
    
    update code[Email Template]
    
    update code
    
    update code
    
    Revert "update code"
    
    This reverts commit 4b7ac7dc3a6238dc6cd6d4b01151e0ba24deb27c.
    
    Revert "update code[Email Template]"
    
    This reverts commit acbd76ef6c9d5c29e8a0ef69c08bb11d262c2d20.
    
    Update code [email template]
    
    Update code [email template]
    
    update code
    
    update code
    
    Revert "update code"
    
    This reverts commit 6eb84d3e92e1c9ee00eefb6c2d9aa792031d3d3e.
    
    Revert "Update code [email template]"
    
    This reverts commit d4072f395cd83fd02bbfc5c6f0a3262f0daf5fb4.
    
    Revert "Revert "Update code [email template]""
    
    This reverts commit 6b434be55333901b486836a590fe721cfbe761e9.
    
    update code
    
    update code

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -103,6 +103,7 @@
         "views/Other/helpdeskTicket.xml",
         "views/Other/header_footer_values.xml",
         "views/Other/preconfigured_sections.xml",
+        # "views/Other/res_config_extend_view.xml",
         "views/Quote/table_align.xml",
         "data/thanks_for_payment.xml",
     ],
