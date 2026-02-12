2db79b0a76c4df6f305330d84ed0dca9b35e26ee
Ezekiel J. deBlois <ezekiel@deblois.me>
2026-02-12 17:49:26 -0500

    Update again

--- prophone/models/quo_call.py
+++ prophone/models/quo_call.py
@@ -24,7 +24,7 @@ class QuoCall(models.Model):
     _inherit = ["mail.thread", "mail.activity.mixin"]
     _order = "started_at desc, id desc"
 
-    name = fields.Char(string="Display Name", compute="_compute_name", store=True)
+    name = fields.Char(string="Display Name", compute="_compute_name", store=False)
 
     quo_call_id = fields.Char(string="Quo Call ID", required=True, index=True)
     phone_number_id = fields.Char(string="Phone Number ID", index=True)
