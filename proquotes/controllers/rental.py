bf0aef92fdc95f6b12e91761fe34265bbf34544d
Ezekiel J. deBlois <ezekiel@deblois.me>
2026-02-16 10:34:55 -0500

    Update to ProQuotes & Rentals #1
    update to fix popup error
    
    Update quote_preview.xml
    
    Fix properly
    
    Update quote_preview.xml
    
    Update quote_preview.xml
    
    Remove rental behaviour
    
    Fix
    
    Update quote_report.xml
    
    Update quote_report.xml
    
    Update quote_report.xml
    
    Update quote_preview.xml
    
    Add latest code for custom rental price and preview address
    
    - Make rental pricing option with custom formula for rental orders
    -Make shipping, delivery address, rental start/end dates and po number editable on quote frontends
    
    Add latest code for resolved Rental Preview issue.
    
    Add latest code for resolved rental preview error.
    
    Add latest code for company hierarchy.

--- proquotes/controllers/rental.py
+++ proquotes/controllers/rental.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+# 2026-02-25 - Brainecrew Apps
 
 import binascii
 from random import sample
@@ -17,262 +18,120 @@ _logger = logging.getLogger(__name__)
 
 
 class RentalCustomerPortal(cPortal):
-    # Handle Update of rental fields in quote
-
-    def validate(self, string):
-        reg = "^[a-zA-Z0-9- ]*$"
-        return not (re.search(reg, string) == None)
-
+    
     @http.route(
-        ["/my/orders/<int:order_id>/newAddress"],
+        ["/my/orders/<int:order_id>/update_invoice_address"],
         type="json",
         auth="public",
         website=True,
     )
-    def newAdd(self, order_id, newAdd, access_token=None, **post):
-        # Confirm Access
+    def update_invoice_address(
+            self, order_id, name=None, street=None, city=None,
+            state=None, zip=None, country=None,
+            access_token=None, **post
+    ):
         try:
-            order_sudo = self._document_check_access(
-                "sale.order", order_id, access_token=access_token
-            )
+            order_sudo = request.env["sale.order"].sudo().browse(order_id)
+            order_sudo.check_access_rights("read")
         except (AccessError, MissingError):
-            return request.redirect("/my")
+            return {"error": "Access denied"}
 
-        if str(newAdd) == "True" or str(newAdd) == "False":
-            order_sudo.rental_diff_add = True if str(newAdd) == "True" else False
+        address_vals = {
+            "name": name,
+            "street": street,
+            "city": city,
+            "zip": zip,
+        }
 
-        return
+        if country:
+            address_vals["country_id"] = int(country)
+
+        if state:
+            address_vals["state_id"] = int(state)
+
+        inv = order_sudo.partner_invoice_id
+
+        if inv and inv.id != order_sudo.partner_id.id:
+            inv.sudo().write(address_vals)
+        else:
+            address_vals.update({
+                "type": "invoice",
+                "parent_id": order_sudo.partner_id.id,
+            })
+            new_partner = request.env["res.partner"].sudo().create(address_vals)
+            order_sudo.partner_invoice_id = new_partner.id
+
+        return {"success": True}
 
     @http.route(
-        ["/my/orders/<int:order_id>/street"], type="json", auth="public", website=True
-    )
-    def street(self, order_id, street, access_token=None, **post):
-        # Confirm Access
-        try:
-            order_sudo = self._document_check_access(
-                "sale.order", order_id, access_token=access_token
-            )
-        except (AccessError, MissingError):
-            return request.redirect("/my")
-
-        if not self.validate(street):
-            return
-
-        order_sudo.rental_street = street
-
-        return
-
-    @http.route(
-        ["/my/orders/<int:order_id>/city"], type="json", auth="public", website=True
-    )
-    def city(self, order_id, city, access_token=None, **post):
-        # Confirm Access
-        try:
-            order_sudo = self._document_check_access(
-                "sale.order", order_id, access_token=access_token
-            )
-        except (AccessError, MissingError):
-            return request.redirect("/my")
-
-        if not self.validate(city):
-            return
-
-        order_sudo.rental_city = city
-
-        return
-
-    @http.route(
-        ["/my/orders/<int:order_id>/zip"], type="json", auth="public", website=True
-    )
-    def zip(self, order_id, zip, access_token=None, **post):
-        # Confirm Access
-        try:
-            order_sudo = self._document_check_access(
-                "sale.order", order_id, access_token=access_token
-            )
-        except (AccessError, MissingError):
-            return request.redirect("/my")
-
-        if not self.validate(zip):
-            return
-
-        order_sudo.rental_zip = zip
-
-        return
-
-    @http.route(
-        ["/my/orders/<int:order_id>/state"], type="json", auth="public", website=True
-    )
-    def state(self, order_id, state, access_token=None, **post):
-        # Confirm Access
-        try:
-            order_sudo = self._document_check_access(
-                "sale.order", order_id, access_token=access_token
-            )
-        except (AccessError, MissingError):
-            return request.redirect("/my")
-
-        if state == "Select":
-            order_sudo.rental_state = False
-            return
-        if not self.validate(state):
-            return
-
-        # Codes Come From Odoo
-
-        # Canada
-        stateCodes = dict()
-        stateCodes["Alberta"] = 533
-        stateCodes["British Columbia"] = 534
-        stateCodes["Manitoba"] = 535
-        stateCodes["New Brunswick"] = 536
-        stateCodes["Newfoundland and Labrador"] = 537
-        stateCodes["Northwest Territories"] = 538
-        stateCodes["Nova Scotia"] = 539
-        stateCodes["Nunavut"] = 540
-        stateCodes["Ontario"] = 541
-        stateCodes["Prince Edward Island"] = 542
-        stateCodes["Quebec"] = 543
-        stateCodes["Saskatchewan"] = 544
-        stateCodes["Yukon"] = 545
-        # US
-        stateCodes["Alabama"] = 9
-        stateCodes["Alaska"] = 10
-        stateCodes["Arizona"] = 11
-        stateCodes["Arkansas"] = 12
-        stateCodes["California"] = 13
-        stateCodes["Colorado"] = 14
-        stateCodes["Connecticut"] = 15
-        stateCodes["Delaware"] = 16
-        stateCodes["District of Columbia"] = 17
-        stateCodes["Florida"] = 18
-        stateCodes["Georgia"] = 19
-        stateCodes["Hawaii"] = 20
-        stateCodes["Idaho"] = 21
-        stateCodes["Illinois"] = 22
-        stateCodes["Indiana"] = 23
-        stateCodes["Iowa"] = 24
-        stateCodes["Kansas"] = 25
-        stateCodes["Kentucky"] = 26
-        stateCodes["Louisiana"] = 27
-        stateCodes["Maine"] = 28
-        stateCodes["Montana"] = 29
-        stateCodes["Nebraska"] = 30
-        stateCodes["Nevada"] = 31
-        stateCodes["New Hampshire"] = 32
-        stateCodes["New Jersey"] = 33
-        stateCodes["New Mexico"] = 34
-        stateCodes["New York"] = 35
-        stateCodes["North Carolina"] = 36
-        stateCodes["North Dakota"] = 37
-        stateCodes["Ohio"] = 38
-        stateCodes["Oklahoma"] = 39
-        stateCodes["Oregon"] = 40
-        stateCodes["Maryland"] = 41
-        stateCodes["Massachusetts"] = 42
-        stateCodes["Michigan"] = 43
-        stateCodes["Minnesota"] = 44
-        stateCodes["Mississippi"] = 45
-        stateCodes["Missouri"] = 46
-        stateCodes["Pennsylvania"] = 47
-        stateCodes["Rhode Island"] = 48
-        stateCodes["South Carolina"] = 49
-        stateCodes["South Dakota"] = 50
-        stateCodes["Tennessee"] = 51
-        stateCodes["Texas"] = 52
-        stateCodes["Utah"] = 53
-        stateCodes["Vermont"] = 54
-        stateCodes["Virginia"] = 55
-        stateCodes["Washington"] = 56
-        stateCodes["West Virginia"] = 57
-        stateCodes["Wisconsin"] = 58
-        stateCodes["Wyoming"] = 59
-
-        if state not in stateCodes:
-            order_sudo.rental_state = False
-
-        order_sudo.rental_state = stateCodes[state]
-
-        return
-
-    @http.route(
-        ["/my/orders/<int:order_id>/country"], type="json", auth="public", website=True
-    )
-    def country(self, order_id, country, access_token=None, **post):
-        # Confirm Access
-        try:
-            order_sudo = self._document_check_access(
-                "sale.order", order_id, access_token=access_token
-            )
-        except (AccessError, MissingError):
-            return request.redirect("/my")
-
-        if not self.validate(country):
-            return
-
-        if country == "Canada":
-            order_sudo.rental_state = False
-            order_sudo.rental_country = 38
-        elif country == "United States":
-            order_sudo.rental_state = False
-            order_sudo.rental_country = 233
-
-        return
-
-    def checkDates(self, order):
-        # Confirm rental date range is valid
-        if order.rental_end == False:
-            return
-        if order.rental_start == False:
-            order.rental_end = False
-
-        start_year, start_month, start_day = str(order.rental_start).split("-")
-        end_year, end_month, end_day = str(order.rental_end).split("-")
-
-        start_date = datetime.date(int(start_year), int(start_month), int(start_day))
-        end_date = datetime.date(int(end_year), int(end_month), int(end_day))
-        _logger.error("Line: 211")
-        if start_date > end_date:
-            order.rental_end = False
-        return
-
-    @http.route(
-        ["/my/orders/<int:order_id>/start_date"],
+        ["/my/orders/<int:order_id>/update_delivery_address"],
         type="json",
         auth="public",
         website=True,
     )
-    def start(self, order_id, start, access_token=None, **post):
-        # Confirm Access
+    def update_delivery_address(
+            self, order_id, name=None, street=None, city=None,
+            state=None, zip=None, country=None,
+            access_token=None, **post
+    ):
         try:
-            order_sudo = self._document_check_access(
-                "sale.order", order_id, access_token=access_token
-            )
+            order_sudo = request.env["sale.order"].sudo().browse(order_id)
+            order_sudo.check_access_rights("read")
         except (AccessError, MissingError):
-            return request.redirect("/my")
+            return {"error": "Access denied"}
 
-        if not self.validate(start):
-            return
+        address_vals = {
+            "name": name,
+            "street": street,
+            "city": city,
+            "zip": zip,
+        }
 
-        order_sudo.rental_start = start
-        self.checkDates(order_sudo)
-        return
+        if country:
+            address_vals["country_id"] = int(country)
+
+        if state:
+            address_vals["state_id"] = int(state)
+
+        dlv = order_sudo.partner_shipping_id
+
+        if dlv and dlv.id != order_sudo.partner_id.id:
+            dlv.sudo().write(address_vals)
+        else:
+            address_vals.update({
+                "type": "delivery",
+                "parent_id": order_sudo.partner_id.id,
+            })
+            new_partner = request.env["res.partner"].sudo().create(address_vals)
+            order_sudo.partner_shipping_id = new_partner.id
+
+        return {"success": True}
+
 
     @http.route(
-        ["/my/orders/<int:order_id>/end_date"], type="json", auth="public", website=True
+        ["/my/orders/<int:order_id>/update_rental_dates"],
+        type="json",
+        auth="public",
+        website=True,
     )
-    def end(self, order_id, end, access_token=None, **post):
-        # Confirm Access
+    def update_rental_dates(self, order_id, rental_start=None, rental_end=None, access_token=None, **post):
+
         try:
             order_sudo = self._document_check_access(
                 "sale.order", order_id, access_token=access_token
             )
         except (AccessError, MissingError):
-            return request.redirect("/my")
+            return {"error": "Access Denied"}
 
-        if not self.validate(end):
-            return
+        values = {}
 
-        order_sudo.rental_end = end
-        self.checkDates(order_sudo)
-        return
+        if rental_start:
+            values["rental_start_date"] = rental_start
+
+        if rental_end:
+            values["rental_return_date"] = rental_end
+
+        if values:
+            order_sudo.sudo().write(values)
+
+        return {"success": True}
