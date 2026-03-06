48450f54872946d22b883989d53c73da3e03c707
Ezekiel J. deBlois <ezekiel@deblois.me>
2026-03-06 10:26:22 -0500

    Maintenance module update

--- proproduct/__manifest__.py
+++ proproduct/__manifest__.py
@@ -15,11 +15,13 @@
         "product",
         "website_sale",
         "payment",
+        "maintenance",
     ],
     "data": [
         "views/website_sale_product.xml",
         "views/product_template_form.xml",
         "views/wishlist_page.xml",
         "views/website_address.xml",
+        "views/maintenance_equipment_views.xml",
     ],
 }
---
+++ proproduct/models/maintenance_equipment.py
@@ -0,0 +1,27 @@
+from odoo import api, fields, models
+
+
+class MaintenanceEquipment(models.Model):
+    _inherit = "maintenance.equipment"
+
+    equipment_assign_to = fields.Selection(
+        selection_add=[("company", "Company")],
+        ondelete={"company": "set default"},
+    )
+
+    owning_company_id = fields.Many2one(
+        "res.partner",
+        string="Owning Company",
+        domain="[('is_company', '=', True)]",
+        help="Company that owns or uses this equipment when 'Used By' is set to Company.",
+    )
+
+    firmware_version = fields.Char(
+        string="Firmware Version",
+    )
+
+    @api.onchange("equipment_assign_to")
+    def _onchange_equipment_assign_to_clear_owning_company(self):
+        for record in self:
+            if record.equipment_assign_to != "company":
+                record.owning_company_id = False
