22991fed7ce7206f03de64c5026fb1b038cae843
Ezekiel J. deBlois <ezekiel@deblois.me>
2026-03-13 11:40:33 -0400

    New and improved header/footer system
    Footer system update
    
    Update res_user.xml
    
    New footer/header defaults code
    
    Update quote_internal.xml
    
    add default company footer behaviour
    
    Update res_company.xml
    
    Update res_company.xml
    
    Update footer behaviour for emails
    
    Update res_users.py
    
    Update mail.xml
    
    Update mail.xml
    
    Fix footer behaviour for invoices and purchase orders
    
    View fixes
    
    Update account_move.py
    
    More view fixes
    
    Small fixes
    
    Remove old footer code
    
    Update header_footer.py
    
    Update header_footer.xml
    
    add back seed data ugh

--- proquotes/models/res_company.py
+++ proquotes/models/res_company.py
@@ -24,3 +24,8 @@ class company(models.Model):
         default="https://cdn.r-e-a-l.it//images/icons/REALiT-Header.gif",
         required="True",
     )
+    default_footer_id = fields.Many2one(
+        "header.footer",
+        string="Default Footer",
+        domain="[('active', '=', True), ('record_type', '=', 'Footer')]",
+    )
