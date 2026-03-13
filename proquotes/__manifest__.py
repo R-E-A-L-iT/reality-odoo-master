e992de13629cdc06f9c71198634ad949356cd0a7
Ezekiel J. deBlois <ezekiel@deblois.me>
2026-03-13 11:40:33 -0400

    Footer system update

--- proquotes/__manifest__.py
+++ proquotes/__manifest__.py
@@ -99,7 +99,7 @@
         "views/Other/pdf_boxed.xml",
         "views/Other/section_name.xml",
         "views/Other/internal_company_backend.xml",
-        "views/Other/internal_user_backend.xml",
+        "views/Other/res_user.xml",
         "views/Other/renewal.xml",
         "views/Other/header_footer.xml",
         "views/Invoice/invoice_lot.xml",
