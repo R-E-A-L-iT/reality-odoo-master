5ea438076142ca5b728be159b05fa01940924009
Ezekiel J. deBlois <ezekiel@deblois.me>
2026-04-16 12:07:07 -0400

    Fix rental pickups using all lines instead of selected

---
+++ proquotes/models/rental_order_wizard.py
@@ -0,0 +1,38 @@
+from odoo import api, models
+
+class RentalOrderWizard(models.TransientModel):
+    _inherit = "rental.order.wizard"   # replace with exact model name
+
+    @api.model
+    def default_get(self, fields_list):
+        vals = super().default_get(fields_list)
+
+        if vals.get("rental_wizard_line_ids"):
+            filtered_commands = []
+            order_line_ids_to_keep = set()
+
+            for command in vals["rental_wizard_line_ids"]:
+                # usually (0, 0, values)
+                if isinstance(command, (list, tuple)) and len(command) >= 3 and command[0] == 0:
+                    line_vals = command[2]
+                    order_line_id = line_vals.get("order_line_id")
+                    if order_line_id:
+                        order_line_ids_to_keep.add(order_line_id)
+
+            selected_ids = set(
+                self.env["sale.order.line"].browse(list(order_line_ids_to_keep)).filtered(
+                    lambda l: l.selected == 'true'
+                ).ids
+            )
+
+            for command in vals["rental_wizard_line_ids"]:
+                if isinstance(command, (list, tuple)) and len(command) >= 3 and command[0] == 0:
+                    line_vals = command[2]
+                    if line_vals.get("order_line_id") in selected_ids:
+                        filtered_commands.append(command)
+                else:
+                    filtered_commands.append(command)
+
+            vals["rental_wizard_line_ids"] = filtered_commands
+
+        return vals
