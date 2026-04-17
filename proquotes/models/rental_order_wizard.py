2e5e8a1bf5c0daf92f29f4183f662bf12932654a
Ezekiel J. deBlois <ezekiel@deblois.me>
2026-04-17 11:17:09 -0400

    Update rental_order_wizard.py
    Update rental_order_wizard.py
    
    Update rental_order_wizard.py
    
    Update sale_order.py
    
    Update sale_order.py
    
    Try again
    
    Test
    
    Update sale_order.py
    
    Update rental_order_wizard.py

--- proquotes/models/rental_order_wizard.py
+++ proquotes/models/rental_order_wizard.py
@@ -1,45 +1,38 @@
 from odoo import api, models
+import logging
+
+_logger = logging.getLogger(__name__)
+
 
 class RentalOrderWizard(models.TransientModel):
-    _inherit = "rental.order.wizard"   # replace with exact model name
+    _inherit = "rental.order.wizard"
+
+    def _remove_unselected_lines(self):
+        for wizard in self:
+            lines_to_remove = wizard.rental_wizard_line_ids.filtered(
+                lambda l: l.order_line_id and l.order_line_id.selected != 'true'
+            )
+            if lines_to_remove:
+                _logger.info(
+                    "Rental pickup wizard %s: removing unselected lines %s",
+                    wizard.id,
+                    lines_to_remove.mapped("order_line_id").ids,
+                )
+                lines_to_remove.unlink()
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        wizards = super().create(vals_list)
+        wizards._remove_unselected_lines()
+        return wizards
+
+    def write(self, vals):
+        res = super().write(vals)
+        self._remove_unselected_lines()
+        return res
 
     def apply(self):
-        for wizard in self:
-            wizard.rental_wizard_line_ids = wizard.rental_wizard_line_ids.filtered(
-                lambda l: l.order_line_id.selected == 'true'
-            )
+        self.rental_wizard_line_ids.filtered(
+            lambda l: l.order_line_id and l.order_line_id.selected != 'true'
+        ).unlink()
         return super().apply()
-
-    @api.model
-    def default_get(self, fields_list):
-        vals = super().default_get(fields_list)
-
-        if vals.get("rental_wizard_line_ids"):
-            filtered_commands = []
-            order_line_ids_to_keep = set()
-
-            for command in vals["rental_wizard_line_ids"]:
-                # usually (0, 0, values)
-                if isinstance(command, (list, tuple)) and len(command) >= 3 and command[0] == 0:
-                    line_vals = command[2]
-                    order_line_id = line_vals.get("order_line_id")
-                    if order_line_id:
-                        order_line_ids_to_keep.add(order_line_id)
-
-            selected_ids = set(
-                self.env["sale.order.line"].browse(list(order_line_ids_to_keep)).filtered(
-                    lambda l: l.selected == 'true'
-                ).ids
-            )
-
-            for command in vals["rental_wizard_line_ids"]:
-                if isinstance(command, (list, tuple)) and len(command) >= 3 and command[0] == 0:
-                    line_vals = command[2]
-                    if line_vals.get("order_line_id") in selected_ids:
-                        filtered_commands.append(command)
-                else:
-                    filtered_commands.append(command)
-
-            vals["rental_wizard_line_ids"] = filtered_commands
-
-        return vals
