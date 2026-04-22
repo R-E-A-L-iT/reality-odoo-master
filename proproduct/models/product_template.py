260fc008676ff67670c92ffa3d8735fd9e7c455b
Ezekiel J. deBlois <ezekiel@deblois.me>
2026-04-22 11:16:28 -0400

    Make rental behaviour on store disabled by default
    Make rental behaviour on store disabled by default

--- proproduct/models/product_template.py
+++ proproduct/models/product_template.py
@@ -10,6 +10,11 @@ class ProductTemplate(models.Model):
 
     approve_financing = fields.Boolean("Approve Financing", default=False, help="If checked, the eCommerce page will show an APPROVE financing section.")
     show_add_to_cart = fields.Boolean("Show Add to Cart Button", default=False, help="If checked, the eCommerce page will show an Add To Cart button.")
+    rental_behaviour_on_store = fields.Boolean(
+        string="Rental behaviour on store",
+        default=False,
+        help="If enabled, rental-specific elements will be shown on the website product page."
+    )
 
     is_us = fields.Boolean(string='Is Published in US')
     is_ca = fields.Boolean(string='Is Published in CA')
