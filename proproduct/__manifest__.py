1f5a9df6b70c53b91a64a23c93b1ecf03a3ab269
Ezekiel J. deBlois <ezekiel@deblois.me>
2026-04-22 11:53:43 -0400

    Revert "Fix taxes in cart"
    This reverts commit cece7d45187a3e9518cbb081fba322970733ce28.

--- proproduct/__manifest__.py
+++ proproduct/__manifest__.py
@@ -24,6 +24,5 @@
         "views/website_address.xml",
         "views/maintenance_equipment_views.xml",
         "views/website_sale_product_renting.xml",
-        "views/website_cart.xml",
     ],
 }
