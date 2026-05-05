2bc0f9bb9e08dc2e0d3452c37b6afcf6516d60ba
umesh-braincrewapps <146071849+umesh-braincrewapps@users.noreply.github.com>
2026-05-05 12:18:23 +0530

    Updated code [Preconfigured section]
    Updated code [Preconfigured section]

--- proquotes/models/sale_order.py
+++ proquotes/models/sale_order.py
@@ -1466,23 +1466,20 @@ class PreconfigSaleOrder(models.Model):
 
     @api.onchange('preconfigured_section_ids')
     def _onchange_preconfigured_sections(self):
-        commands = []
+        # Keep lines that are NOT from any preconfigured section
+        non_preconfigured = self.order_line.filtered(lambda l: not l.preconfigured_section_id)
 
-        # Delete all previously tracked section lines (handles both removal and re-add without duplicates)
-        for line in self.order_line:
-            if line.preconfigured_section_id:
-                commands.append((2, line.id, 0))
-
-        # Re-add lines for all currently selected sections
+        # Build new virtual records for all currently selected sections
+        new_lines = self.env['sale.order.line']
         for section in self.preconfigured_section_ids:
-            commands.append((0, 0, {
+            new_lines |= self.env['sale.order.line'].new({
                 'order_id': self.id,
                 'name': section.section_name,
                 'display_type': 'line_section',
                 'preconfigured_section_id': section.id,
-            }))
+            })
             for pl in section.product_line_ids:
-                commands.append((0, 0, {
+                new_lines |= self.env['sale.order.line'].new({
                     'order_id': self.id,
                     'product_id': pl.product_id.id,
                     'name': pl.product_name,
@@ -1493,7 +1490,6 @@ class PreconfigSaleOrder(models.Model):
                     'discount': pl.discount,
                     'product_uom_qty': 1,
                     'preconfigured_section_id': section.id,
-                }))
+                })
 
-        if commands:
-            self.order_line = commands
+        self.order_line = non_preconfigured + new_lines
