b33451a855a101241167439d4638124b0a5ac44d
umesh-braincrewapps <146071849+umesh-braincrewapps@users.noreply.github.com>
2026-06-30 11:13:19 +0530

    Updated code [Email button fix]
    Updated code [Email button fix]

--- proquotes/models/crm_lead.py
+++ proquotes/models/crm_lead.py
@@ -83,7 +83,7 @@ class CrmLead(models.Model):
             _logger.info('>>>>>>>Successfully assigned company %s to lead %s', company_to_assign.name, lead.id)
 
     def _notify_by_email_render_layout(self, message, recipients_group, msg_vals=False, render_values=None):
-        if self:
+        if self and recipients_group.get('notification_group_name') in ('user', 'group_sale_salesman'):
             self.ensure_one()
             recipients_group = dict(recipients_group)
             recipients_group['has_button_access'] = True
