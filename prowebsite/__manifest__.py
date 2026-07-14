08199cf52500a860d51b9d3d94e8a41dc4d9bc03
Ezekiel J. deBlois <ezekiel@deblois.me>
2026-07-14 16:27:07 -0400

    New RTC demo booking page

--- prowebsite/__manifest__.py
+++ prowebsite/__manifest__.py
@@ -25,12 +25,14 @@
             'prowebsite/static/src/css/product_page.css',
             'prowebsite/static/src/css/shop_page.css',
             'prowebsite/static/src/css/multimapper.css',
+            'prowebsite/static/src/css/rtc_demo_request.css',
             'prowebsite/static/src/js/header_dropdowns.js',
             'prowebsite/static/src/js/three_product.js',
             'prowebsite/static/src/js/rtc_series.js',
             'prowebsite/static/src/js/rtc_scroll_model.js',
             'prowebsite/static/src/js/product_page.js',
             'prowebsite/static/src/js/multimapper.js',
+            'prowebsite/static/src/js/rtc_demo_request.js',
         ],
     }
 }
