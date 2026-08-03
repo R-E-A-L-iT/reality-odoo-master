64689d8121d41b1314d0a100e9e8e2b786f85d05
umesh-braincrewapps <146071849+umesh-braincrewapps@users.noreply.github.com>
2026-08-03 11:45:22 +0530

    [Chatter Google Message Button] odoo-19 migration
    [Chatter Google Message Button] odoo-19 migration

--- chatter_google_message/static/src/js/chatter_patch.js
+++ chatter_google_message/static/src/js/chatter_patch.js
@@ -8,7 +8,7 @@ patch(Chatter.prototype, {
      * Override toggleComposer to automatically set simple_email_layout
      * before opening the message composer on crm.lead records.
      */
-    async toggleComposer(mode = false) {
+    async toggleComposer(mode = false, options = {}) {
         if (mode === 'message' && this.state.thread.model === 'crm.lead' && this.props.threadId) {
             const result = await this.orm.call('crm.lead', 'prepare_google_message', [[this.props.threadId]]);
             this._google_message_original_value = result.original_value;
@@ -16,14 +16,14 @@ patch(Chatter.prototype, {
         } else {
             this._is_google_message = false;
         }
-        return super.toggleComposer(mode);
+        return super.toggleComposer(mode, options);
     },
 
     /**
      * Override post callback to restore settings after sending on crm.lead.
      */
-    async onPostCallback(data) {
-        const result = await super.onPostCallback(data);
+    async onPostCallback() {
+        const result = await super.onPostCallback();
         if (this._is_google_message && this.state.thread.model === 'crm.lead' && this.props.threadId) {
             await this.orm.call('crm.lead', 'restore_google_message_settings',
                 [[this.props.threadId], this._google_message_original_value]
