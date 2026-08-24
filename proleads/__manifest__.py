583957f7aa4f4e08876c8772d3894be50e584df7
Ezekiel J. deBlois <ezekiel@deblois.me>
2026-08-24 11:53:08 -0400

    Module merge

--- proleads/__manifest__.py
+++ proleads/__manifest__.py
@@ -6,10 +6,15 @@
     "license": "LGPL-3",
     'summary': 'Adds automation for lead registration',
     'description': ' ',
-    'depends': ['base', 'crm', 'website_crm'],
+    # website + website_crm_iap_reveal added when the ProMeasure functionality was
+    # merged in: they provide the website.visitor and crm.reveal.rule/crm.reveal.view
+    # models the merged code extends.
+    'depends': ['base', 'crm', 'website_crm', 'website', 'website_crm_iap_reveal'],
     "data": [
         "views/leadsBackend.xml",
         "views/crm_stage.xml",
+        # Merged from ProMeasure: adds IP address to the website visitor views.
+        "views/website_visitor_views.xml",
     ],
     'assets': {
         'web.assets_backend': [
--- proleads/models/__init__.py
+++ proleads/models/__init__.py
@@ -1,2 +1,7 @@
 from . import crm_lead
 from . import crm_stage
+# Merged from the ProMeasure module: website-visitor IP capture + linking
+# CRM IP-reveal leads to their website visitor.
+from . import crm_reveal_rule
+from . import crm_reveal_view
+from . import website_visitor
