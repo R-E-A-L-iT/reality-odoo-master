bf0bdb32e6bea2949c2f1ad21f982a9183173274
Ezekiel J. deBlois <ezekiel@deblois.me>
2026-09-03 11:11:05 -0400

    Portal fix

--- proportal/controllers/portal_orders.py
+++ proportal/controllers/portal_orders.py
@@ -1,21 +1,22 @@
 from odoo import http
 from odoo.http import request
-from odoo.osv import expression
+from odoo.fields import Domain
 from odoo.addons.portal.controllers.portal import CustomerPortal
 
 class CustomCustomerPortal(CustomerPortal):
 
     def _get_orders_domain(self, partner):
-        domain = super()._get_orders_domain(partner)
+        # Odoo 19 combines this with `&` on fields.Domain objects, so returning
+        # the plain list odoo.osv.expression built in 17 raises
+        # "unsupported operand type(s) for &". See portal_invoice.py.
+        domain = Domain(super()._get_orders_domain(partner))
 
         # Existing behavior: followers can see it
-        follower_domain = [('message_partner_ids', 'in', [partner.id])]
-        domain = expression.OR([domain, follower_domain])
+        domain |= Domain('message_partner_ids', 'in', [partner.id])
 
         # New behavior: portal admin can see orders addressed to their portal companies
         if partner.portal_administrator:
             allowed_company_ids = partner.get_portal_company_commercial_ids()
-            admin_domain = [('partner_id.commercial_partner_id', 'in', allowed_company_ids)]
-            domain = expression.OR([domain, admin_domain])
+            domain |= Domain('partner_id.commercial_partner_id', 'in', allowed_company_ids)
 
         return domain
