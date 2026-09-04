368c731042c7f73c108588ad032ae31ba2379f4f
Ezekiel J. deBlois <ezekiel@deblois.me>
2026-09-04 12:32:45 -0400

    Update

--- proportal/models/res_partner.py
+++ proportal/models/res_partner.py
@@ -15,6 +15,7 @@ from odoo import api, fields, models, SUPERUSER_ID, _, tools
 from odoo.exceptions import AccessError, UserError, ValidationError
 from odoo.tools.misc import formatLang, get_lang
 from odoo.tools import float_is_zero, float_compare
+from odoo.tools import format_date
 from odoo import models, fields, api
 import logging
 
