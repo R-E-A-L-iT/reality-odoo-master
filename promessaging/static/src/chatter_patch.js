624b2f539738b8b8d3d313cf4f9608205a187c39
Ezekiel J. deBlois <ezekiel@deblois.me>
2026-09-22 11:11:21 -0400

    Send message from draft confirmation

--- promessaging/static/src/chatter_patch.js
+++ promessaging/static/src/chatter_patch.js
@@ -3,6 +3,7 @@
 import { Chatter } from "@mail/core/web/chatter";
 import { patch } from "@web/core/utils/patch";
 import { useService } from "@web/core/utils/hooks";
+import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 import { session } from "@web/session";
 import { useState } from "@odoo/owl";
 import { _t } from "@web/core/l10n/translation";
@@ -11,6 +12,7 @@ patch(Chatter.prototype, {
     setup() {
         super.setup();
         this.notification = useService("notification");
+        this.promessagingDialog = useService("dialog");
         this.promessagingDraft = useState({
             draft: false,
             editing: false,
@@ -88,11 +90,24 @@ patch(Chatter.prototype, {
         this.promessagingDraft.value = "";
     },
 
-    async promessagingSendDraft() {
+    promessagingSendDraft() {
         const draft = this.promessagingDraft.draft;
         if (!draft) {
             return;
         }
+        this.promessagingDialog.add(ConfirmationDialog, {
+            title: _t("Send draft as message"),
+            body: _t(
+                "This draft will be posted as a message and emailed to the followers of this document, customers included. This cannot be undone."
+            ),
+            confirmLabel: _t("Send message"),
+            confirm: () => this.promessagingConfirmSendDraft(draft),
+            cancelLabel: _t("Cancel"),
+            cancel: () => {},
+        });
+    },
+
+    async promessagingConfirmSendDraft(draft) {
         await this.orm.call("promessaging.draft", "action_send_draft", [[draft.id]]);
         this.promessagingResetDraft();
         this.load(this.state.thread, ["messages"]);
